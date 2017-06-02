75396256d177e27abff154e80db8507ca5ffc09e
Bryan Ledford <git@bryanledford.com>
2017-06-02 12:27:15 -0700

    update to form post

--- aem-project/core/src/main/java/com/bmc/servlets/FormProcessingServlet.java
+++ aem-project/core/src/main/java/com/bmc/servlets/FormProcessingServlet.java
@@ -1,8 +1,19 @@
 package com.bmc.servlets;
 
 import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
 
 @SlingServlet(resourceTypes = "bmc/components/structure/page", selectors = "wm", methods = {"POST"})
 public class FormProcessingServlet extends SlingAllMethodsServlet {
+
+    @Override
+    protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
+
+    }
+
 }
