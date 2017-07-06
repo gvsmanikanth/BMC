ecd894653ec8ad98a9189026f3aad0198b23d1b8
Peter Heide <pheide@connectivedx.com>
2017-07-06 11:28:05 -0700

    Adding Partner List component. Adding Partner Data page template.

---
+++ aem-project/core/src/main/java/com/bmc/components/PartnerList.java
@@ -0,0 +1,20 @@
+package com.bmc.components;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+
+public class PartnerList extends WCMUsePojo {
+
+    @Override
+    public void activate() throws Exception {
+
+    }
+
+    public String getItems() {
+        String json = "";
+
+
+
+        return json;
+    }
+
+}
