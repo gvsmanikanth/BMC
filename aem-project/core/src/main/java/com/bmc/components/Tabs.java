9681e8557f15a84c100653ce8eb765bc00e88b3d
Eaton Lambert <elambert@connectivedx.com>
2017-07-18 14:46:38 -0700

    DXP-686 Tabs Fix

--- aem-project/core/src/main/java/com/bmc/components/Tabs.java
+++ aem-project/core/src/main/java/com/bmc/components/Tabs.java
@@ -3,14 +3,26 @@ package com.bmc.components;
 import com.adobe.cq.sightly.WCMUsePojo;
 import com.bmc.mixins.MultifieldNodeProvider;
 import org.apache.sling.api.resource.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
+import javax.jcr.Node;
+import javax.jcr.Session;
+import javax.swing.text.AbstractDocument;
 import java.util.List;
 
 public class Tabs extends WCMUsePojo implements MultifieldNodeProvider {
 
+    private static final Logger logger = LoggerFactory.getLogger(Tabs.class);
+
+
+    protected Resource resource;
+
     @Override
     public void activate() throws Exception {
         tabs = getMultiFieldNodes("tabdata");
+        logger.info(resource.getValueMap().get("jcr:baseVersion").toString());
     }
     private List<Resource> tabs;
 
