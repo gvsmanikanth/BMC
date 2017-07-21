c391ac5dc609a86bae243b6cd35a0bd58a06dcd6
Chris Anderson <canderson@connectivedx.com>
2017-07-21 10:58:24 -0700

    Revert "Revert "DXP-493: CustomerStoriesList component""
    This reverts commit d99bdc7c6109f996456d70c2be82c5d8a89c7d01.

---
+++ aem-project/core/src/main/java/com/bmc/components/CustomerStoryList.java
@@ -0,0 +1,36 @@
+package com.bmc.components;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+import com.bmc.mixins.*;
+import com.bmc.models.components.CustomerStoryCard;
+import com.bmc.models.components.CustomerStoryFilter;
+import com.bmc.models.components.FeaturedCustomerStoryCard;
+import com.bmc.services.CustomerStoryService;
+
+import java.util.List;
+
+public class CustomerStoryList extends WCMUsePojo
+        implements AdaptableResourceProvider, ModelFactory, MultifieldDataProvider
+{
+    private CustomerStoryService storyService;
+    private FeaturedCustomerStoryCard featuredStory;
+    private List<CustomerStoryCard> stories;
+    private List<CustomerStoryFilter> filters;
+
+    public List<CustomerStoryFilter> getFilters() { return filters; }
+    public FeaturedCustomerStoryCard getFeaturedStory() { return featuredStory; }
+    public List<CustomerStoryCard> getStories() { return stories; }
+
+    @Override
+    public void activate() throws Exception {
+        storyService = getSlingScriptHelper().getService(CustomerStoryService.class);
+
+        filters = storyService.getFilters(this);
+        featuredStory = storyService.getFeaturedStoryCard(
+                getProperties().get("featuredStoryPath", ""),
+                getProperties().get("featuredStoryBgImgSrc", ""),
+                this);
+        stories = mapMultiFieldJsonObjects("stories",
+                map -> storyService.getStoryCard(map.get("pagePath", ""), this));
+    }
+}
