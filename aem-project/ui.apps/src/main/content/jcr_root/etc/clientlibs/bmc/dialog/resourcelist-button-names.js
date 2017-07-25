b2831b6276dfb7bd0be45424197911049c85e0ab
Eaton Lambert <elambert@connectivedx.com>
2017-07-25 02:57:53 -0700

    DXP-814-ResourcesList-Fixes

--- aem-project/ui.apps/src/main/content/jcr_root/etc/clientlibs/bmc/dialog/resourcelist-button-names.js
+++ aem-project/ui.apps/src/main/content/jcr_root/etc/clientlibs/bmc/dialog/resourcelist-button-names.js
@@ -8,7 +8,7 @@
     // when dialog gets injected
     $(document).on("foundation-contentloaded", function(e) {
         // if there is already an inital value make sure the according target element becomes visible
-        console.log($(".bmc-cq-dialog-add-heading-name > .coral-Multifield-add").html($(".bmc-cq-dialog-add-heading-name").attr( 'data-label' )));
-        console.log($(".bmc-cq-dialog-add-resource-name > .coral-Multifield-add").html($(".bmc-cq-dialog-add-resource-name").attr( 'data-label' )));
+        $(".bmc-cq-dialog-add-heading-name > .coral-Multifield-add").html($(".bmc-cq-dialog-add-heading-name").attr( 'data-label' ));
+        $(".bmc-cq-dialog-add-resource-name > .coral-Multifield-add").html($(".bmc-cq-dialog-add-resource-name").attr( 'data-label' ));
     });
 })(document,Granite.$);
