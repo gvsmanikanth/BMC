7fca84ca2eb7f79994695f5419e7d0b02283ea45
dolsen <dolsen@connectivedx.com>
2017-07-27 14:47:40 -0700

    Adds width attribute to HeaderBand play icon

--- front-end/Assets/src/templates/php-inc/pattern-library/HTML/header-about-us.php
+++ front-end/Assets/src/templates/php-inc/pattern-library/HTML/header-about-us.php
@@ -5,7 +5,7 @@
     			<div class="offset-hero-medium-inner-wrap">
 					<h1 class="white">How we're<br> revolutionizing IT</h1>
 						<a href="/templates/Media_Video_Mobile?vID=5eceeee45f5c0" class="lead -sm bold white yellow-orange-hover inline-block modal-video-player" title="Transforming the Digital Enterprise">Watch the short video on IT Innovation (1:17)&nbsp;&nbsp;
-							<svg class="align-middle fill-white" height="1.2em" viewBox="0 0 100 100">
+							<svg class="align-middle fill-white" width="1.2em" height="1.2em" viewBox="0 0 100 100">
 								<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#s-VideoPlay"></use>
 							</svg></a>
     			</div>
