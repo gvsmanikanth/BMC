84c505089def3f66f578ff4725029b4821a38736
Chris Anderson <canderson@connectivedx.com>
2017-07-31 08:03:36 -0700

    adds a simple, reusable datasource for granite/ui/components/foundation/form/select fields
    see SelectOptionsDataSource javadoc

--- aem-project/core/src/main/java/com/bmc/util/StringHelper.java
+++ aem-project/core/src/main/java/com/bmc/util/StringHelper.java
@@ -1,7 +1,13 @@
 package com.bmc.util;
 
+import org.apache.sling.api.resource.ValueMap;
+
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public interface StringHelper {
     /**
@@ -45,17 +51,37 @@ public interface StringHelper {
      */
     @SafeVarargs
     static <T> Optional<String> coalesceStringMember(final T context, final Function<T, String>...getItems) {
-        if (context != null) {
-            for (Function<T, String> item : getItems) {
-                if (item == null)
-                    continue;
-                String str = item.apply(context);
-                if (str == null || str.isEmpty())
-                    continue;
-                return Optional.of(str);
-            }
-        }
+        return coalesceStringMember(context, Arrays.stream(getItems));
+    }
 
-        return Optional.empty();
+    /**
+     * For the given object instance {@code context}, returns the first result from {@code getItems} which is not null or empty.<br><br>
+     *
+     * Useful to avoid multiple String lookups when unnecessary and expensive
+     * @param context the object instance to pass to {@code getItems}
+     * @param getItems the mapping function(s) generating string values from {@code context}
+     * @param <T> the type of the {@code context} instance
+     * @see #coalesceStringMember(Object, Function[])
+     * @return the first result from {@code getItems} which is not null or empty
+     */
+    static <T> Optional<String> coalesceStringMember(final T context, final Stream<Function<T, String>> getItems) {
+        return getItems.map(getItem -> getItem.apply(context))
+                .filter(str->str != null && !str.isEmpty())
+                .findFirst();
+    }
+
+    /**
+     * For the given {@code map}, returns the first value mapped from {@code keys} which is not null or empty.
+     * @param map the {@link ValueMap} instance
+     * @param keys the {@link ValueMap} keys to test values for
+     * @return the first value mapped from {@code keys} which is not null or empty
+     */
+    static Optional<String> coalesceStringValue(final ValueMap map, final String...keys) {
+        return coalesceStringMember(map, Arrays.stream(keys)
+                .map(key-> new Function<ValueMap, String>() {
+                    public String apply(ValueMap valueMap) {
+                        return valueMap.get(key, String.class);
+                    }
+                }));
     }
 }
