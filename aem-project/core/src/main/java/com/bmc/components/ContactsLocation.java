0124a3bff1386235e8b2e673771d1b9e7b1bab2b
Peter Heide <pheide@connectivedx.com>
2017-08-04 16:15:43 -0700

    Building single location dialog.

---
+++ aem-project/core/src/main/java/com/bmc/components/ContactsLocation.java
@@ -0,0 +1,12 @@
+package com.bmc.components;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+
+/**
+ * Created by pheide on 8/3/17.
+ */
+public class ContactsLocation extends WCMUsePojo {
+
+    @Override
+    public void activate() throws Exception { }
+}
