e6e222e906bc14e43d3066b1aadf6f4ccbd1dad0
Peter Heide <pheide@connectivedx.com>
2017-08-08 14:53:22 -0700

    ContactsLocations model.

--- aem-project/core/src/main/java/com/bmc/components/ContactsLocation.java
+++ aem-project/core/src/main/java/com/bmc/components/ContactsLocation.java
@@ -1,12 +1,25 @@
 package com.bmc.components;
 
 import com.adobe.cq.sightly.WCMUsePojo;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
+
 
 /**
  * Created by pheide on 8/3/17.
  */
 public class ContactsLocation extends WCMUsePojo {
 
+    private ContactsLocations parentResource;
+
     @Override
-    public void activate() throws Exception { }
+    public void activate() throws Exception {
+//        parentResource = getCurrentPage().getParent().getContentResource("root/maincontentcontainer/responsivegrid/contacts_locations").adaptTo(ContactsLocations.class);
+    }
+
+    public String getTest() {
+        parentResource = getCurrentPage().getParent().getContentResource("root/maincontentcontainer/responsivegrid/contacts_locations").adaptTo(ContactsLocations.class);
+//        return getCurrentPage().getParent().getContentResource("root/maincontentcontainer/responsivegrid/contacts_locations").toString();
+        return parentResource.getSalesContactURL();
+    }
 }
