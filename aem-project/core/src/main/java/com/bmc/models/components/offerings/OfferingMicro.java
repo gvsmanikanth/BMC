28a93a5e9e1013f0e1b0e5399967931b65b30508
Eaton Lambert <elambert@connectivedx.com>
2017-08-07 17:05:57 -0700

    DXP-550 V3

---
+++ aem-project/core/src/main/java/com/bmc/models/components/offerings/OfferingMicro.java
@@ -0,0 +1,108 @@
+package com.bmc.models.components.offerings;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.PropertyIterator;
+import javax.jcr.Session;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+@Model(adaptables=Resource.class)
+public class OfferingMicro {
+    private static final Logger logger = LoggerFactory.getLogger(OfferingMicro.class);
+
+    @Inject
+    private Resource resource;
+
+    @Inject
+    private Session session;
+
+    @Inject @Named("resourcePath")
+    private String resourcePath;
+
+
+    private HashMap<String,String> resourceProps;
+
+    @PostConstruct
+    protected void init() {
+        try {
+            Node mainNode;
+            Node longDiscription;
+            mainNode = session.getNode(resourcePath+"/jcr:content/root/offer_item");
+            longDiscription = mainNode.getNode("productDiscription");
+
+            resourceProps = new HashMap<>();
+
+            if(mainNode.hasProperty("datasheetIconImage"))
+                resourceProps.put("datasheetIconImage",mainNode.getProperty("datasheetIconImage").getValue().getString());
+
+            if(mainNode.hasProperty("datasheetLink")) {
+                Node dataSheet =  session.getNode(mainNode.getProperty("datasheetPicker").getValue().getString()+"/jcr:content/metadata");
+                resourceProps.put("datasheetLink", dataSheet.getProperty("jcr:title").getValue().getString() != "" ? dataSheet.getProperty("jcr:title").getValue().getString() : mainNode.getProperty("datasheetLink").getValue().getString());
+            }
+
+            if(mainNode.hasProperty("datasheetPicker")) {
+                    resourceProps.put("datasheetPicker", mainNode.getProperty("datasheetPicker").getValue().getString());
+            }
+
+            if(mainNode.hasProperty("datasheetPrefix"))
+                resourceProps.put("datasheetPrefix",mainNode.getProperty("datasheetPrefix").getValue().getString());
+
+
+            if(mainNode.hasProperty("assetIconImage"))
+                resourceProps.put("assetIconImage",mainNode.getProperty("assetIconImage").getValue().getString());
+
+            if(mainNode.hasProperty("assetLinkText")) {
+                Node assetNode =  session.getNode(mainNode.getProperty("assetPicker").getValue().getString()+"/jcr:content/video-data");
+                resourceProps.put("assetLinkText", assetNode.getProperty("title").getValue().getString() != "" ? assetNode.getProperty("title").getValue().getString() : mainNode.getProperty("assetLinkText").getValue().getString());
+            }
+
+            if(mainNode.hasProperty("assetPrefix"))
+                resourceProps.put("assetPrefix",mainNode.getProperty("assetPrefix").getValue().getString());
+
+            if(mainNode.hasProperty("assetPicker"))
+                resourceProps.put("assetPicker",mainNode.getProperty("assetPicker").getValue().getString());
+
+
+
+
+            if(mainNode.hasProperty("primaryParentOfferingPage"))
+                resourceProps.put("primaryParentOfferingPage",mainNode.getProperty("primaryParentOfferingPage").getValue().getString());
+
+            if(mainNode.hasProperty("productAvailability"))
+                resourceProps.put("productAvailability",mainNode.getProperty("productAvailability").getValue().getString());
+
+            if(mainNode.hasProperty("productAvailabilityListPicker"))
+                resourceProps.put("productAvailabilityListPicker",mainNode.getProperty("productAvailabilityListPicker").getValue().getString());
+
+            if(mainNode.hasProperty("productName"))
+                resourceProps.put("productName",mainNode.getProperty("productName").getValue().getString());
+
+            if(mainNode.hasProperty("linkText"))
+                resourceProps.put("linkText",mainNode.getProperty("linkText").getValue().getString());
+
+            if(mainNode.hasProperty("shortDescription"))
+                resourceProps.put("shortDescription",mainNode.getProperty("shortDescription").getValue().getString());
+
+            if(longDiscription.hasProperty("text"))
+                resourceProps.put("longDiscription",longDiscription.getProperty("text").getValue().getString());
+
+
+        }catch (Exception e){
+            logger.error("ERROR: {}", e);
+        }
+    }
+
+    public HashMap<String, String> getResourceProps() {
+        return resourceProps;
+    }
+}
