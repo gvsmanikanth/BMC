ead8550e19ac9dcd7aaae58617db2a7a79fa6399
Eaton Lambert <elambert@connectivedx.com>
2017-08-10 13:08:30 -0700

    DXP-537 SwiftType

--- aem-project/core/src/main/java/com/bmc/models/components/search/SearchResultsModel.java
+++ aem-project/core/src/main/java/com/bmc/models/components/search/SearchResultsModel.java
@@ -1,22 +1,31 @@
 package com.bmc.models.components.search;
 
 import com.day.cq.wcm.api.Page;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.request.RequestParameterMap;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.jcr.Session;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 
-@Model(adaptables=Resource.class)
+@Model(adaptables={ SlingHttpServletRequest.class, Resource.class })
 public class SearchResultsModel {
 
     private static final Logger logger = LoggerFactory.getLogger(SearchResultsModel.class);
 
     @Inject
     private Session session;
+
+    @Self
+    private SlingHttpServletRequest httpServletRequest;
 
     @Inject
     private Page resourcePage;
@@ -28,12 +37,9 @@ public class SearchResultsModel {
         try {
            // Node currentNode = request.getResource().adaptTo(Node.class);
             logger.info("SEARCHING");
-            pageLocale = formatMetaLocale();
+            pageLocale = formatMetaLocale().toLowerCase();
         } catch (Exception e) {
             logger.error(e.getMessage());
-        } finally {
-           // if (session != null && session.isLive())
-            //    session.logout();
         }
     }
 
@@ -47,5 +53,17 @@ public class SearchResultsModel {
 
     public String getPageLocale() {
         return pageLocale;
+    }
+
+
+    public Map getParameterMap() {
+        Map<String, Object> map = new HashMap<>();
+        Map pamaMap = httpServletRequest.getParameterMap();
+        Set<String> keySet = pamaMap.keySet();
+        List<String> keys = new ArrayList<>(keySet);
+        keys.forEach(s -> {
+            map.put(s, pamaMap.get(s));
+        });
+        return map;
     }
 }
