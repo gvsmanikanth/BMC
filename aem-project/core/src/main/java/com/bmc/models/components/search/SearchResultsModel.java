8739a389f52e404876187b818f6afe1c9d313b96
Eaton Lambert <elambert@connectivedx.com>
2017-08-11 12:21:54 -0700

    DXP-537 Class

--- aem-project/core/src/main/java/com/bmc/models/components/search/SearchResultsModel.java
+++ aem-project/core/src/main/java/com/bmc/models/components/search/SearchResultsModel.java
@@ -32,6 +32,8 @@ public class SearchResultsModel {
 
     private String pageLocale;
 
+    private HashMap<String, Object> queryHashMap = new HashMap<>();
+
     @PostConstruct
     protected void init() {
         try {
@@ -66,4 +68,64 @@ public class SearchResultsModel {
         });
         return map;
     }
+
+    public void getFilters(){
+
+        String labelParam = getParameterMap().get("label").toString();
+
+        String page = "page";
+        String fieldName = "source";
+        String type = "or";
+        List<String> values = new ArrayList<>();
+
+        HashMap<String, Object> valueMap = new HashMap<>();
+
+            if(labelParam.equals("news")){
+                values.add("newsroom");
+            }
+            else if(labelParam.equals("corporate")){
+                values.add(getPageLocale());
+            }
+            else if(labelParam.equals("communities")){
+                values.add("communities");
+            }
+            else if(labelParam.equals("documentation")){
+                values.add("docs");
+            }
+            else if(labelParam.equals("marketplace")){
+                values.add("productsSolutions");
+                fieldName = "category";
+            }
+            else if(labelParam.equals("productsSolutions")){
+                fieldName = "category";
+                values.add("marketplace");
+            }
+            else if(labelParam.equals("services")){
+                fieldName = "category";
+                values.add("services");
+            }
+            else if(labelParam.equals("education")){
+                fieldName = "category";
+                values.add("education");
+            }
+            else if(labelParam.equals("blogs")){
+                values.add("blogs");
+            }
+
+            valueMap.put("values",values);
+            valueMap.put("type",type);
+            setQueryParams(page, fieldName, valueMap);
+    }
+
+    private void setQueryParams(String page, String fieldName, HashMap valueMap){
+        HashMap<String, Object> queryHashMapItem = new HashMap<>();
+        List<HashMap> queryMap = new ArrayList<>();
+
+        queryHashMapItem.put("page",page);
+        queryHashMapItem.put("fieldName",fieldName);
+        queryHashMapItem.put("valueMap",valueMap);
+        queryMap.add(queryHashMapItem);
+
+    }
+
 }
