d224335e5c9d886a1af95ab297a9b48fd3387268
Eaton Lambert <elambert@connectivedx.com>
2017-08-25 13:58:01 -0700

    DXP-686 Tabs UUID hashmarks

--- aem-project/core/src/main/java/com/bmc/models/components/tabs/TabsModel.java
+++ aem-project/core/src/main/java/com/bmc/models/components/tabs/TabsModel.java
@@ -1,21 +1,13 @@
 package com.bmc.models.components.tabs;
 
-import com.bmc.models.components.forms.FormModel;
-import com.bmc.models.utils.ContentIdGenerator;
-import com.day.cq.wcm.api.Page;
-import com.sun.org.apache.regexp.internal.RE;
-import org.apache.felix.scr.annotations.sling.SlingFilter;
-import org.apache.jackrabbit.oak.commons.jmx.Name;
+
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Optional;
-import org.apache.sling.settings.SlingSettingsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.Resources;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.jcr.Node;
@@ -34,11 +26,16 @@ public class TabsModel {
     @Inject
     private Resource resource;
 
+    @Inject
+    private Session session;
+
     @Inject @Named("tabdata")
     @Optional
     private List<Resource> tabs;
 
     protected List<Resource> tabItems;
+
+    protected List<String> tabID;
 
 
     @PostConstruct
@@ -46,9 +43,21 @@ public class TabsModel {
         try {
             Iterator<Resource> tabsSections = resource.getChildren().iterator();
             tabItems = new ArrayList<>();
+            tabID = new ArrayList<>();
             while(tabsSections.hasNext()){
                 Resource tabResource = tabsSections.next();
                 if((tabResource.getResourceType() != null) && (tabResource.getResourceType().equals("bmc/components/content/section"))) {
+                    Node tabNode = tabResource.adaptTo(Node.class);
+                    if(!tabNode.hasProperty("jcr:mixinTypes")){
+                        tabNode.addMixin("mix:referenceable");
+                    }
+                    if(!tabNode.hasProperty("tabID")) {
+                        tabID.add(tabResource.getValueMap().get("jcr:uuid").toString());
+                        tabNode.setProperty("tabID",tabResource.getValueMap().get("jcr:uuid").toString());
+                        session.save();
+                    }else{
+                        tabID.add(tabNode.getProperty("tabID").getValue().getString());
+                    }
                     tabItems.add(tabResource);
                 }
             }
@@ -57,10 +66,8 @@ public class TabsModel {
         }
     }
 
-    public String getContentID(){
-        ContentIdGenerator contentIdGenerator = new ContentIdGenerator(resource.getPath());
-        return contentIdGenerator.getNewContentID();
-
+    public List getTabID() {
+        return tabID;
     }
 
     public Iterable<Resource> getTabs() {
