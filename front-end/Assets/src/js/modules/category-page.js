6a38b174bb51429589f46f8cbedd891c060047a8
Kadam, Shailesh <shailesh_kadam@bmc.com>
2017-08-30 20:59:44 +0530

    DXP-1064 : Front-end - Add support for relative path to secondary nav -> float-rail-nav - Added ".float-rail-nav a" selector to add current class using front-end. - Added ".float-rail-nav li" selector to add current class using front-end. - Updated getPageName function to return page name for passed page path - Change logic for current page comparison to support relative path

--- front-end/Assets/src/js/modules/category-page.js
+++ front-end/Assets/src/js/modules/category-page.js
@@ -3,28 +3,48 @@
 	//DXP-157: Cleanup guide pages for category.
 	
 	if ($("body").hasClass("page-category") == true) {
-	 	  $('.itil-nav a').each(function() {
-		    if (jQuery(this).attr('href')  ===  window.location.pathname) {
+	 	  
+		  //DXP-1064: Added ".float-rail-nav a" selector to add current class using front-end.
+		  
+		  $('.itil-nav a, .float-rail-nav a').each(function() {
+			
+			var link_href = jQuery(this).attr('href');
+			
+		    if (getPageName(link_href)  ===  getPageName(window.location.pathname)) {
 		      jQuery(this).addClass('current');
 		    }
 		  });
 		  
-		  $('.itil-nav li').each(function() {
-		    var href = jQuery(this).find('a').attr('href');
-		    if (href === window.location.pathname) {
-		      jQuery(this).addClass('current');
-		    }
-		  });
 		  
-		  this.getPageName = function() {
-			    var index = window.location.href.lastIndexOf("/") + 1,
-			        filenameWithExtension = window.location.href.substr(index),
+		  //DXP-1064: Added ".float-rail-nav li" selector to add current class using front-end.
+		  
+		  $('.itil-nav li, .float-rail-nav li').each(function() {
+			var full_href = jQuery(this).find('a').attr('href');
+			
+			if(full_href){
+			    var href = getPageName(full_href);
+			    if (href === getPageName(window.location.pathname)) {
+			      jQuery(this).addClass('current');
+			    }
+			}
+			
+		  });	  
+		  
+		 //DXP-1064: Updated function to return page name for current page comparison.
+		  
+		  function getPageName (pPath) {
+			  if(pPath){
+			    var index = pPath.lastIndexOf("/") + 1,
+			        filenameWithExtension = pPath.substr(index),
 			        filename = filenameWithExtension.split(".")[0];  
-
-			    return filename;                                     
-			};
+			    return filename;
+			  }
+			  else{
+				return "";  
+			  }
+		  };
 		  
-		  var pageName = "[data-showon='"+getPageName()+"']";
+		  var pageName = "[data-showon='"+getPageName(window.location.href)+"']";
 		  $(pageName).show();
 	
 		  $(document).scroll(function(){	
