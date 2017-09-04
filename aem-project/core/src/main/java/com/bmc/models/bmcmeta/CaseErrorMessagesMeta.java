0e82912f203a685b99a383ffcb8d98f0eed1e2f8
Bryan Ledford <git@bryanledford.com>
2017-09-04 11:29:38 -0700

    DXP-1018 Support Central updates

--- aem-project/core/src/main/java/com/bmc/models/bmcmeta/CaseErrorMessagesMeta.java
+++ aem-project/core/src/main/java/com/bmc/models/bmcmeta/CaseErrorMessagesMeta.java
@@ -5,9 +5,9 @@ package com.bmc.models.bmcmeta;
  */
 public class CaseErrorMessagesMeta {
     private final String NOT_FOUND = "<p>You don't have any open cases.</p>";
-    private final String ERROR = "Appropriate SupportMeta ID is required. Please edit your profile by selecting 'Edit Profile' or chat with a technical representative by selecting 'Need Help?'";
-    private final String DEFAULT_ERROR_MESSAGE = "Appropriate SupportMeta ID is required. Please edit your profile by selecting 'Edit Profile' or chat with a technical representative by selecting 'Need Help?'";
-    private final String ERROR_TIMEOUT = "We are unable to display Cases for you at this time. If you have subscribed to SupportMeta, click the View All Cases button to access your Cases. If you have not yet subscribed, you may edit your profile using the link above, or chat with Customer Care by clicking 'Need Help?'";
+    private final String ERROR = "Appropriate Support ID is required. Please edit your profile by selecting 'Edit Profile' or chat with a technical representative by selecting 'Need Help?'";
+    private final String DEFAULT_ERROR_MESSAGE = "Appropriate Support ID is required. Please edit your profile by selecting 'Edit Profile' or chat with a technical representative by selecting 'Need Help?'";
+    private final String ERROR_TIMEOUT = "We are unable to display Cases for you at this time. If you have subscribed to Support, click the View All Cases button to access your Cases. If you have not yet subscribed, you may edit your profile using the link above, or chat with Customer Care by clicking 'Need Help?'";
 
     public String getNOT_FOUND() {
         return NOT_FOUND;
