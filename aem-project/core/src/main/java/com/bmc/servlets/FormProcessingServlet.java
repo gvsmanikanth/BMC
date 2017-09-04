eea8679042e5e4361edb26f1d9e0acf576a14694
Bryan Ledford <git@bryanledford.com>
2017-09-04 01:13:33 -0700

    DXP-793 business automation email and config

--- aem-project/core/src/main/java/com/bmc/servlets/FormProcessingServlet.java
+++ aem-project/core/src/main/java/com/bmc/servlets/FormProcessingServlet.java
@@ -1,6 +1,7 @@
 package com.bmc.servlets;
 
 import com.adobe.acs.commons.email.EmailService;
+import com.bmc.services.FormProcessingXMLService;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
 import org.apache.felix.scr.annotations.Activate;
@@ -19,6 +20,7 @@ import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.servlet.ServletException;
+import javax.xml.bind.Element;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -41,6 +43,9 @@ public class FormProcessingServlet extends SlingAllMethodsServlet {
     private String serviceUrl = "";
     private String elqSiteID = "";
     private int timeout = 5000;
+
+    private Boolean automationEmailEnabled = false;
+    private String[] automationEmailRecipients;
 
     private Session session;
 
@@ -98,6 +103,8 @@ public class FormProcessingServlet extends SlingAllMethodsServlet {
         serviceUrl = PropertiesUtil.toString(config.get("serviceUrl"), null);
         elqSiteID = PropertiesUtil.toString(config.get("elqSiteID"), "");
         timeout = PropertiesUtil.toInteger(config.get("timeout"), 5000);
+        automationEmailEnabled = PropertiesUtil.toBoolean(config.get("automationEmailEnabled"), false);
+        automationEmailRecipients = PropertiesUtil.toStringArray(config.get("automationEmailRecipients"));
     }
 
     @Override
@@ -117,18 +124,26 @@ public class FormProcessingServlet extends SlingAllMethodsServlet {
         String data = prepareFormData(formData, formProperties);
         logger.trace("Encoded Form Data: " + data);
         String formType = (String) formProperties.getOrDefault("formType", "Lead Capture");
+        int status = 0;
         switch (formType) {
             case "Lead Capture":
-                sendData(data);
+                status = sendData(data);
                 break;
             case "Parallel":
-                sendData(data);
-                sendParallelEmail(formData, formProperties, formPage, request);
+                status = sendData(data);
+                sendParallelEmail(formData, formProperties, formPage, request, status);
                 break;
             case "Email Only":
                 sendBasicEmail(formData, formProperties, formPage, request);
                 break;
         }
+        String xml = "";
+        try {
+            xml = FormProcessingXMLService.getFormXML(formData, formProperties, request, serviceUrl);
+        } catch (Exception e) {
+            logger.error("Error getting XML for form email automation.");
+        }
+        sendAutomationEmail(xml, status);
         if (purlPage != null) {
             PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
             Page page = pageManager.getPage(purlPage);
@@ -142,7 +157,22 @@ public class FormProcessingServlet extends SlingAllMethodsServlet {
         }
     }
 
-    private void sendBasicEmail(Map<String, String> formData, Map formProperties, Page formPage, SlingHttpServletRequest request) {
+    private void sendAutomationEmail(String xml, int status) {
+        logger.info(xml);
+        if (automationEmailEnabled) {
+            Map<String, String> emailParams = new HashMap<>();
+            String templatePath = "/etc/notification/email/text/plaintext.txt";
+            emailParams.put("fromAddress", FROM_ADDRESS);
+            String subject = (status == 200 || status == 302)
+                    ? "Form Submission Success"
+                    : "Form Submission Failure";
+            emailParams.put("subject", subject);
+            emailParams.put("body", xml);
+            emailService.sendEmail(templatePath, emailParams, automationEmailRecipients);
+        }
+    }
+
+    private void sendBasicEmail(Map<String, String> formData, Map<String, String> formProperties, Page formPage, SlingHttpServletRequest request) {
         String templatePath = "/etc/notification/email/html/form-emailonly.html";
         String recipient = getRecipient(formProperties);
         if (recipient.isEmpty()) return;
@@ -153,12 +183,14 @@ public class FormProcessingServlet extends SlingAllMethodsServlet {
         StringBuilder body = new StringBuilder("<h2>Form Data</h2><br/>");
         String[] honeypotFields = {"Address3", ":cq_csrf_token", "Surname", "wcmmode"};
         formData.forEach((k,v) -> body.append((!Arrays.asList(honeypotFields).contains(k)) ? "<strong>" + k + "</strong>: " + v + "<br/>" : ""));
+        body.append("<h2>Event Specific Information</h2>");
+        body.append("<p><strong>").append(formPage.getTitle()).append("</strong></p>");
         getCommonEmailBody(formProperties, formPage, request, body);
         emailParams.put("body", body.toString());
         emailService.sendEmail(templatePath, emailParams, recipients);
     }
 
-    private void sendParallelEmail(Map<String, String> formData, Map formProperties, Page formPage, SlingHttpServletRequest request) {
+    private void sendParallelEmail(Map<String, String> formData, Map formProperties, Page formPage, SlingHttpServletRequest request, int status) {
         String templatePath = "/etc/notification/email/html/form-emailonly.html";
         String recipient = getRecipient(formProperties);
         if (recipient.isEmpty()) return;
@@ -170,6 +202,10 @@ public class FormProcessingServlet extends SlingAllMethodsServlet {
         String[] honeypotFields = {"Address3", ":cq_csrf_token", "Surname", "wcmmode"};
         formData.forEach((k,v) -> body.append((!Arrays.asList(honeypotFields).contains(k)) ? "<strong>" + k + "</strong>: " + v + "<br/>" : ""));
         formProperties.forEach((k,v) -> body.append((!Arrays.asList(honeypotFields).contains(k)) ? "<strong>" + k + "</strong>: " + v + "<br/>" : ""));
+        body.append("<h2>Event Specific Information</h2>");
+        body.append("<p><strong>").append(formPage.getTitle()).append("</strong></p>");
+        body.append("<p>Webmethods Response Code: ").append(status).append("</p>");
+        body.append("<p>Service URL: ").append(serviceUrl).append("</p>");
         getCommonEmailBody(formProperties, formPage, request, body);
         emailParams.put("body", body.toString());
         emailService.sendEmail(templatePath, emailParams, recipients);
@@ -192,8 +228,6 @@ public class FormProcessingServlet extends SlingAllMethodsServlet {
     }
 
     private void getCommonEmailBody(Map formProperties, Page formPage, SlingHttpServletRequest request, StringBuilder body) {
-        body.append("<h2>Event Specific Information</h2>");
-        body.append("<p><strong>").append(formPage.getTitle()).append("</strong></p>");
         body.append("<p>").append(new SimpleDateFormat("EEE MMM d HH:mm:ss z YYYY").format(new Date())).append("</p>");
         body.append("<h2>System Information</h2>");
         body.append("<h3>Content</h3>");
@@ -219,7 +253,7 @@ public class FormProcessingServlet extends SlingAllMethodsServlet {
         body.append("</dl>");
     }
 
-    private void sendData(String data) {
+    private int sendData(String data) {
         logger.trace("Sending Data: " + serviceUrl);
         String charset = StandardCharsets.UTF_8.name();
         HttpURLConnection connection = null;
@@ -273,6 +307,7 @@ public class FormProcessingServlet extends SlingAllMethodsServlet {
             if (connection != null) {
                 connection.disconnect();
             }
+            return status;
         }
     }
 
