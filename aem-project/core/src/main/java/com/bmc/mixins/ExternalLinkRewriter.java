0f253bd2781a857669b89bb3792e5afdb823b179
SamikshaAnvekar <samiksha_anvekar@bmc.com>
2017-09-06 18:43:09 +0530

    Changes added for performance issues
    Added a new TransformerFactory class.

--- aem-project/core/src/main/java/com/bmc/mixins/ExternalLinkRewriter.java
+++ aem-project/core/src/main/java/com/bmc/mixins/ExternalLinkRewriter.java
@@ -41,143 +41,24 @@ public class ExternalLinkRewriter implements TransformerFactory {
 	/** Default log. */
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
        
-  //Inject a Sling ResourceResolverFactory
-  	@Reference
-  	private ResourceResolverFactory resolverFactory;
-  	
-  	@Reference
-    private ExternalLinkRewriterService dataService;
-  	
-  	ExternalLinkNodeItem item ;
-  	
-  	 long millisEnd;
-  	
-  	 long millisStart;
-  	
-    public Transformer createTransformer() {
+    @Reference
+    private ExternalLinkRewriterService service;
+   
+    public final Transformer createTransformer() {
     	
-        return new ExternalLinkTransformer();
+        return new ExternalLinkTransformer(service);
     }
-
+    
     @Activate
     protected void activate(Map<String, Object> properties) {
-    	logger.info("Activated the ExternalLinkResolver");
+    	logger.info("Activated the ExternalLinkRewriter");
+    	
     }
 
     @Deactivate
     protected void deactivate(ComponentContext ctx) {
     }
 
-    private class ExternalLinkTransformer implements Transformer {
-    	/** Default log. */
-        protected final Logger log = LoggerFactory.getLogger(this.getClass());
-    	
-        private ContentHandler contentHandler;
-        private SlingHttpServletRequest request;
-
-        
-        public void characters(char[] ch, int start, int length) throws SAXException {
-            contentHandler.characters(ch, start, length);
-        }
-
-        public void dispose() {
-        }
-
-        public void endDocument() throws SAXException {
-        	long millisEnd = Calendar.getInstance().getTimeInMillis();       	
-            log.info("END DOCUMENT SCAN : "+millisEnd);
-            long timeLapsed = (long) (millisEnd - millisStart * 0.001);
-            log.info("Time lapsed in the operation" +timeLapsed);
-            contentHandler.endDocument();
-        }
-
-        public void endElement(String uri, String localName, String qName) throws SAXException {
-            contentHandler.endElement(uri, localName, qName);
-        }
-
-        public void endPrefixMapping(String prefix) throws SAXException {
-            contentHandler.endPrefixMapping(prefix);
-        }
-
-        public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
-            contentHandler.ignorableWhitespace(ch, start, length);
-        }
-
-        public void init(ProcessingContext context, ProcessingComponentConfiguration config) throws IOException {
-            request = context.getRequest();
-          
-        }
-
-        public void processingInstruction(String target, String data) throws SAXException {
-            contentHandler.processingInstruction(target, data);
-        }
-
-        public void setContentHandler(ContentHandler handler) {
-            this.contentHandler = handler;
-        }
-
-        public void setDocumentLocator(Locator locator) {
-            contentHandler.setDocumentLocator(locator);
-        }
-
-        public void skippedEntity(String name) throws SAXException {
-            contentHandler.skippedEntity(name);
-        }
-
-        public void startDocument() throws SAXException {
-        	long millisStart = Calendar.getInstance().getTimeInMillis();
-        	logger.info("START DOCUMENT SCAN : "+millisStart);
-            contentHandler.startDocument();
-        }
-
-        public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException
-        {        	
-        	 
-        		//Parsing the HTML for hrefs 
-            	ExternalLinkNodeItem externalNodeItem = null;
-            	final AttributesImpl attributes = new AttributesImpl(atts);
-            	final String href = attributes.getValue("href");
-            	//service reference method for JCR lookup.
-            	
-            	externalNodeItem = dataService.checkJcrRepository(href);
-                for (int i = 0; i < attributes.getLength(); i++) {                	
-                	if("href".equalsIgnoreCase(attributes.getQName(i)) && !attributes.getValue(attributes.getQName(i)).equals("#")
-                			&& !attributes.getValue(attributes.getQName(i)).equals("/") )
-                    {
-                		String hrefName = attributes.getValue(attributes.getQName(i));
-                		log.info("Href name  : "+hrefName);
-                        	if (externalNodeItem.getLinkAbstractorURL()!=null  
-                        		&& externalNodeItem.getLinkPath().equals(hrefName) 
-                        		&& externalNodeItem.getLinkAbstractor().equalsIgnoreCase("external-link")) 
-                        			{
-                        				log.info("EXTERNAL LINK FOUND");
-                        				attributes.setValue(i, externalNodeItem.getLinkAbstractorURL());
-                        				//Condition to check the attribute target=_blank/_self in the href tag.
-                        				if(externalNodeItem.getLinkAbstractorTarget().equals("new"))
-                        					{
-                        						log.info("NEW LINK FOUND");
-                        						attributes.addAttribute(null, "target", "target", null, "_blank");
-                        	
-                        					}else if(externalNodeItem.getLinkAbstractorTarget().equals("self"))
-                        					{
-                        						log.info("SELF LINK FOUND");
-                        						attributes.addAttribute(null, "target", "target", null, "_self");                       	
-                        					}           			
-                        			}                       
-                        		break;
-                    	}	
-                }
-                		externalNodeItem =  null;
-                		contentHandler.startElement(uri, localName, qName, attributes);
-                		
-        }
-
-        public void startPrefixMapping(String prefix, String uri) throws SAXException {
-            contentHandler.startPrefixMapping(prefix, uri);
-        }
-        
-      
-    }
     
     
    
