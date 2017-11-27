7232bd02f591ca8d7d99effab7655a248fe75786
ADPROD\sanvekar <samiksha_anvekar@bmc.com>
2017-11-27 18:41:09 +0530

    DXP-769 NEW - Scheduler for fetching the on gig data from the service.

---
+++ aem-project/core/src/main/java/com/bmc/components/CareersLocationItem.java
@@ -0,0 +1,45 @@
+package com.bmc.components;
+
+public class CareersLocationItem {
+
+	private String countryURL;
+	private String countryName;
+	private String number_of_jobs;
+	private String continent;
+	
+	public CareersLocationItem(String countryURL, String countryName, String number_of_jobs, String continent)
+	{
+		this.countryURL = countryURL;
+		this.countryName = countryName;
+		this.number_of_jobs = number_of_jobs;
+		this.continent = continent;
+    }
+	public String getCountryURL() {
+		return countryURL;
+	}
+	public void setCountryURL(String countryURL) {
+		this.countryURL = countryURL;
+	}
+	public String getCountryName() {
+		return countryName;
+	}
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+	public String getNumber_of_jobs() {
+		return number_of_jobs;
+	}
+	public void setNumber_of_jobs(String number_of_jobs) {
+		this.number_of_jobs = number_of_jobs;
+	}
+	public String getContinent() {
+		return continent;
+	}
+	public void setContinent(String continent) {
+		this.continent = continent;
+	}
+	
+	
+}
+
+
