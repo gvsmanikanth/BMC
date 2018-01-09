938f1d0be1f858d3dc8dcef02612a23e4dd75415
Kadam, Shailesh <shailesh_kadam@bmc.com>
2018-01-09 15:56:38 +0530

    WEB-2671 : Product/Category New Headers
    Added pattern for new product/Category Header Component.

---
+++ front-end/Assets/src/templates/php-inc/header-product-category.php
@@ -0,0 +1,398 @@
+
+<!-- First -->
+
+<section class="wallpapered bar full-bleed-page-banner product-category-header padding-bottom-0" 
+	data-wallpaper-options="{&quot;source&quot;:&quot;//www.bmc.com/content/dam/bmc/images/ControlMTest_ProductPages_IMG_Marquee_9_8_17.png&quot;}">
+
+	<div class="offset-wrapper">
+		<div class="layout-inner-wrap py3">
+			<h1 class="white">Control-M</h1>
+			<h2>Power digital business automation: Go beyond traditional workload
+				automation</h2>
+			<div class="text-center mobile-only readability-large">
+				<a class="btn-secondary"
+					href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html"
+					data-form="" data-location="Top-CTA-Button-Area"
+					data-parent-id="259175041" target="_self">Start a free trial of
+					Control-M &nbsp;&#8250; </a>
+			</div>
+
+			<div class="text-center desktop-only">
+				<a class="btn-secondary mb3"
+					href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html"
+					data-form="" data-location="Top-CTA-Button-Area"
+					data-parent-id="259175041" target="_self">Start a free trial of
+					Control-M &nbsp;&#8250; </a>
+			</div>
+		</div>
+	</div>
+
+	<div class="assetStrip">
+		<div class="inner px1">
+			<div class="sm-flex flex-justify-center contentContainer">
+				<a href="http://www.bmc.com/info/LP-Demo-CTM.html" class="flex-item sm-col-4 mx2 shadow-top">
+				
+					<div class="inner">
+						<div class="sm-flex flex-center">
+							<div class="flex-item sm-col-2 assetImg">
+								<img
+									src="//www.bmc.com/content/dam/bmc/images/assetStrip_video-icon.png"
+									alt="" width="104" height="94">
+							</div>
+							<div class="flex-item sm-col-10 assetTxt">
+								<div class="inner">
+									<h5>Demo:</h5>
+									<p>
+										Customizable video
+									</p>
+									<p class="link">
+										Watch it
+											now&nbsp;&#8250;
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</a>
+				<a href="http://www.bmc.com/info/LP-Demo-CTM.html" class="flex-item sm-col-4 mx2 shadow-top">
+				
+					<div class="inner ">
+						<div class="sm-flex flex-center">
+							<div class="flex-item sm-col-2 assetImg">
+								<img
+									src="//www.bmc.com/content/dam/bmc/images/assetStrip_document-icon.png"
+									alt="" width="104" height="94">
+							</div>
+							<div class="flex-item sm-col-10 assetTxt">
+								<div class="inner">
+									<h5>Datasheet:</h5>
+									<p>
+										Control-M
+									</p>
+									<p class="link">
+										Download
+											now&nbsp;&#8250;
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</a>
+				<a href="http://www.bmc.com/info/LP-Demo-CTM.html" class="flex-item sm-col-4 mx2 no-border shadow-top">
+				
+					<div class="inner ">
+						<div class="sm-flex  flex-center">
+							<div class="flex-item sm-col-2  assetImg">
+								<img
+									src="//www.bmc.com/content/dam/bmc/images/forrester-tei-icon.png"
+									alt="" width="104" height="94">
+							</div>
+							<div class="flex-item sm-col-10 assetTxt">
+								<div class="inner">
+									<h5>Analyst report:</h5>
+									<p>
+										Forrester's Total Economic Impact Study
+									</p>
+									<p class="link learn-more">
+										Download the report
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+	<!--end new-->
+</section>
+
+
+						
+<section class="layout-full-bleed support-promo bg-mediumOrange product-category-promo">
+	<p class="align-center white promp-content">
+		Looking for OnDemand Services Support? 
+		<a class="learn-more white" href="http://i.onbmc.com"><strong>OnDemand Services Support</strong></a>
+	</p>
+</section>
+
+
+<!-- Second  -->
+
+<section class="wallpapered bar full-bleed-page-banner product-category-header padding-bottom-0" 
+	data-wallpaper-options="{&quot;source&quot;:&quot;//www.bmc.com/content/dam/bmc/images/ControlMTest_ProductPages_IMG_Marquee_9_8_17.png&quot;}">
+
+	<div class="offset-wrapper">
+		<div class="layout-inner-wrap py3">
+			<h1 class="white">Control-M</h1>
+			<h2>Power digital business automation: Go beyond traditional workload
+				automation</h2>
+			<div class="text-center mobile-only readability-large">
+				<a class="btn-secondary"
+					href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html"
+					data-form="" data-location="Top-CTA-Button-Area"
+					data-parent-id="259175041" target="_self">Start a free trial of
+					Control-M &nbsp;&#8250; </a>
+			</div>
+
+			<div class="text-center desktop-only">
+				<a class="btn-secondary mb3"
+					href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html"
+					data-form="" data-location="Top-CTA-Button-Area"
+					data-parent-id="259175041" target="_self">Start a free trial of
+					Control-M &nbsp;&#8250; </a>
+			</div>
+		</div>
+	</div>
+
+	<div class="assetStrip">
+		<div class="inner px1">
+			<div class="sm-flex flex-justify-center contentContainer">
+				<a href="http://www.bmc.com/info/LP-Demo-CTM.html" class="flex-item sm-col-4 mx2 shadow-top">
+				
+					<div class="inner">
+						<div class="sm-flex flex-center">
+							<div class="flex-item sm-col-4 assetImg text-right">
+								<img
+									src="//www.bmc.com/content/dam/bmc/images/assetStrip_video-icon.png"
+									alt="" >
+							</div>
+							<div class="flex-item sm-col-6 assetTxt">
+								<div class="inner">
+									<h5>Demo:</h5>
+									<p>
+										Customizable video
+									</p>
+									<p class="link">
+										Watch it
+											now&nbsp;&#8250;
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</a>
+				<a href="http://www.bmc.com/info/LP-Demo-CTM.html" class="flex-item sm-col-4 mx2 shadow-top">
+				
+					<div class="inner ">
+						<div class="sm-flex flex-center">
+							<div class="flex-item sm-col-6 assetImg text-right">
+								<img
+									src="//www.bmc.com/content/dam/bmc/images/assetStrip_document-icon.png"
+									alt="">
+							</div>
+							<div class="flex-item sm-col-6 assetTxt">
+								<div class="inner">
+									<h5>Datasheet:</h5>
+									<p>
+										Control-M
+									</p>
+									<p class="link">
+										Download
+											now&nbsp;&#8250;
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</a>
+				<a href="http://www.bmc.com/info/LP-Demo-CTM.html" class="flex-item sm-col-4 mx2 no-border shadow-top">
+				
+					<div class="inner ">
+						<div class="sm-flex flex-center">
+							<div class="flex-item sm-col-2 assetImg text-right">
+								<img
+									src="//www.bmc.com/content/dam/bmc/images/forrester-tei-icon.png"
+									alt="">
+							</div>
+							<div class="flex-item sm-col-10 assetTxt">
+								<div class="inner">
+									<h5>Analyst report:</h5>
+									<p>
+										Forrester's Total Economic Impact Study
+									</p>
+									<p class="link">
+										Download the report&nbsp;&#8250;
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+	<!--end new-->
+</section>
+
+
+						
+<section class="layout-full-bleed support-promo bg-mediumOrange product-category-promo">
+	<p class="align-center white promp-content">
+		Looking for OnDemand Services Support? 
+		<a class="learn-more white" href="http://i.onbmc.com"><strong>OnDemand Services Support</strong></a>
+	</p>
+</section>
+
+
+
+<section class="wallpapered bar full-bleed-page-banner product-category-header padding-bottom-0" 
+	data-wallpaper-options="{&quot;source&quot;:&quot;//www.bmc.com/content/dam/bmc/images/ControlMTest_ProductPages_IMG_Marquee_9_8_17.png&quot;}">
+
+	<div class="offset-wrapper">
+		<div class="layout-inner-wrap py3">
+			<h1 class="white">Control-M</h1>
+			<h2>Power digital business automation: Go beyond traditional workload
+				automation</h2>
+			<div class="text-center mobile-only readability-large">
+				<a class="btn-secondary"
+					href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html"
+					data-form="" data-location="Top-CTA-Button-Area"
+					data-parent-id="259175041" target="_self">Start a free trial of
+					Control-M &nbsp;&#8250; </a>
+			</div>
+
+			<div class="text-center desktop-only">
+				<a class="btn-secondary mb3"
+					href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html"
+					data-form="" data-location="Top-CTA-Button-Area"
+					data-parent-id="259175041" target="_self">Start a free trial of
+					Control-M &nbsp;&#8250; </a>
+			</div>
+		</div>
+	</div>
+
+	<div class="assetStrip">
+		<div class="inner px1">
+			<div class="sm-flex flex-justify-center contentContainer">
+				<a href="http://www.bmc.com/info/LP-Demo-CTM.html" class="flex-item sm-col-4 mx2 shadow-top">
+				
+					<div class="inner ">
+						<div class="sm-flex flex-center">
+							<div class="flex-item sm-col-2 assetImg">
+								<img
+									src="//www.bmc.com/content/dam/bmc/images/assetStrip_video-icon.png"
+									alt="" width="104" height="94">
+							</div>
+							<div class="flex-item sm-col-10 assetTxt">
+								<div class="inner">
+									<h5>Demo:</h5>
+									<p>
+										Customizable video
+									</p>
+									<p class="link">
+										Watch it
+											now&nbsp;&#8250;
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</a>
+				<a href="http://www.bmc.com/info/LP-Demo-CTM.html" class="flex-item sm-col-4 mx2 shadow-top">
+				
+					<div class="inner ">
+						<div class="sm-flex flex-center">
+							<div class="flex-item sm-col-2 assetImg">
+								<img
+									src="//www.bmc.com/content/dam/bmc/images/assetStrip_document-icon.png"
+									alt="" width="104" height="94">
+							</div>
+							<div class="flex-item sm-col-10 assetTxt">
+								<div class="inner">
+									<h5>Datasheet:</h5>
+									<p>
+										Control-M
+									</p>
+									<p class="link">
+										Download
+											now&nbsp;&#8250;
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</a>
+				
+			</div>
+		</div>
+	</div>
+	<!--end new-->
+</section>
+
+						
+<section class="layout-full-bleed support-promo bg-mediumOrange product-category-promo">
+	<p class="align-center white promp-content">
+		Looking for OnDemand Services Support? 
+		<a class="learn-more white" href="http://i.onbmc.com"><strong>OnDemand Services Support</strong></a>
+	</p>
+</section>
+
+
+<section class="wallpapered bar full-bleed-page-banner product-category-header padding-bottom-0" 
+	data-wallpaper-options="{&quot;source&quot;:&quot;//www.bmc.com/content/dam/bmc/images/ControlMTest_ProductPages_IMG_Marquee_9_8_17.png&quot;}">
+
+	<div class="offset-wrapper">
+		<div class="layout-inner-wrap py3">
+			<h1 class="white">Control-M</h1>
+			<h2>Power digital business automation: Go beyond traditional workload
+				automation</h2>
+			<div class="text-center mobile-only readability-large">
+				<a class="btn-secondary"
+					href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html"
+					data-form="" data-location="Top-CTA-Button-Area"
+					data-parent-id="259175041" target="_self">Start a free trial of
+					Control-M &nbsp;&#8250; </a>
+			</div>
+
+			<div class="text-center desktop-only">
+				<a class="btn-secondary mb3"
+					href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html"
+					data-form="" data-location="Top-CTA-Button-Area"
+					data-parent-id="259175041" target="_self">Start a free trial of
+					Control-M &nbsp;&#8250; </a>
+			</div>
+		</div>
+	</div>
+
+	<div class="assetStrip">
+		<div class="inner px1">
+			<div class="sm-flex flex-justify-center contentContainer">
+				<a href="http://www.bmc.com/info/LP-Demo-CTM.html" class="flex-item sm-col-4 mx2 shadow-top">
+				
+					<div class="inner ">
+						<div class="sm-flex flex-center">
+							<div class="flex-item sm-col-2 assetImg">
+								<img
+									src="//www.bmc.com/content/dam/bmc/images/assetStrip_video-icon.png"
+									alt="" width="104" height="94">
+							</div>
+							<div class="flex-item sm-col-10 assetTxt">
+								<div class="inner">
+									<h5>Demo:</h5>
+									<p>
+										Customizable video
+									</p>
+									<p class="link">
+										Watch it
+											now&nbsp;&#8250;
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</a>
+				
+			</div>
+		</div>
+	</div>
+	<!--end new-->
+</section>
+
+<section class="layout-full-bleed support-promo bg-mediumOrange product-category-promo">
+	<p class="align-center white promp-content">
+		Looking for OnDemand Services Support? 
+		<a class="learn-more white" href="http://i.onbmc.com"><strong>OnDemand Services Support</strong></a>
+	</p>
+</section>
+
---
+++ front-end/Assets/src/templates/product-category-header.php
@@ -0,0 +1,9 @@
+<?php
+	$pageTitle = 'Category (Functional)';
+	$bodyClass = 'section-products page-category';
+	include 'php-inc/head.php';
+?>
+
+<?php include 'php-inc/header-product-category.php';?>
+
+<?php include 'php-inc/foot.php'; ?>
