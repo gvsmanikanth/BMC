280afe482ad231d49e0608d9e46e646891d98190
Kadam, Shailesh <shailesh_kadam@bmc.com>
2018-01-29 20:02:37 +0530

    WEB-2671 - Navigation Update. Updated Navigation as per ticket description.

--- front-end/Assets/src/templates/php-inc/navigation-primary.php
+++ front-end/Assets/src/templates/php-inc/navigation-primary.php
@@ -1,62 +1,96 @@
 <nav class="layout-navigation">
 	<div class="layout-inner-wrap" style="position: relative;">
 		<ul class="navigation-primary">
-			<li class="navigation-menu">
-				<a href="#" class="navigation-menu-link"></a>
+			<li class="navigation-menu"><a href="#" class="navigation-menu-link"></a>
 			</li>
-			<li class="navigation-home">
-				<a href="#" class="navigation-home-link"></a>
+			<li class="navigation-home"><a href="#" class="navigation-home-link"></a>
 			</li>
+
+			<li class="navigation-products navigation-secondary-toggle"><a
+				href="#">Solutions</a>
+				<?php
+				
+include ('navigation-secondary-solutions-new.php')?>
+			</li>
+			<li class="navigation-industries navigation-secondary-toggle"><a
+				href="#">Products</a>
+				<?php
+				
+include ('navigation-secondary-products-new.php')?>
+			</li>
+
+			<li class="navigation-partners navigation-secondary-toggle"><a
+				href="#">Partners</a>
+				<?php
+				
+include ('navigation-secondary-patners-new.php')?>
+			</li>
+			<li class="navigation-partners navigation-secondary-toggle"><a
+				href="#">Support & Services</a>
+				<?php
+				
+include ('navigation-secondary-support-services-new.php')?>
+			</li>
+
+			<!-- 			
 			<li class="navigation-products navigation-secondary-toggle">
 				<a href="#">Solutions</a>
-				<?php include('navigation-secondary-products.php')
+				< ?php include('navigation-secondary-products.php')
 				?>
 			</li>
 			<li class="navigation-industries navigation-secondary-toggle">
 				<a href="#">Industries &amp; Roles</a>
-				<?php include('navigation-secondary-industry.php')
+				< ?php include('navigation-secondary-industry.php')
 				?>
 			</li>
 			<li class="navigation-services navigation-secondary-toggle">
 				<a href="#">Training &amp; Services</a>
-				<?php include('navigation-secondary-services.php')
+				< ?php include('navigation-secondary-services.php')
 				?>
 			</li>
 			<li class="navigation-education navigation-secondary-toggle">
 				<a href="#">Partners &amp; Developers</a>
-				<?php include('navigation-secondary-education.php')
+				< ?php include('navigation-secondary-education.php')
 				?>
 			</li>
 			<li class="navigation-partners navigation-secondary-toggle">
 				<a href="#">Communities</a>
-				<?php include('navigation-secondary-patners.php')
+				< ?php include('navigation-secondary-patners.php')
 				?>
 			</li>
 			<li class="navigation-community navigation-secondary-toggle">
 				<a href="#">Support</a>
-				<?php include('navigation-secondary-communities.php')
+				< ?php include('navigation-secondary-communities.php')
 				?>
-			<li class="navigation-about navigation-secondary-toggle">
-				<a href="#">About BMC</a>
-				<?php include('navigation-secondary-about.php')
-				?>
+			</li>	
+			
+ -->
+
+			<li class="navigation-about navigation-secondary-toggle"><a href="#">About
+					BMC</a>
+				<?php
+				
+include ('navigation-secondary-about-new.php')?>
 			</li>
 		</ul>
 		<ul class="navigation-utility">
-			<li class="navigation-trials ">
-				<a href="http://www.bmc.com/it-solutions/free-product-trials.html"  class="">Free Trials</a>
-			</li>
-			<li class="navigation-contact">
-				<a href="#" class="contact-bmc">Contact BMC</a>
-			</li>
-			<li class="navigation-country">
-				<a href="#" class="select-country">Worldwide (English)</a>
-			</li>
+			<li class="navigation-trials "><a
+				href="http://www.bmc.com/it-solutions/free-product-trials.html"
+				class="">Free Trials</a></li>
+			<li class="navigation-contact"><a href="#" class="contact-bmc">Contact
+					BMC</a></li>
+			<li class="navigation-country"><a href="#" class="select-country">Worldwide (English)</a></li>
+<!-- 			<li class="navigation-search-icon"><a href="#" class="select-search"></a> -->
+<!-- 			</li> -->
 			<li class="navigation-search js-navigation-search">
 				<form action="/search/" class="search-site">
 					<div class="search-site-b" dir="ltr">
 						<div class="search-focus">
-							<input maxlength="2048" name="q" autocapitalize="off" autocomplete="off" autocorrect="off" title="Search..." type="text" value="" aria-label="Search" aria-haspopup="false" role="combobox" aria-autocomplete="both" dir="ltr" spellcheck="false" placeholder="Search...">
+							<input maxlength="2048" name="q" autocapitalize="off"
+								autocomplete="off" autocorrect="off" title="Search..."
+								type="text" value="" aria-label="Search" aria-haspopup="false"
+								role="combobox" aria-autocomplete="both" dir="ltr"
+								spellcheck="false" placeholder="Search...">
 						</div>
 						<button></button>
 					</div>
@@ -64,9 +98,30 @@
 			</li>
 		</ul>
 		<!-- end chat now -->
-	</div><!-- / layout-inner-wrap -->
-</nav><!-- / layout-nav -->
+	</div>
+	<!-- / layout-inner-wrap -->
+</nav>
+<!-- / layout-nav 
 
+<div id="layout-inner-wrap" tabindex="0" style="position: absolute;top: 0px;left: 0px;width: 100%;background-color: #0009;height: 100%;z-index: 22;">
+	<div id="hf-search-box" style="/*! max-width:1440px; */ margin:auto;background-color: white;height: 100px;">
+		<div class="hf-centered-content" style="padding-top:1rem;padding-bottom:1rem;  max-width: 1440px;margin: auto;">
+			<div class="navigation-search js-navigation-search" style="width:90%;">
+				<form action="/search/" class="search-site">
+					<div class="search-site-b" dir="ltr" style="height:50px ;">
+						<div class="search-focus">
+							<input maxlength="2048" name="q" autocapitalize="off" autocomplete="off" autocorrect="off" title="Search..." value="" aria-label="Search" aria-haspopup="false" role="combobox" aria-autocomplete="both" dir="ltr" spellcheck="false" placeholder="Search..." type="text">
+						</div>
+						<button></button>
+					</div>
+				</form>
+			</div>
+			<a href="#" class="modal-close" style="float: left;height: 50px;width: 50px;position: relative;"></a>
+		</div>
+		
+	</div>
+</div>
+-->
 <!-- chat now -->
 <!-- 		<div class="fixed-tab-chat-now"> -->
 <!-- 			<a href="#" class="chat-now-link" id="fixed-tab-chat-now">Chat Now</a> -->
