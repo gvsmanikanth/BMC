96bb5fa1be5347184240a6aee29bea25a5e52bbc
Kadam, Shailesh <shailesh_kadam@bmc.com>
2018-02-27 16:16:45 +0530

    WEB-2081 - Animation RND

---
+++ front-end/Assets/src/templates/homepage-e.php
@@ -0,0 +1,310 @@
+<?php
+$pageTitle = 'BMC Home';
+$bodyClass = 'home';
+include 'php-inc/head.php';
+?>
+<?php
+include 'php-inc/svg.php';
+?>
+<style>
+
+body {
+  font-family: Helvetica,Tahoma;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+
+.aos-all {
+  width: 1000px;
+  max-width: 98%;
+  margin: 10vh auto 0 auto;
+}
+
+.aos-item {
+  display: inline-block;
+  float: left;
+  width: 33.3333%;
+  height: 300px;
+  padding: 20px;
+}
+
+.aos-item__inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  float: left;
+  background: #1da4e2;
+  line-height: 260px;
+  text-align: center;
+  color: #fff;
+}
+
+@media screen and (max-width: 800px) {
+  .aos-item {
+    width: 50%;
+  }
+}
+
+.main-content{
+	padding: 0 0 33px;
+    background: url("/bmc-dxp/front-end/Assets/dist/mainbannerimg.png") no-repeat;
+    width: 100%;
+    background-size: cover;
+}
+
+body{
+ 	background-color:white;
+}
+
+
+h2 {
+    font-size: 50px;
+    line-height: 58px;
+    color: #fe5000;
+    font-weight: 300;
+    font-family: 'Open Sans', sans-serif;
+    letter-spacing: 0.5px;
+}
+
+p {
+    font-size: 24px;
+    line-height: 48px;
+    color: #414042;
+    font-weight: 400;
+    font-family: 'Open Sans', sans-serif;
+}
+
+a {
+    display: inline-block;
+    color: #3cb6ce;
+    font-size: 24px;
+    line-height: 26px;
+    font-weight: 400;
+    font-family: 'Open Sans', sans-serif;
+}
+
+a, a:hover {
+    text-decoration: none;
+}
+/**
+*/
+
+
+</style>
+
+
+
+
+<div class="video-hero-header" >
+	<section class="hero flex-col">
+		<div class="hero-image xs-only"></div>
+		<div class="hero-image video xs-max-hide">
+			<div class="vidbg-box" style="width: 100%; height: 100%" data-vidbg-bg="mp4:http://service.twistage.com/videos/de1ac177ef090/formats/360p-BitRateOptimized/file.mp4,webm:http://service.twistage.com/videos/de1ac177ef090/formats/360p-BitRateOptimized-Webm/file.webm,poster: http://service.twistage.com/images/i80f46b2f2e4a.jpg" data-vidbg-options="loop: true, muted: true, overlay: false, overlayColor: #000, overlayAlpha: 0"></div>
+		</div>
+		<div class="layout-inner-wrap hero-content transparent-bg white text-left" data-aos="fade-right">
+			<strong class="ha mb2" style="width:40%;font-size: 66px; line-height: 81px;font-weight: 300;">All Your Clouds Now <span style="color:#3cb6ce" >Simplified</span></strong>
+			<div class="h3" style="width:30%;font-size: 24px; line-height: 41px;font-weight: 400;color: #fff;font-style: italic; padding-bottom:2rem">BMC multi-cloud management solution make all clouds better.</div>
+			<a href="http://www.bmc.com/templates/Media_Video_Mobile?vID=4QtzW0S0PQ0" style="border-radius: 0;" class="modal-youtube-video-player btn btn-corporate" title="Transforming the Digital Enterprise"> 
+				Watch the video >
+			</a>
+		</div>
+	</section>
+</div>
+
+<div class="main-content">
+	<section class="section py2" data-aos="fade-in" data-aos-anchor-placement="bottom-bottom">
+		<div class="section-content text-center">
+			<h2 style="color:black">BMC is a global leader in multi-cloud software solutions</h2>
+		</div>
+	</section>
+	
+
+<section class="autoScroll section py2" style="background-color: rgba(247, 247, 246, 0.52);">
+		<div class="section-content py4" style="max-width: 1440px;">
+			<div class="md-flex full-bleed-two-column">
+				<div class="flex-item col col-12 md-col-6 pxr1" >
+                        <h2 data-aos="fade-right" data-aos-anchor-placement="top-bottom" data-aos-delay='100'>BMC multi-cloud<br>management solutions<br>make all clouds better.</h2>
+                        <p data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='200'>Eliminate the chasm between your on-premises data center and a cloud-based infrastructure. BMC helps companies bridge the gap to enable more innovation, agility, scalability, and cost savings. Our solutions provide answers about what to migrate, which clouds to use, how to manage cloud spend, how to ensure security, and how to optimize each step of your journey.</p>
+                        <a href="#" data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='400'>Learn more about multi-cloud migration&gt;</a>
+				</div>
+				<div class="flex-item col col-12 md-col-6 pxr1 text-center" >
+					<img src="http://localhost/bmc-dxp/front-end/Assets/dist/industry/Medical__industrypage_tab1-2x.png" alt="Mobile phone and app infographic">
+				</div>
+			</div>
+		</div>
+</section>
+
+
+<section class="autoScroll section py2" style="background-color: rgba(241, 241, 241, 0.52);">
+		<div class="section-content py4" style="max-width: 1440px;">
+			<div class="md-flex full-bleed-two-column">
+				<div class="flex-item col col-12 md-col-6 pxr1 text-center">
+					<img src="http://localhost/bmc-dxp/front-end/Assets/dist/industry/Medical__industrypage_tab1-2x.png" alt="Mobile phone and app infographic">
+				</div>
+				<div class="flex-item col col-12 md-col-6 pxr1 " style="padding-left:8rem;">
+                      	<h2 data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='100'>Migration</h2>
+                        <h4 data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='200'>Ensure your move to the cloud is painless</h4>
+                        <ul>
+                            <li data-aos="fade-left" data-aos-anchor-placement="bottom-bottom" data-aos-delay='300'> <p>Assess your unique needs </p></li>
+                            <li data-aos="fade-left" data-aos-anchor-placement="bottom-bottom" data-aos-delay='400'> <p>Build the right plan</p></li>
+                            <li data-aos="fade-left" data-aos-anchor-placement="bottom-bottom" data-aos-delay='500'> <p>Establish a governance model </p></li>
+                            <li data-aos="fade-left" data-aos-anchor-placement="bottom-bottom" data-aos-delay='600'> <p>Measure progress </p></li>
+                        </ul>
+                        <a href="#" data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='800'>Learn more about multi-cloud migration &gt;</a>
+				</div>
+				
+			</div>
+		</div>
+</section>
+
+<section class="autoScroll section py2" style="background-color: rgba(247, 247, 246, 0.52);">
+		<div class="section-content py4" style="max-width: 1440px;">
+			<div class="md-flex full-bleed-two-column">
+				<div class="flex-item col col-12 md-col-6 pxr1">
+                        <h2 data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='100'>Visibility</h2>
+                        <h4 data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='200'>Keep track of cloud assets</h4>
+                        <ul>
+                            <li data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='300'><p>Inventory all your IT assets </p></li>
+                            <li data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='400'><p>Map relationships across cloud platforms</p></li>
+                            <li data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='500'><p>Know which assets impact critical business<br> functions</p></li>
+                        </ul>
+                        <a href="#" data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='700'>Learn more about multi-cloud visibility &gt;</a>
+				</div>
+				<div class="flex-item col col-12 md-col-6 pxr1 text-center" >
+					<img src="http://localhost/bmc-dxp/front-end/Assets/dist/industry/Medical__industrypage_tab1-2x.png" alt="Mobile phone and app infographic">
+				</div>
+			</div>
+		</div>
+</section>
+
+
+<section class="autoScroll section py2" style="background-color: rgba(241, 241, 241, 0.52);">
+		<div class="section-content py4" style="max-width: 1440px;">
+			<div class="md-flex full-bleed-two-column">
+				<div class="flex-item col col-12 md-col-6 pxr1 text-center">
+					<img src="http://localhost/bmc-dxp/front-end/Assets/dist/industry/Medical__industrypage_tab1-2x.png" alt="Mobile phone and app infographic">
+				</div>
+				<div class="flex-item col col-12 md-col-6 pxr1 " style="padding-left:8rem;">
+                      	<h2 data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='100'>Cost</h2>
+                        <h4 data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='200'>Right-size your cloud services to reduce cost</h4>
+                        <ul>
+                            <li data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='300'> <p>Simulate migrations to see which cloud<br>  services fit best </p></li>
+                            <li data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='400'> <p>Track and compare on-premises and public<br> cloud usage</p></li>
+                            <li data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='500'> <p>Forecast annual multi-cloud costs</p></li>
+                        </ul>
+                        <a href="#" data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='700'>Learn more about multi-cloud cost  &gt;</a>
+				</div>
+				
+			</div>
+		</div>
+</section>
+
+<section class="autoScroll section py2" style="background-color: rgba(247, 247, 246, 0.52);">
+		<div class="section-content py4" style="max-width: 1440px;">
+			<div class="md-flex full-bleed-two-column">
+				<div class="flex-item col col-12 md-col-6 pxr1">
+                         <h2 data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='100'>Performance</h2>
+                        <h4 data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='200'>Monitor and manage integrated<br>cloud/data center performance</h4>
+                        <ul>
+                            <li data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='300'> <p>Monitor performance across all your clouds<br>   in real time </p></li>
+                            <li data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='400'> <p>Get rapid root cause analysis</p></li>
+                            <li data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='500'><p>Ensure optimal user experiences</p></li>
+                        </ul>
+                        <a href="#" data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='700'>Learn about cloud performance monitoring &gt;</a>
+				</div>
+				<div class="flex-item col col-12 md-col-6 pxr1 text-center" >
+					<img src="http://localhost/bmc-dxp/front-end/Assets/dist/industry/Medical__industrypage_tab1-2x.png" alt="Mobile phone and app infographic">
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class="autoScroll section py2" style="background-color: rgba(241, 241, 241, 0.52);">
+		<div class="section-content py4" style="max-width: 1440px;">
+			<div class="md-flex full-bleed-two-column">
+				<div class="flex-item col col-12 md-col-6 pxr1 text-center">
+					<img src="http://localhost/bmc-dxp/front-end/Assets/dist/industry/Medical__industrypage_tab1-2x.png" alt="Mobile phone and app infographic">
+				</div>
+				<div class="flex-item col col-12 md-col-6 pxr1 " style="padding-left:8rem;" data-aos="fade-left" data-aos-anchor-placement="center-bottom">
+                      	<h2  data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='100'>Automation</h2>
+                        <h4  data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='200'>Apply automation to optimize workflows</h4>
+                        <ul>
+                            <li data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='300'><p>Orchestrate data, applications, and infrastructure</p></li>
+                            <li data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='400'><p>Centrally manage diverse workloads</p></li>
+                            <li data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='500'><p>Automatically provision cloud servers and services</p></li>
+                        </ul>
+                        <a href="#"  data-aos="fade-left" data-aos-anchor-placement="center-bottom" data-aos-delay='700'>Learn more about multi-cloud automation &gt;</a>
+				</div>
+				
+			</div>
+		</div>
+</section>
+
+<section class="autoScroll section py2" style="background-color: rgba(247, 247, 246, 0.52);" data-aos-anchor-placement="center-bottom">
+		<div class="section-content py4" style="max-width: 1440px;">
+			<div class="md-flex full-bleed-two-column">
+				<div class="flex-item col col-12 md-col-6 pxr1">
+                        <h2 data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='100'>Security</h2>
+                        <h4 data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='200'>Achieve security and compliance<br> across clouds</h4>
+                        <ul>
+                            <li data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='300'><p>Find and fix security and compliance gaps</p></li>
+                            <li data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='400'><p>Automate remediation</p></li>
+                            <li data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='500'><p>Be audit ready all the time</p></li>
+                        </ul>
+                        <a href="#" data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay='700'>Learn more about multi-cloud security &gt;</a>
+				</div>
+				<div class="flex-item col col-12 md-col-6 pxr1 text-center">
+					<img src="http://localhost/bmc-dxp/front-end/Assets/dist/industry/Medical__industrypage_tab1-2x.png" alt="Mobile phone and app infographic">
+				</div>
+			</div>
+		</div>
+</section>
+	
+</div>
+
+<script src="http://localhost/bmc-dxp/front-end/aos.js"></script>
+  
+  	<script>
+      AOS.init({
+    	  offset: 100,
+          duration: 600,
+          easing: 'ease-in',
+          delay: 300,
+          startEvent: 'load',
+      });
+	
+      function defer(method) {
+    	    if (window.jQuery) {
+    	        //method();
+    	    } else {
+    	        setTimeout(function() { defer(method) }, 50);
+    	    }
+    	}
+
+      defer(function () {
+    	    $('.autoScroll').each(function() {
+    			var position = $(this).position(),
+    				// 170 is an arbitrary number found by trial and error
+    				offset = $(this).height()/2;
+    			$(this).scrollspy({
+    				min: position.top - offset,
+    				max: (position.top - offset) + $(this).height(),
+    				onEnter: function(element, position) {
+    					//alert($(element).offset().top - $('.layout-header').height());
+    					$('html,body').animate({
+    						scrollTop: $(element).offset().top - $('.layout-header').height()
+    					}, 500);
+    				}
+    			});
+    		});
+    	});
+  	
+    </script>
+
+<?php
+include 'php-inc/foot.php';
+?>
