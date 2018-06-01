d9ab4844500f648e2931febdd53b261132bba264
Dermot Conner <github@dermot.org>
2018-06-01 01:42:22 -0500

    Pom files updated for 3.4.0. Minor tweak to front end build command addressing go-live issue

--- front-end/Assets/src/build/tasks.js
+++ front-end/Assets/src/build/tasks.js
@@ -46,7 +46,7 @@ module.exports = function(configuration) {
 			.pipe(gulp.dest('./../../../aem-project/ui.apps/src/main/content/jcr_root/etc/clientlibs/bmc/head'));
 
 	});
-	
+
 	//aem-production command to publish JS and CSS in minified format.
 	gulp.task('aem-production', ['aem-minification'], function() {
 		// copies files from FED build location to a corresponding location for vlt to bundle and deploy to aem
@@ -63,36 +63,36 @@ module.exports = function(configuration) {
 		// css images
 		//gulp.src('./../dist/**/*') // WEB-2331 : commented, becasue no need to update individual source code js/css files to AEM)
 		//Promoting only image files.
-		gulp.src('./../dist/**/*.{jpg,png,gif,ico,svg}')
+		gulp.src('./../dist/**/*')
 			.pipe(gulp.dest('./../../../aem-project/ui.apps/src/main/content/jcr_root/etc/clientlibs/bmc/head'));
 
 	});
-	
+
 	gulp.task('aem-minification', loader.getBuildDependencies(), function() {
 		// copies files from FED build location to a corresponding location for vlt to bundle and deploy to aem
 		// main client lib
-		
+
 		var masterStream,
 		sLoader = new StreamLoader(gulp, configuration);
 
 		if(configuration.cleanProduction) {
 			rimraf.sync(configuration.output);
 		}
-	
+
 		sLoader.loadStreams(false);
-	
+
 		masterStream = sLoader.getTaskStreams();
-	
+
 		if (configuration.cacheBusting) {
 			// rev stamps all files with a hash for cache-busting
 			// ignores php files and contents of /img/content directory.
 			masterStream = masterStream.pipe(rev({ ignore: ['.php', /img\/content/] }));
 		}
-	
+
 		masterStream = sLoader.executeCustomOutput(masterStream);
-	
+
 		return masterStream.pipe(gulp.dest(configuration.output));
-		
+
 	});
 
 	// this is the top level production task (minified, no sourcemaps, rev'd)
@@ -139,7 +139,7 @@ module.exports = function(configuration) {
 
 		return masterStream.pipe(gulp.dest(configuration.output));
 	});
-	
+
 	gulp.task('allianceProgram', loader.getBuildDependencies(), function() {
 		var masterStream,
 			sLoader = new StreamLoader(gulp, configuration);
@@ -152,7 +152,7 @@ module.exports = function(configuration) {
 
 		return masterStream.pipe(gulp.dest(configuration.output));
 	});
-	
+
 	gulp.task('onGig', loader.getBuildDependencies(), function() {
 		var masterStream,
 		sLoader = new StreamLoader(gulp, configuration);
