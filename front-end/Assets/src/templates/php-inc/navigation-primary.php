cab71174cda9d4f66ab27ae37d9fbacadf997e20
ADPROD\mkotapal <mahesh_kotapali_tp@bmc.com>
2018-07-30 13:03:01 +0530

    WEB-2853 - Scroll Removed

--- front-end/Assets/src/templates/php-inc/navigation-primary.php
+++ front-end/Assets/src/templates/php-inc/navigation-primary.php
@@ -78,12 +78,12 @@
 				  <div class="search-panel">
 					 <div class="search_click btn-white">Search</div>
 					 <form accept-charset="utf-8" id="search_form" onsubmit="return false" data-keywords="/etc/tags/keywords" data-json="/bin/servicenow/searchservlet.keywords.json">
-						<input class="search_input" autocapitalize="off" autocorrect="off" aria-haspopup="false" autocomplete="off" aria-autocomplete="both" dir="ltr" autofocus="" type="text" name="q" title="search" value="" id="search_input" x-webkit-speech="" x-webkit-grammar="builtin:search" lang="en" spellcheck="false" placeholder="Search">   
+						<input class="search_input" autocapitalize="off" autocorrect="off" aria-haspopup="false" autocomplete="on" aria-autocomplete="both" dir="ltr" autofocus="" type="text" name="q" title="search" value="" id="search_input" x-webkit-speech="" x-webkit-grammar="builtin:search" lang="en" spellcheck="false" placeholder="Search">   
 					 </form>
 					 <div class="drop_overlay"></div>
 				  </div>
 				  <div class="overlay_keywords">
-					 <div class="heading">People also search for:</div>
+					 <div class="heading">Popular Destinations</div>
 					 <div class="keywords" data-nr-items="6">
 						<span><a href="">About BMC</a></span>
 						<span><a href="" target="_blank">On-demand Webinars</a></span>
