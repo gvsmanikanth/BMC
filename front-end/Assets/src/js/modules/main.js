40087a25274fb2ca71aa4297a3995f4a6bfea464
ADPROD\mkotapal <mahesh_kotapali_tp@bmc.com>
2018-09-25 15:20:21 +0530

    WEB-3881 - Jump Link

--- front-end/Assets/src/js/modules/main.js
+++ front-end/Assets/src/js/modules/main.js
@@ -718,9 +718,9 @@ $("a").productInterest();//WEB-2626
 
 //WEB-3881 Header Strip Jump click event
 $('a').click(function(e) {
-	e.preventDefault();
 	var href = $(this).attr("href"); 
-    if(href.indexOf("jumpTo_") > -1){
+    if(href.indexOf("jumpTo_") > -1){ 
+		e.preventDefault();
 		var newHref = "#"+ href.replace("jumpTo_",'');
 		window.location.hash = newHref;
 		if($(".tab-wrapper").length >= 1){
