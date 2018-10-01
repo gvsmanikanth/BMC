797da388909b7b5f958202f61cad53f635bfb4a0
ADPROD\sanvekar <samiksha_anvekar@bmc.com>
2018-10-01 15:36:56 +0530

    on gig data scheduler API call

--- aem-project/core/src/main/java/com/bmc/schedulers/onGigDataScheduler.java
+++ aem-project/core/src/main/java/com/bmc/schedulers/onGigDataScheduler.java
@@ -3,6 +3,7 @@ package com.bmc.schedulers;
 
 import java.util.ArrayList;
 import java.util.Map;
+
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Deactivate;
@@ -12,18 +13,21 @@ import org.apache.felix.scr.annotations.Reference;
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import javax.jcr.LoginException;
+
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.commons.osgi.PropertiesUtil;
 import org.apache.sling.commons.scheduler.Scheduler;
+
 import com.bmc.components.OnGigDataItem;
 import com.bmc.services.OnGigDataService;
 
  
 @Component(
-        label = "Sample Sling Scheduler",
-        description = "Sample Description",
+        label = "OnGigData Sling Scheduler",
+        description = "onGigData Scheduler Description",
         immediate = true,
         metatype = true
 )
@@ -39,7 +43,7 @@ import com.bmc.services.OnGigDataService;
         )
        
 @Service
-public class SampleScheduledService implements Runnable {
+public class onGigDataScheduler implements Runnable {
  
     
     @Reference
@@ -49,38 +53,38 @@ public class SampleScheduledService implements Runnable {
     
     @Reference
     private OnGigDataService dataService;
-    
-    private static final String baseURL  = "http://talent.ongig.com/api/v1/groups/pages/20?gid=20&group=bmc&Content-Type=application/x-www-form-urlencoded";
-    
-    private static final String BASE = "content";
-    
+        
     ArrayList<OnGigDataItem> dataListItem;
-    
+        
+	private String baseURL;
+	
+	 @Activate
+	    protected void activate(final ComponentContext componentContext) throws Exception {
+	        final Map<String, String> properties = (Map<String, String>) componentContext.getProperties();			
+	    }
+	 
+	 
     @Override
     public void run() {
        try
        {
-            log.info("Started the SampleScheduledService");
+            log.info("Started the Scheduler for the onGigData");
           //Fetch the data from the server
-            dataListItem =	dataService.getdataConnection(baseURL);
+            dataListItem =	dataService.getdataConnection(null);
             //Add the data to the repository
-            	dataService.injestonGigData(dataListItem, BASE);
+            	dataService.injestonGigData(dataListItem);
             
         } catch(Exception e)
         {
-        	log.info("caught the exception");
+        	log.info("Error: onGigDataScheduler "+e.getMessage());
         }
         	finally {
         
-        	log.info("FInally");
+        	log.info("Finally");
         }
     }
  
-    @Activate
-    protected void activate(final ComponentContext componentContext) throws Exception {
-        final Map<String, String> properties = (Map<String, String>) componentContext.getProperties();
-        
-    }
+   
  
     @Deactivate
     protected void deactivate(ComponentContext ctx) {
