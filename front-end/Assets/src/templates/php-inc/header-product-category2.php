ef3c54040b96fbbad8d1be8c524a5ff4339712d9
Kadam, Shailesh <shailesh_kadam@bmc.com>
2018-10-08 13:33:53 +0530

    Added Class.

--- front-end/Assets/src/templates/php-inc/header-product-category2.php
+++ front-end/Assets/src/templates/php-inc/header-product-category2.php
@@ -1,9 +1,4 @@
 <!-- First -->
-<style>
-.btn_secondary_new{
-	padding-right: 2.37778em !important;
-}
-</style>
 <section
 	class="wallpapered bar full-bleed-page-banner product-category-header product-category-header2 padding-bottom-0"
 	data-wallpaper-options='{"source":{"fallback": "//www.bmc.com/content/dam/bmc/images/NewBrandImage-MobileBanner-Test-700x700px.png","(min-width: 640px)": "//www.bmc.com/content/dam/bmc/images/NewBrandImage-WebBanner-Test-1900px700px.png"}}'>
@@ -82,22 +77,24 @@
 
 <!-- Second -->
 
+<!-- First -->
 <section
-	class="wallpapered bar full-bleed-page-banner product-category-header  product-category-header2 padding-bottom-0"
+	class="wallpapered bar full-bleed-page-banner product-category-header product-category-header2 padding-bottom-0"
 	data-wallpaper-options='{"source":{"fallback": "//www.bmc.com/content/dam/bmc/images/NewBrandImage-MobileBanner-Test-700x700px.png","(min-width: 640px)": "//www.bmc.com/content/dam/bmc/images/NewBrandImage-WebBanner-Test-1900px700px.png"}}'>
 
 	<div class="offset-wrapper flex-center">
 		<div class="inner">
-			<div class="bannerContent flex-center">
+			<div class="bannerContent">
 				<div class="layout-inner-wrap">
 					<div class="flex-item col col-12 md-col-6 sm-col-8">
-					<h1 class="white">Lorem ipsum dolor sit Lorem ipsum dolor sitis</h1>
-					<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam felis sapien, ornare condimentum</h2>
+					<h1 class="white">Lorem ipsum dolor sit</h1>
+					<h2>Lorem ipsum dolor sit amet, consectetur adipiscin Aliquam felis sapien, ornare condimentum</h2>
 					<div class="button-wrapper">
-						<a class="btn-secondary mr1"
-							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example ❭ </a>
-						<a class="btn-secondary"
-							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example ❭ </a>
+						<a class="btn-secondary learn-more mr1"
+							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example</a>
+						<a class="btn-secondary btn_secondary_new btn video-play modal-youtube-video-player"
+							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example</a>
+<!-- 							 ❭  -->
 					</div>
 					</div>
 				</div>
@@ -106,33 +103,40 @@
 				<div class="inner">
 					<div class="sm-flex flex-justify-center contentContainer">
 						<a href="http://www.bmc.com/info/LP-Demo-CTM.html"
-							class="flex-item sm-col-5 shadow-top assetStripItem">
+							class="flex-item sm-col-4 shadow-top assetStripItem">
 							<div class="inner assetTxt">
 								<div class="flex-center">
-									<img
-										src="//www.bmc.com/content/dam/bmc/images/Video_TEST.png"
-										alt="">
+									<img src="//www.bmc.com/content/dam/bmc/images/Datasheet_TEST.png" alt="">
 									<div class="inner">
-										<h5>Demo:</h5>
-										<p>Customizable video</p>
-										<p class="link">Watch it now&nbsp;&#8250;</p>
+										<h5>Analyst report:</h5>
+										<p>Lorem ipsum Lorem ipsum Lorem ipsum</p>
+										<p class="link">Download now&nbsp;›</p>
+									</div>
+								</div>
+							</div>
+						</a>
+						<a href="http://www.bmc.com/info/LP-Demo-CTM.html"
+							class="flex-item sm-col-4 shadow-top assetStripItem">
+							<div class="inner assetTxt">
+								<div class="flex-center">
+									<img src="//www.bmc.com/content/dam/bmc/images/Datasheet_TEST.png" alt="">
+									<div class="inner">
+										<h5>Analyst report:</h5>
+										<p>Lorem ipsum Lorem ipsum Lorem ipsum</p>
+										<p class="link">Download now&nbsp;›</p>
 									</div>
 								</div>
 							</div>
 						</a> <a href="http://www.bmc.com/info/LP-Demo-CTM.html"
-							class="flex-item sm-col-5 shadow-top assetStripItem">
+							class="flex-item sm-col-4 no-border shadow-top assetStripItem">
 							<div class="inner assetTxt">
-								<div class="flex-center ">
-
-									<img
-										src="//www.bmc.com/content/dam/bmc/images/Datasheet_TEST.png"
-										alt="">
+								<div class="flex-center">
+									<img src="//www.bmc.com/content/dam/bmc/images/Datasheet_TEST.png" alt="">
 									<div class="inner">
-										<h5>Datasheet:</h5>
-										<p>Control-M</p>
-										<p class="link">Download now&nbsp;&#8250;</p>
+										<h5>Analyst report:</h5>
+										<p>Lorem ipsum Lorem ipsum Lorem ipsum</p>
+										<p class="link">Download now&nbsp;›</p>
 									</div>
-
 								</div>
 							</div>
 						</a>
@@ -152,22 +156,24 @@
 
 <!-- Third -->
 
+<!-- First -->
 <section
-	class="wallpapered bar full-bleed-page-banner product-category-header  product-category-header2 padding-bottom-0"
+	class="wallpapered bar full-bleed-page-banner product-category-header product-category-header2 padding-bottom-0"
 	data-wallpaper-options='{"source":{"fallback": "//www.bmc.com/content/dam/bmc/images/NewBrandImage-MobileBanner-Test-700x700px.png","(min-width: 640px)": "//www.bmc.com/content/dam/bmc/images/NewBrandImage-WebBanner-Test-1900px700px.png"}}'>
 
 	<div class="offset-wrapper flex-center">
 		<div class="inner">
-			<div class="bannerContent flex-center">
+			<div class="bannerContent">
 				<div class="layout-inner-wrap">
 					<div class="flex-item col col-12 md-col-6 sm-col-8">
-					<h1 class="white">Lorem ipsum dolor sit Lorem ipsum dolor sitis</h1>
-					<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam felis sapien, ornare condimentum</h2>
+					<h1 class="white">Lorem ipsum dolor sit</h1>
+					<h2>Lorem ipsum dolor sit amet, consectetur adipiscin Aliquam felis sapien, ornare condimentum</h2>
 					<div class="button-wrapper">
-						<a class="btn-secondary mr1"
-							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example ❭ </a>
-						<a class="btn-secondary"
-							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example ❭ </a>
+						<a class="btn-secondary learn-more mr1"
+							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example</a>
+						<a class="btn-secondary btn_secondary_new btn video-play modal-youtube-video-player"
+							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example</a>
+<!-- 							 ❭  -->
 					</div>
 					</div>
 				</div>
@@ -176,16 +182,39 @@
 				<div class="inner">
 					<div class="sm-flex flex-justify-center contentContainer">
 						<a href="http://www.bmc.com/info/LP-Demo-CTM.html"
-							class="flex-item sm-col-6 shadow-top assetStripItem">
+							class="flex-item sm-col-4 shadow-top assetStripItem">
 							<div class="inner assetTxt">
 								<div class="flex-center">
-									<img
-										src="//www.bmc.com/content/dam/bmc/images/Video_TEST.png"
-										alt="">
+									<img src="//www.bmc.com/content/dam/bmc/images/Datasheet_TEST.png" alt="">
 									<div class="inner">
-										<h5>Demo:</h5>
-										<p>Customizable video</p>
-										<p class="link">Watch it now&nbsp;&#8250;</p>
+										<h5>Analyst report:</h5>
+										<p>Lorem ipsum Lorem ipsum Lorem ipsum</p>
+										<p class="link">Download now&nbsp;›</p>
+									</div>
+								</div>
+							</div>
+						</a>
+						<a href="http://www.bmc.com/info/LP-Demo-CTM.html"
+							class="flex-item sm-col-4 shadow-top assetStripItem">
+							<div class="inner assetTxt">
+								<div class="flex-center">
+									<img src="//www.bmc.com/content/dam/bmc/images/Datasheet_TEST.png" alt="">
+									<div class="inner">
+										<h5>Analyst report:</h5>
+										<p>Lorem ipsum Lorem ipsum Lorem ipsum</p>
+										<p class="link">Download now&nbsp;›</p>
+									</div>
+								</div>
+							</div>
+						</a> <a href="http://www.bmc.com/info/LP-Demo-CTM.html"
+							class="flex-item sm-col-4 no-border shadow-top assetStripItem">
+							<div class="inner assetTxt">
+								<div class="flex-center">
+									<img src="//www.bmc.com/content/dam/bmc/images/Datasheet_TEST.png" alt="">
+									<div class="inner">
+										<h5>Analyst report:</h5>
+										<p>Lorem ipsum Lorem ipsum Lorem ipsum</p>
+										<p class="link">Download now&nbsp;›</p>
 									</div>
 								</div>
 							</div>
@@ -206,22 +235,23 @@
 
 <!-- Third -->
 
+<!-- First -->
 <section
-	class="wallpapered bar full-bleed-page-banner product-category-header  product-category-header2 padding-bottom-0"
+	class="wallpapered bar full-bleed-page-banner product-category-header product-category-header2 padding-bottom-0"
 	data-wallpaper-options='{"source":{"fallback": "//www.bmc.com/content/dam/bmc/images/NewBrandImage-MobileBanner-Test-700x700px.png","(min-width: 640px)": "//www.bmc.com/content/dam/bmc/images/NewBrandImage-WebBanner-Test-1900px700px.png"}}'>
-
 	<div class="offset-wrapper flex-center">
 		<div class="inner">
 			<div class="bannerContent flex-center">
 				<div class="layout-inner-wrap">
 					<div class="flex-item col col-12 md-col-6 sm-col-8">
-					<h1 class="white">Lorem ipsum dolor sit Lorem ipsum dolor sitis</h1>
-					<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam felis sapien, ornare condimentum</h2>
+					<h1 class="white">Lorem ipsum dolor sit</h1>
+					<h2>Lorem ipsum dolor sit amet, consectetur adipiscin Aliquam felis sapien, ornare condimentum</h2>
 					<div class="button-wrapper">
-						<a class="btn-secondary mr1"
-							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example ❭ </a>
-						<a class="btn-secondary"
-							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example ❭ </a>
+						<a class="btn-secondary learn-more mr1"
+							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example</a>
+						<a class="btn-secondary btn_secondary_new btn video-play modal-youtube-video-player"
+							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example</a>
+<!-- 							 ❭  -->
 					</div>
 					</div>
 				</div>
