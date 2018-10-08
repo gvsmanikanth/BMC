c6cb2b946c6a9cd544c58ee77a559806a6e12e97
Kadam, Shailesh <shailesh_kadam@bmc.com>
2018-10-08 16:24:53 +0530

    WEB-4066 : Alignment center settings

--- front-end/Assets/src/templates/php-inc/header-product-category2.php
+++ front-end/Assets/src/templates/php-inc/header-product-category2.php
@@ -7,7 +7,7 @@
 		<div class="inner">
 			<div class="bannerContent">
 				<div class="layout-inner-wrap">
-					<div class="flex-item col col-12 md-col-6 sm-col-8">
+					<div class="flex-item col col-12 md-col-6 sm-col-8 add-top-bottom-margin">
 					<h1 class="white">Lorem ipsum dolor sit</h1>
 					<h2>Lorem ipsum dolor sit amet, consectetur adipiscin Aliquam felis sapien, ornare condimentum</h2>
 					<div class="button-wrapper">
@@ -86,7 +86,7 @@
 		<div class="inner">
 			<div class="bannerContent">
 				<div class="layout-inner-wrap">
-					<div class="flex-item col col-12 md-col-6 sm-col-8">
+					<div class="flex-item col col-12 md-col-6 sm-col-8 add-top-bottom-margin">
 					<h1 class="white">Lorem ipsum dolor sit</h1>
 					<h2>Lorem ipsum dolor sit amet, consectetur adipiscin Aliquam felis sapien, ornare condimentum</h2>
 					<div class="button-wrapper">
@@ -165,7 +165,7 @@
 		<div class="inner">
 			<div class="bannerContent">
 				<div class="layout-inner-wrap">
-					<div class="flex-item col col-12 md-col-6 sm-col-8">
+					<div class="flex-item col col-12 md-col-6 sm-col-8 add-top-bottom-margin">
 					<h1 class="white">Lorem ipsum dolor sit</h1>
 					<h2>Lorem ipsum dolor sit amet, consectetur adipiscin Aliquam felis sapien, ornare condimentum</h2>
 					<div class="button-wrapper">
@@ -238,24 +238,56 @@
 <!-- First -->
 <section
 	class="wallpapered bar full-bleed-page-banner product-category-header product-category-header2 padding-bottom-0"
-	data-wallpaper-options='{"source":{"fallback": "//www.bmc.com/content/dam/bmc/images/NewBrandImage-MobileBanner-Test-700x700px.png","(min-width: 640px)": "//www.bmc.com/content/dam/bmc/images/NewBrandImage-WebBanner-Test-1900px700px.png"}}'>
+	data-wallpaper-options='{"source":{"fallback": "//www.bmc.com/content/dam/bmc/images/NewBrandImage-MobileBanner-Test-700x700px.png","(min-width: 640px)": "//www.bmc.com/content/dam/bmc/corporate/tbn-leadership.png"}}'>
 	<div class="offset-wrapper flex-center">
 		<div class="inner">
 			<div class="bannerContent flex-center">
 				<div class="layout-inner-wrap">
-					<div class="flex-item col col-12 md-col-6 sm-col-8">
-					<h1 class="white">Lorem ipsum dolor sit</h1>
-					<h2>Lorem ipsum dolor sit amet, consectetur adipiscin Aliquam felis sapien, ornare condimentum</h2>
-					<div class="button-wrapper">
-						<a class="btn-secondary learn-more mr1"
-							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example</a>
-						<a class="btn-secondary btn_secondary_new btn video-play modal-youtube-video-player"
-							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example</a>
-<!-- 							 ❭  -->
-					</div>
+					<div class="flex-item col col-12 md-col-6 sm-col-8 no-top-margin">
+					<h1 class="white">About BMC</h1>
+					<h2>Master modern IT with software, solutions, and services to help you run your business as you reinvent it</h2>
+<!-- 					<div class="button-wrapper"> -->
+<!-- 						<a class="btn-secondary learn-more mr1" -->
+<!-- 							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example</a> -->
+<!-- 						<a class="btn-secondary btn_secondary_new btn video-play modal-youtube-video-player" -->
+<!-- 							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example</a> -->
+<!-- 					</div> -->
 					</div>
 				</div>
 			</div>
+</section>
+
+<section
+	class="layout-full-bleed support-promo bg-mediumOrange product-category-promo">
+	<p class="align-center white promo-content">
+		Looking for OnDemand Services Support? <a class="learn-more white"
+			href="http://i.onbmc.com"><span>OnDemand Services Support (BG Center Center)</span></a>
+	</p>
+</section>
+
+
+<!-- First -->
+<section
+	class="wallpapered bar full-bleed-page-banner product-category-header product-category-header2 banner-image-from-top padding-bottom-0"
+	data-wallpaper-options='{"source":{"fallback": "//www.bmc.com/content/dam/bmc/images/NewBrandImage-MobileBanner-Test-700x700px.png","(min-width: 640px)": "//www.bmc.com/content/dam/bmc/solutions/banners/tbn-dsm-mc-helix-service.png"}}'>
+	<div class="inner outer-banner-content">
+	<div class="offset-wrapper flex-center">
+		<div class="inner">
+			<div class="bannerContent flex-center">
+				<div class="layout-inner-wrap">
+					<div class="flex-item col col-12 md-col-6 sm-col-8 no-top-bottom-margin">
+					<h1 class="white">Corporate Leadership</h1>
+					<h2>Meet the people who run and reinvent BMC (BG Image from Top)</h2>
+<!-- 					<div class="button-wrapper"> -->
+<!-- 						<a class="btn-secondary learn-more mr1" -->
+<!-- 							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example</a> -->
+<!-- 						<a class="btn-secondary btn_secondary_new btn video-play modal-youtube-video-player" -->
+<!-- 							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example</a> -->
+<!-- 					</div> -->
+					</div>
+				</div>
+			</div>
+	</div>		
 </section>
 
 <section
