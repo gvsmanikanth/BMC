581cb6ad6c601baa5b7e3dc5e52fc4bf2001b525
Kadam, Shailesh <shailesh_kadam@bmc.com>
2018-10-08 19:59:19 +0530

    Trying to solve alignment issue in IE-11 through Javascript

--- front-end/Assets/src/templates/php-inc/header-product-category2.php
+++ front-end/Assets/src/templates/php-inc/header-product-category2.php
@@ -230,26 +230,20 @@
 
 <!-- Third -->
 
-<!-- First -->
-<section
-	class="wallpapered bar full-bleed-page-banner product-category-header product-category-header2 padding-bottom-0"
+<section class="wallpapered bar full-bleed-page-banner product-category-header product-category-header2 padding-bottom-0 js-content-center"
 	data-wallpaper-options='{"source":{"fallback": "//www.bmc.com/content/dam/bmc/images/NewBrandImage-MobileBanner-Test-700x700px.png","(min-width: 640px)": "//www.bmc.com/content/dam/bmc/corporate/tbn-leadership.png"}}'>
-	<div class="offset-wrapper flex-center">
+	<div class="offset-wrapper flex-center js-content-center-item">
 		<div class="inner">
 			<div class="bannerContent flex-center">
 				<div class="layout-inner-wrap">
 					<div class="flex-item col col-12 md-col-6 sm-col-8 no-top-margin">
 					<h1 class="white">About BMC</h1>
 					<h2>Master modern IT with software, solutions, and services to help you run your business as you reinvent it</h2>
-<!-- 					<div class="button-wrapper"> -->
-<!-- 						<a class="btn-secondary learn-more mr1" -->
-<!-- 							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example</a> -->
-<!-- 						<a class="btn-secondary btn_secondary_new btn video-play modal-youtube-video-player" -->
-<!-- 							href="http://www.bmc.com/forms/control-m-enterprise-free-trial-runaware.html">CTA Example</a> -->
-<!-- 					</div> -->
 					</div>
 				</div>
 			</div>
+		</div>
+	</div>		
 </section>
 
 <section
@@ -261,11 +255,11 @@
 </section>
 
 
-<!-- First -->
+<!-- Fourth -->
 <section
 	class="wallpapered bar full-bleed-page-banner product-category-header product-category-header2 banner-image-from-top padding-bottom-0"
 	data-wallpaper-options='{"source":{"fallback": "//www.bmc.com/content/dam/bmc/images/NewBrandImage-MobileBanner-Test-700x700px.png","(min-width: 640px)": "//www.bmc.com/content/dam/bmc/solutions/banners/tbn-dsm-mc-helix-service.png"}}'>
-	<div class="inner outer-banner-content">
+	<div class="outer-banner-content">
 		<div class="offset-wrapper flex-center">
 			<div class="inner">
 				<div class="bannerContent flex-center">
