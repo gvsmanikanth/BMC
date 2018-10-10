2c8d1139ff4993357b4fa23b630aa4e1c05fdbec
ADPROD\mkotapal <mahesh_kotapali_tp@bmc.com>
2018-10-10 17:09:22 +0530

    WEB-2853 - Support Central Search Alignment

--- front-end/Assets/src/templates/php-inc/support/head.php
+++ front-end/Assets/src/templates/php-inc/support/head.php
@@ -10,18 +10,18 @@
 	<meta name="apple-mobile-web-app-title" content="BMC Software">
 	
 	<!-- icon and tile color in hex # for Windows phones -->
-	<meta name="msapplication-TileImage" content="includes/touch-icon.png">
+	<meta name="msapplication-TileImage" content="Assets/dist/touch-icon.png">
 	<meta name="msapplication-TileColor" content="#ffffff">
 	
-	<script src="includes/head.js"></script>
+	<script src="Assets/dist/head.js"></script>
 	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,400italic,600,700,800" rel="stylesheet" type="text/css">
 	
-	<link rel="stylesheet" media="all" href="includes/style.css">
+	<link rel="stylesheet" media="all" href="Assets/dist/style.css">
 
 	<!-- For everything else --> 
-	<link rel="shortcut icon" href="includes/favicon.png"> 
+	<link rel="shortcut icon" href="Assets/dist/favicon.png"> 
 	<!-- Serve one icon for all Apple devices -->
-	<link rel="apple-touch-icon-precomposed" href="includes/touch-icon.png"> 
+	<link rel="apple-touch-icon-precomposed" href="Assets/dist/touch-icon.png"> 
 </head>
 
 <body class="<?php echo $bodyClass; ?>">
