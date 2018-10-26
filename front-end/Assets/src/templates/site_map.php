13003bc5c42c21e5b18f485cc99fa30846088752
ADPROD\mkotapal <mahesh_kotapali_tp@bmc.com>
2018-10-26 10:16:46 +0530

    WEB-4108 - Site Map

---
+++ front-end/Assets/src/templates/site_map.php
@@ -0,0 +1,84 @@
+<?php
+	$pageTitle = 'Site Map';
+	include 'php-inc/head.php';
+?>
+
+<?php include 'php-inc/svg.php'; ?>
+
+<section class="layout-full-bleed py1 sitemap">
+	<div class="layout-inner-wrap">
+		<h1 class="h1">BMC Site Map</h2>
+			<h3 class="h2">Products:</h3>
+			<div>
+			<h4 class="h4">DSM (Digital Service Managemnet)</h4><hr/>
+				<ul class="mb4">
+					<li ><a href="#">BMC CMDB</a></li>
+					<li ><a href="#">Client Management</a></li>
+					<li ><a href="#">Digital Workplace</a></li>
+					<li ><a href="#">Application Dependency</a></li>
+					<li ><a href="#">Mapping</a></li>
+					<li ><a href="#">Footprints Service Desk</a></li>
+					<li ><a href="#">BMC CMDB</a></li>
+					<li ><a href="#">Client Management</a></li>
+					<li ><a href="#">Digital Workplace</a></li>
+					<li ><a href="#">Application Dependency</a></li>
+					<li ><a href="#">Mapping</a></li>
+					<li ><a href="#">Footprints Service Desk</a></li>
+					<li ><a href="#">BMC CMDB</a></li>
+					<li ><a href="#">Client Management</a></li>
+					<li ><a href="#">Digital Workplace</a></li>
+					<li ><a href="#">Application Dependency</a></li>
+					<li ><a href="#">Mapping</a></li>
+					<li ><a href="#">Footprints Service Desk</a></li>
+				</ul>
+			</div>
+			<div>
+				<h4 class="h4">DBA (Digital Business Automation)</h4><hr/>
+					<ul class="mb4">
+						<li ><a href="#">BMC CMDB</a></li>
+						<li ><a href="#">Client Management</a></li>
+						<li ><a href="#">Digital Workplace</a></li>
+						<li ><a href="#">Application Dependency</a></li>
+						<li ><a href="#">Mapping</a></li>
+						<li ><a href="#">Footprints Service Desk</a></li>
+						<li ><a href="#">BMC CMDB</a></li>
+						<li ><a href="#">Client Management</a></li>
+						<li ><a href="#">Digital Workplace</a></li>
+						<li ><a href="#">Application Dependency</a></li>
+						<li ><a href="#">Mapping</a></li>
+						<li ><a href="#">Footprints Service Desk</a></li>
+						<li ><a href="#">BMC CMDB</a></li>
+						<li ><a href="#">Client Management</a></li>
+						<li ><a href="#">Digital Workplace</a></li>
+						<li ><a href="#">Application Dependency</a></li>
+						<li ><a href="#">Mapping</a></li>
+						<li ><a href="#">Footprints Service Desk</a></li>
+					</ul>
+			</div>
+			<div>
+				<h4 class="h4">DSO (Digital Service Operations)</h4><hr/>
+					<ul class="mb4">
+						<li ><a href="#">BMC CMDB</a></li>
+						<li ><a href="#">Client Management</a></li>
+						<li ><a href="#">Digital Workplace</a></li>
+						<li ><a href="#">Application Dependency</a></li>
+						<li ><a href="#">Mapping</a></li>
+						<li ><a href="#">Footprints Service Desk</a></li>
+						<li ><a href="#">BMC CMDB</a></li>
+						<li ><a href="#">Client Management</a></li>
+						<li ><a href="#">Digital Workplace</a></li>
+						<li ><a href="#">Application Dependency</a></li>
+						<li ><a href="#">Mapping</a></li>
+						<li ><a href="#">Footprints Service Desk</a></li>
+						<li ><a href="#">BMC CMDB</a></li>
+						<li ><a href="#">Client Management</a></li>
+						<li ><a href="#">Digital Workplace</a></li>
+						<li ><a href="#">Application Dependency</a></li>
+						<li ><a href="#">Mapping</a></li>
+						<li ><a href="#">Footprints Service Desk</a></li>
+				</ul>
+			</div>
+	</div>
+</section>
+
+<?php include 'php-inc/foot.php'; ?>
