3985ec8e46fe23806fad5f079a593a1bb5c9269b
ADPROD\mkotapal <mahesh_kotapali_tp@bmc.com>
2018-12-03 19:52:13 +0530

    WEB-4283 - Home Page with asset strip

--- front-end/Assets/src/js/modules/homepage-aos.js
+++ front-end/Assets/src/js/modules/homepage-aos.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
         $('.section-wrap-header').css('height', windowHeight-120);
         
         if($(".js-content-center") && $(".js-content-center-item")){
-			var compHeight = $(".js-content-center").innerHeight() - $(".assetStrip.assetStripBottom").innerHeight();
+			var compHeight = $(".js-content-center").innerHeight() - $(".assetStrip.assetStripBottom").innerHeight(); 
 			var contentHeight = $(".js-content-center-item").innerHeight();
 			$('.js-content-center-item').css('margin-top', (compHeight-contentHeight)/2);
 			$('.js-content-center-item').css('margin-bottom', (compHeight-contentHeight)/2);
