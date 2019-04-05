0b2afeafdac07a60f2896fc6313422b9bc19e26d
Pradip Thigale <40828590+Pradipthigale@users.noreply.github.com>
2019-04-05 14:53:06 +0530

    PullQuote Light version created

---
+++ front-end/Assets/src/templates/pullquote-light.php
@@ -0,0 +1,16 @@
+<?php
+	$pageTitle = 'pullQuote';
+ $bodyClass = '';
+include_once 'php-inc/head.php';
+?>
+<div class="pullquote-light aem-GridColumn aem-GridColumn--default--12">
+    <blockquote class="fullbleed-blockquote">
+        <div class="layout-inner-wrap">            
+                PullQuote in Full-Width Column PullQuote in Full-Width Column PullQuote in Full-Width Column PullQuote in Full-Width Column PullQuote in Full-Width Column PullQuote in Full-Width Column PullQuote in Full-Width Column           
+             <cite>
+                Citation Citation Citation Citation Citation
+            </cite>  
+        </div>
+    </blockquote>
+</div>
+<?php  include_once 'php-inc/foot.php'; ?>
