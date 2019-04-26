0af711bcd6c6ae5f3eed7e1bb1adbd2fe2c04f1b
ADPROD\mkotapal <mahesh_kotapali_tp@bmc.com>
2019-04-26 15:35:02 +0530

    WEB-4940 - Alignment and Mobile Responsive JS Added

--- front-end/Assets/src/js/modules/contact-form.js
+++ front-end/Assets/src/js/modules/contact-form.js
@@ -1,13 +1,20 @@
 
-    if ($('body').hasClass('form2')) {
-        var isMobile = window.matchMedia("only screen and (max-width: 900px)").matches;
-        if (!isMobile) {
-            // getting current header height and add ttop minus margin to the form
-            var headerHeight = '-'+ $(".ornate-header").height()+'px';
-            $('.form2 form').css('margin-top',headerHeight);           
-        } else{
-            // for rmoving header from top and add to after form 
-            $('.product-category-header').appendTo('.form-wrap');
-        }
-    }
+function insertAfter(){
+	if ($('body').hasClass('form2')) {
+		var isMobile = window.matchMedia("only screen and (max-width: 900px)").matches;
+		if (!isMobile) {
+			// getting current header height and add ttop minus margin to the form
+			var headerHeight = '-'+ $(".ornate-header").height()+'px';
+			$('.form2 form').css('margin-top',headerHeight); 
+			$('.product-category-header').insertBefore($('.form-wrap').parents().closest(".maincontentcontainer")); 
+		} else{
+			// for rmoving header from top and add to after form 
+			$('.product-category-header').insertAfter('.form-wrap');
+		}
+	}
+}
+insertAfter();
 
+$(window).resize(function() {
+	  insertAfter();
+});
