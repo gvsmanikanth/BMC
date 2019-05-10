92514eb92f3e5dd2f430e2474a96f1e880a1dfb7
Mahesh <mahesh_kotapali_tp@bmc.com>
2019-05-10 15:45:55 +0530

    WEB-5159 - Image Vertical Alignment inside TAB Comp

---
+++ front-end/Assets/src/templates/tabs_comp.php
@@ -0,0 +1,195 @@
+<?php
+$pageTitle = 'BMC Home';
+$bodyClass = 'home page-home';
+include 'php-inc/head.php';
+?>
+
+<script>
+	var bmcMeta = {
+		"page": {
+			"productCategories": "Remedy",
+		},
+	};
+</script>
+
+<div class="responsivegrid aem-GridColumn aem-GridColumn--default--12">
+   <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+      <div class="maincontentcontainer responsivegrid aem-GridColumn aem-GridColumn--default--12">
+         <section class="aem-Grid aem-Grid--12 aem-Grid--default--12  layout-full-bleed">
+            <div class="100contentcontainer aem-GridColumn aem-GridColumn--default--12">
+               <section>
+                  <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 layout-inner-wrap">
+                     <div class="responsivegrid aem-GridColumn--phone--12 aem-GridColumn aem-GridColumn--default--12">
+                        <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+                           <div class="tabs responsivegrid aem-GridColumn aem-GridColumn--default--12">
+                              <section class="p0">
+                                 <div class="tab-wrapper bg-corduroy  r-tabs">
+                                    <h2 class="m0 text-center white">Check Image Alignment</h2>
+                                    <ul class="tabs-nav bg-corduroy center-block overflow-none flex-table r-tabs-nav">
+                                       <li class="p2 flex-cell n-col-3 r-tabs-tab r-tabs-state-active">
+                                          <a class="white bold no-scroll r-tabs-anchor" href="#tab-936679b5-2164-4bd5-852b-ab00e1ee3abf" onclick="">Top Align</a>
+                                       </li>
+                                       <li class="p2 flex-cell n-col-3 r-tabs-state-default r-tabs-tab">
+                                          <a class="white bold no-scroll r-tabs-anchor" href="#tab-d0be9a15-decf-4133-972a-191c40804ed9" onclick="">Bottom Align</a>
+                                       </li>
+                                    </ul>
+                                    <div class="r-tabs-accordion-title r-tabs-state-active"><a href="#tab-936679b5-2164-4bd5-852b-ab00e1ee3abf" class="r-tabs-anchor">Top Align</a></div>
+                                    <div id="tab-936679b5-2164-4bd5-852b-ab00e1ee3abf" class="section bg-seashell r-tabs-panel r-tabs-state-active" style="display: block;">
+                                       <div class="section-content flex-tab">
+                                          <div class="section responsivegrid">
+                                             <section>
+                                                <div class="layout-inner-wrap">
+                                                   <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+                                                      <div class="cmp cmp-title aem-GridColumn aem-GridColumn--default--12">
+                                                         <h2>
+                                                            Assets, Blogs, Product Cards Displays
+                                                         </h2>
+                                                      </div>
+                                                      <div class="33-66contentcontainer aem-GridColumn aem-GridColumn--default--12">
+                                                         <section>
+                                                            <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 layout-inner-wrap">
+                                                               <div class="responsivegrid aem-GridColumn--phone--12 aem-GridColumn aem-GridColumn--default--4 layout-secondary">
+                                                                  <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+                                                                     <div class="cmp cmp-image aem-GridColumn aem-GridColumn--default--12 ">
+                                                                        <div class="cq-dd-image text-left">
+                                                                           <img src="https://www.bmc.com/content/dam/bmc/migration/image/truesight-TI-Tab-03.jpg" alt="asdfdf">
+                                                                        </div>
+                                                                     </div>
+                                                                  </div>
+                                                               </div>
+                                                               <div class="responsivegrid layout-primary aem-GridColumn--phone--12 aem-GridColumn aem-GridColumn--default--8">
+                                                                  <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+                                                                     <div class="htmlarea aem-GridColumn aem-GridColumn--default--12">
+                                                                        <p>Make your IT department a key enabler of digital transformation and a strategic business partner. Designed with the needs of mid-size businesses, BMC Helix Remedyforce cost-effectively addresses the full range of ITSM requirements with the stability, scalability, and security your business demands, plus the modern experience today’s users expect.</p>
+                                                                        <ul class="orange-tick-bullets">
+                                                                           <li>Comprehensive, easy-to-implement ITSM</li>
+                                                                           <li>Built on Salesforce—the #1 scalable, secure, and stable cloud platform</li>
+                                                                           <li>Insight and visibility through dashboards, reporting, and analytics</li>
+                                                                           <li>Intelligent, omni-channel experience for agents and users</li>
+                                                                           <li>Smart Business Practices for service requests, incidents, and lines of business (IT, HR, Facilities)</li>
+                                                                           <li>Simple, value-based pricing</li>
+                                                                        </ul>
+                                                                        <div class=" resource-download">
+                                                                           <div class="icon-document"></div>
+                                                                           <strong>E-book:</strong> 
+                                                                           <ul class="plain">
+                                                                              <li><a href="http://documents.bmc.com/products/documents/85/81/498581/498581.pdf" target="_blank" class="">ITSM Buyer's Guide for the Mid-Market &nbsp;›</a></li>
+                                                                           </ul>
+                                                                        </div>
+                                                                        <div class=" resource-download">
+                                                                           <div class="icon-document"> 
+                                                                           </div>
+                                                                           <strong>Analyst research:</strong> 
+                                                                           <ul class="plain">
+                                                                              <li><a href="/content/dam/bmc/solutions/SWOT+Assessment+-+Remedyforce.pdf" target="_blank" class="">Ovum Consulting: SWOT Assessment: Remedyforce &nbsp;›</a></li>
+                                                                           </ul>
+                                                                        </div>
+                                                                        <div class=" resource-video">
+                                                                           <div class="icon-document"> 
+                                                                           </div>
+                                                                           <strong>Video:</strong> 
+                                                                           <ul class="plain">
+                                                                              <li><a href="/content/bmc/videos.html?vID=KUHmfrl3Ev4" target="_self" class="modal-youtube-video-player">Transform digital service delivery in less than 45 days (1:43) &nbsp;›</a></li>
+                                                                           </ul>
+                                                                        </div>
+                                                                     </div>
+                                                                  </div>
+                                                               </div>
+                                                            </div>
+                                                         </section>
+                                                      </div>
+                                                   </div>
+                                                </div>
+                                             </section>
+                                          </div>
+                                       </div>
+                                    </div>
+                                    <div class="r-tabs-accordion-title"><a href="#tab-d0be9a15-decf-4133-972a-191c40804ed9" class="r-tabs-anchor">Bottom Align</a></div>
+                                    <div id="tab-d0be9a15-decf-4133-972a-191c40804ed9" class="section bg-seashell r-tabs-state-default r-tabs-panel">
+                                       <div class="section-content flex-tab">
+                                          <div class="section responsivegrid">
+                                             <section>
+                                                <div class="layout-inner-wrap">
+                                                   <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+                                                      <div class="cmp cmp-title aem-GridColumn aem-GridColumn--default--12">
+                                                         <h2>
+                                                            Assets, Blogs, Product Cards Displays
+                                                         </h2>
+                                                      </div>
+                                                      <div class="33-66contentcontainer aem-GridColumn aem-GridColumn--default--12">
+                                                         <section>
+                                                            <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 layout-inner-wrap">
+                                                               <div class="responsivegrid aem-GridColumn--phone--12 aem-GridColumn aem-GridColumn--default--4 layout-secondary">
+                                                                  <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+                                                                     <div class="cmp cmp-image aem-GridColumn aem-GridColumn--default--12 align_bottom">
+                                                                        <div class="cq-dd-image text-center">
+                                                                           <img src="https://www.bmc.com/content/dam/bmc/migration/image/truesight-TI-Tab-03.jpg" alt="asdfdf">
+                                                                        </div>
+                                                                     </div>
+                                                                  </div>
+                                                               </div>
+                                                               <div class="responsivegrid layout-primary aem-GridColumn--phone--12 aem-GridColumn aem-GridColumn--default--8">
+                                                                  <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+                                                                     <div class="htmlarea aem-GridColumn aem-GridColumn--default--12">
+                                                                        <p>Make your IT department a key enabler of digital transformation and a strategic business partner. Designed with the needs of mid-size businesses, BMC Helix Remedyforce cost-effectively addresses the full range of ITSM requirements with the stability, scalability, and security your business demands, plus the modern experience today’s users expect.</p>
+                                                                        <ul class="orange-tick-bullets">
+                                                                           <li>Comprehensive, easy-to-implement ITSM</li>
+                                                                           <li>Built on Salesforce—the #1 scalable, secure, and stable cloud platform</li>
+                                                                           <li>Insight and visibility through dashboards, reporting, and analytics</li>
+                                                                           <li>Intelligent, omni-channel experience for agents and users</li>
+                                                                           <li>Smart Business Practices for service requests, incidents, and lines of business (IT, HR, Facilities)</li>
+                                                                           <li>Simple, value-based pricing</li>
+                                                                        </ul>
+                                                                        <div class=" resource-download">
+                                                                           <div class="icon-document"></div>
+                                                                           <strong>E-book:</strong> 
+                                                                           <ul class="plain">
+                                                                              <li><a href="http://documents.bmc.com/products/documents/85/81/498581/498581.pdf" target="_blank" class="">ITSM Buyer's Guide for the Mid-Market &nbsp;›</a></li>
+                                                                           </ul>
+                                                                        </div>
+                                                                        <div class=" resource-download">
+                                                                           <div class="icon-document"> 
+                                                                           </div>
+                                                                           <strong>Analyst research:</strong> 
+                                                                           <ul class="plain">
+                                                                              <li><a href="/content/dam/bmc/solutions/SWOT+Assessment+-+Remedyforce.pdf" target="_blank" class="">Ovum Consulting: SWOT Assessment: Remedyforce &nbsp;›</a></li>
+                                                                           </ul>
+                                                                        </div>
+                                                                        <div class=" resource-video">
+                                                                           <div class="icon-document"> 
+                                                                           </div>
+                                                                           <strong>Video:</strong> 
+                                                                           <ul class="plain">
+                                                                              <li><a href="/content/bmc/videos.html?vID=KUHmfrl3Ev4" target="_self" class="modal-youtube-video-player">Transform digital service delivery in less than 45 days (1:43) &nbsp;›</a></li>
+                                                                           </ul>
+                                                                        </div>
+                                                                     </div>
+                                                                  </div>
+                                                               </div>
+                                                            </div>
+                                                         </section>
+                                                      </div>
+                                                   </div>
+                                                </div>
+                                             </section>
+                                          </div>
+                                       </div>
+                                    </div>
+                          
+                                 </div>
+                              </section>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </section>
+            </div>
+         </section>
+      </div>
+   </div>
+</div>
+
+
+<?php
+include 'php-inc/foot.php';
+?>
