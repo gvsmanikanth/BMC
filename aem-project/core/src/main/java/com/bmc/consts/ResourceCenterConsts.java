a9ec6a1e6e78c7e0b3723f356b284a16e78f12e3
ayokota <akiyo.yokota@herodigital.com>
2019-05-15 15:08:40 -0700

    adding constants

---
+++ aem-project/core/src/main/java/com/bmc/consts/ResourceCenterConsts.java
@@ -0,0 +1,35 @@
+package com.bmc.consts;
+
+public class ResourceCenterConsts {
+    public static final String loggerName = "recourceCenter";
+
+    public static final String PREDICATE_PREFIX = "group.";
+
+    /* url parameters */
+    public static final String RC_URL_PARAM_PATH = "rootPath";
+    public static final String RC_URL_PARAM_KEYWORD = "keyword";
+    public static final String RC_URL_PARAM_FILTER = "filter";
+    public static final String RC_URL_PARAM_PAGINATION = "pagination";
+    public static final String RC_URL_PARAM_PAGE_INDEX = "pageIndex";
+    public static final String RC_URL_PARAM_RESULTS_PER_PAGE = "resultsPerPage";
+
+    /* query parameters */
+    public static final String QUERY_PARAM_PATH = "path";
+    public static final String QUERY_PARAM_KEYWORD = "keyword";
+    public static final String QUERY_PARAM_FILTER = "filter";
+    public static final String QUERY_PARAM_PAGINATION = "pagination";
+
+
+    /* property fields */
+    public static final String QUERY_PARAM_PROP = "property";
+    public static final String QUERY_PARAM_PROP_VAL = "property.value";
+
+    /* matching mode */
+    public static final String QUERY_PARAM_PROP_OPERATION = "property.operation";
+    public static final String QUERY_PARAM_PROP_OPERATION_LIKE = "like";
+
+    /* pagination */
+    public static final String QUERY_PARAM_PAGE_IDX = "p.offset";
+    public static final String QUERY_PARAM_RESULT_PER_PAGE = "p.limit";
+
+}
