84e392cb4afd7844bfa2423400a8f55cae0cfc15
ADPROD\pthigale <pradip_thigale_tp@bmc.com>
2019-06-12 14:58:38 +0530

    Added condition to make code  'Document Container' specific.

--- front-end/Assets/src/js/modules/document_container.js
+++ front-end/Assets/src/js/modules/document_container.js
@@ -5,14 +5,17 @@
 */
 
 (function($) {
-    function getAvailableHeight() {
-        var actualHeight = window.innerHeight ||
-                          document.documentElement.clientHeight ||
-                          document.body.clientHeight ||
-                          document.body.offsetHeight;
-        return actualHeight;
-    }    
-    var windowHeight = getAvailableHeight();    
-    document.getElementById("pdfReader").style.height = (windowHeight - 80) + "px";
+    if ($('body').hasClass('document_container')) {
+        function getAvailableHeight() {
+            var actualHeight = window.innerHeight ||
+                            document.documentElement.clientHeight ||
+                            document.body.clientHeight ||
+                            document.body.offsetHeight;
+            return actualHeight;
+        }    
+        var windowHeight = getAvailableHeight();    
+        document.getElementById("pdfReader").style.height = (windowHeight - 80) + "px";
+
+    }
 
 })(jQuery);
