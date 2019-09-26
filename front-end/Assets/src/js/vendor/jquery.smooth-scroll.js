55a052467cea3ec4266f55ccea7d829cd1b36d72
ADPROD\pthigale <pradip_thigale_tp@bmc.com>
2019-09-26 13:23:03 +0530

    Code clean up

--- front-end/Assets/src/js/vendor/jquery.smooth-scroll.js
+++ front-end/Assets/src/js/vendor/jquery.smooth-scroll.js
@@ -1,6 +1,6 @@
 // http://css-tricks.com/snippets/jquery/smooth-scrolling/
 $(function() {
-	$('a[href*=#]').not('[href=#], .modal-inline, .no-scroll, .r-tabs-anchor, .accordion-item-anchor, .tab_link').click(function() {
+	$('a[href*=#]').not('[href=#], .modal-inline, .no-scroll, .r-tabs-anchor, .accordion-item-anchor').click(function() {
 		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
