78d844cf341ddab27b6ffa3257816489279a1734
ADPROD\pthigale <pradip_thigale_tp@bmc.com>
2019-11-21 16:41:35 +0530

    Added utility classes for WEB-5910 and WEB-5907

---
+++ front-end/Assets/src/templates/course-page.php
@@ -0,0 +1,218 @@
+<?php
+	$pageTitle = 'Education COURSE page - TrueSight Operations Management Ultimate Subscription - BMC Software';
+	$bodyClass = 'section-education home page-home education ';
+	include 'php-inc/head.php';
+?>
+
+<div class="responsivegrid aem-GridColumn aem-GridColumn--default--12">
+   <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+      <div class="maincontentcontainer responsivegrid aem-GridColumn aem-GridColumn--default--12">
+         <section class="navigation-breadcrumb layout-full-bleed">
+            <span class="cmp cmp-breadcrumb layout-inner-wrap">
+               <ul>
+                  <li class="breadcrumb-item ">
+                     <a href="http://www.bmc.com/education.html">
+                     Training &amp; Certification
+                     </a>
+                  </li>
+                  <li class="breadcrumb-item ">
+                     <a href="http://www.bmc.com/education/courses.html">
+                     Find a Course
+                     </a>
+                  </li>
+                  <li class="breadcrumb-item active">
+                     TrueSight Operations Management Ultimate Subscription
+                  </li>
+               </ul>
+            </span>
+         </section>
+         <section class="aem-Grid aem-Grid--12 aem-Grid--default--12  layout-full-bleed">
+            <div class="75-25contentcontainer aem-GridColumn aem-GridColumn--default--12">
+               <section>
+                  <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 layout-inner-wrap">
+                     <div class="responsivegrid layout-primary aem-GridColumn--phone--12 aem-GridColumn--default--9 aem-GridColumn">
+                        <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+                           <div class="cmp cmp-title aem-GridColumn aem-GridColumn--default--12">
+                              <h1>
+                                 TrueSight Operations Management Ultimate Subscription
+                              </h1>
+                           </div>
+                           <div class="cmp cmp-text aem-GridColumn aem-GridColumn--default--12">
+                              <p><b>TrueSight Operations Management: Ultimate Subscription</b> provides continuous access to all TrueSight Operations Management training material, at one price, for a 12-month period.</p>
+                              <p>The major benefits of the subscription are:</p>
+                              <div class="md-flex">
+                                 <div class="flex-item md-col-6">
+                                    <ul>
+                                       <li><b>Get More</b> – Continuous access to entire education portfolio for one product including all courses and certification exams</li>
+                                       <li><b>Future Proof</b> - New content and latest releases automatically added at no additional cost so your subscription is always up to date</li>
+                                       <li><b>Exclusive Content</b> - Access exclusive education services e.g. 6 hours of expert coaching annually</li>
+                                    </ul>
+                                 </div>
+                                 <div class="flex-item md-col-6">
+                                    <ul>
+                                       <li><b>Save Money</b> – Save up to 74% over the cost of purchasing the same content separately</li>
+                                       <li><b>Ease of Use</b> – One-stop-shop to a complete product portfolio of education</li>
+                                       <li><b>Flexibility</b> – Train on what you need, when you need it, and how you want to access - multiple learning options (ASP, WBT, Instructor Led)</li>
+                                    </ul>
+                                 </div>
+                              </div>
+                              <p>This is an annual subscription and is regularly updated and changed, without notice, based on product updates, improvements, and additions. During the duration of the subscription, users will have access to the latest content without incurring additional fees or being subject to additional registrations.</p>
+                           </div>
+                           <div class="CTA-button-row aem-GridColumn aem-GridColumn--default--12">
+                              <div class="flex-grid">
+                                 <div class="lg-flex">
+                                    <div class="lg-col-9">
+                                       <a class="btn learn-more" href="https://www.bmc.com/forms/contact-bmc-education.html?productInterest=truesight%20operations%20management" title="Contact an Education Advisor" data-inc="1">Contact an Education Advisor</a>
+                                       <a class="btn-secondary learn-more" href="https://bmc.csod.com/default.aspx?c=ab&amp;loid=e7beed1b-4656-43f7-b99f-662fec47db3b" title="Pricing &amp; Registration" target="_blank">Pricing &amp; Registration</a>
+                                    </div>
+                                    <div class="lg-col-3">
+                                       <a class="course-abstract link-icon-pdf" href="https://www.bmc.com/content/dam/bmc/education/Abstract_TSOM-ULT-SUBS.pdf" title="Course Abstract" target="_blank">Course Abstract</a>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="course-data-grid aem-GridColumn aem-GridColumn--default--12">
+                              <div class="row">
+                                 <div class="two-up">
+                                    <div>
+                                       <h3>Major release:</h3>
+                                    </div>
+                                    <div>
+                                       <p>TrueSight Operations Management 10.x, 11.x</p>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="row">
+                                 <div class="two-up">
+                                    <div>
+                                       <h3>Good for:</h3>
+                                    </div>
+                                    <div>
+                                       <p>Administrators, Operators</p>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="htmlarea aem-GridColumn aem-GridColumn--default--12">
+                              <h3>Learner Objectives </h3>
+                              <div class="md-flex">
+                                 <div class="flex-item md-col-6">
+                                    <ul>
+                                       <li>Get familiar with the TrueSight Operations Management</li>
+                                       <li>Understand concepts and architecture of TrueSight Operations Management and its components </li>
+                                       <li>Learn to install, configure and administer TrueSight Operations Management components</li>
+                                    </ul>
+                                 </div>
+                                 <div class="flex-item md-col-6">
+                                    <ul>
+                                       <li>Understand features and functionalities of TrueSight Operations Management</li>
+                                       <li>Learn to use TrueSight Operations Management effectively to increase efficiency of your business </li>
+                                       <li>Get certified to implement, operate and support TrueSight Operations Management</li>
+                                    </ul>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="htmlarea aem-GridColumn aem-GridColumn--default--12">
+                              <h2 class="h1-variation-courses">Subscription Courses</h2>
+                              <h3>Offerings:</h3>
+                              <div class="md-flex">
+                                 <div class="flex-item md-col-6">
+                                    <ul>
+                                       <li>ILT Course 1: BMC TrueSight Operations Management 11.x: Fundamentals Installing</li>
+                                       <li>ILT Course 2: BMC TrueSight Operations Management 11.x: Fundamentals Operating</li>
+                                       <li>ILT Course 3: BMC TrueSight Operations Management 11.x: Fundamentals Administering</li>
+                                       <li>ILT Course 4: BMC TrueSight Operations Management 10.x: Expert Level Training</li>
+                                       <li>ILT Course 5: BMC TrueSight Operations Management 10.x: Fundamentals for Administrators – Part 1</li>
+                                       <li>ILT Course 6: BMC TrueSight Operations Management 10.x: Fundamentals for Administrators – Part 2</li>
+                                       <li>ILT Course 7: BMC TrueSight Operations Management 11.x: Advanced Event Management</li>
+                                       <li>ILT Course 8: BMC TrueSight Operations Management 11.x: Advanced Training - Service Modelling</li>
+                                       <li>WBT Course 1: TrueSight Operations Management 10.7: Using New Features</li>
+                                       <li>WBT Course 2: TrueSight Operations Management 10.5 Integration with IT Data Analytics (WBT)</li>
+                                       <li>WBT Course 3: TrueSight Operations Management 10.5: Working with the TrueSight Console (WBT)</li>
+                                       <li>WBT Course 4: TrueSight Operations Management 10.x: Advanced Training for Oracle KM (WBT)</li>
+                                       <li>WBT Course 5: BMC TrueSight Operations Management 10.5: What’s New (WBT)</li>
+                                    </ul>
+                                 </div>
+                                 <div class="flex-item md-col-6">
+                                    <ul>
+                                       <li>WBT Course 6: BMC TrueSight Operations Management 10.0: Getting Started (WBT)</li>
+                                       <li>WBT Course 7: BMC TrueSight Operations Management 11.0: Using New Features (WBT)</li>
+                                       <li>WBT Course 8: BMC TrueSight Operations Management 11.x: Fundamentals Architecture and Components (WBT)</li>
+                                       <li>ASP Course 1: BMC TrueSight Operations Management 10.x: Fundamentals for Administrators – Part 1 (ASP)</li>
+                                       <li>ASP Course 2: BMC TrueSight Operations Management 10.x: Fundamentals for Administrators – Part 2 (ASP)</li>
+                                       <li>ASP Course 3: BMC TrueSight Operations Management 10.x: Installation and Configuration – Fundamentals (ASP)</li>
+                                       <li>ASP Course 4: BMC TrueSight Operations Management 10.x: Walkthrough of Consoles – Fundamentals (ASP)</li>
+                                       <li>ASP Course 5: BMC TrueSight Operations Management 10.x: Analytics – Fundamentals (ASP)</li>
+                                       <li>ASP Course 6: BMC TrueSight Operations Management 10.x: Event Management – Fundamentals (ASP)</li>
+                                       <li>ASP Course 7: BMC TrueSight Operations Management 10.x: Service Modeling – Fundamentals (ASP)</li>
+                                       <li>ASP Course 8: BMC TrueSight Operations Management 11.x: Fundamentals Installing (ASP)</li>
+                                       <li>ASP Course 9: BMC TrueSight Operations Management 11.x: Fundamentals Operating (ASP)</li>
+                                    </ul>
+                                 </div>
+                              </div>
+                              <h3>Certifications:</h3>
+                              <div class="md-flex">
+                                 <div class="flex-item md-col-6">
+                                    <ul>
+                                       <li>BMC Certified Associate: TrueSight Operations Management 10.x Online Exam</li>
+                                       <li>BMC Certified Professional: TrueSight Operations Management 10.x Online Proctored Exam</li>
+                                       <li>BMC Certified Expert: TrueSight Operations Management 10.x Online Proctored Exam</li>
+                                       <li>BMC Certified Professional: TrueSight Operations Management 11.x Online Proctored Exam</li>
+                                    </ul>
+                                 </div>
+                                 <div class="flex-item md-col-6">
+                                    <ul>
+                                       <li>BMC Certified Associate: TrueSight Operations Management 11.x for Operators Online Exam</li>
+                                       <li>BMC Certified Associate: TrueSight Operations Management 11.x for Administrators Online Exam</li>
+                                       <li>BMC Certified Associate: TrueSight Operations Management 11.x for Consultants Online Exam</li>
+                                    </ul>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="responsivegrid aem-GridColumn--phone--12 aem-GridColumn aem-GridColumn--default--3 layout-secondary">
+                        <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+                           <div class="htmlarea aem-GridColumn aem-GridColumn--default--12">
+                              <section>
+                                 <h3>Related Learning Paths</h3>
+                                 <ul class="plain">
+                                    <li><a href="/education/courses/truesight-operations-management-training.html" target="_self"><b>TrueSight Operations Management Training</b></a></li>
+                                 </ul>
+                              </section>
+                           </div>
+                           <div class="htmlarea aem-GridColumn aem-GridColumn--default--12">
+                              <h3>Related Skills Assessments</h3>
+                              <ul class="plain">
+                                 <li><a href="https://bmc.csod.com/default.aspx?c=ab&amp;loid=2684c55d-3a95-41f0-bfce-399b7e7f3299" target="_blank"><b>TrueSight Operations Management 10.x: Skills Assessment</b></a></li>
+                                 <li><a href="https://bmc.csod.com/default.aspx?c=ab&amp;loid=42511373-a9fd-4c69-b424-3481a8d423dd" target="_blank"><b>TrueSight Operations Management 11.x: Skills Assessment</b></a></li>
+                              </ul>
+                           </div>
+                           <div class="htmlarea aem-GridColumn aem-GridColumn--default--12">
+                              <h3>New subscription services</h3>
+                              <ul class="plain">
+                                 <li><b>TrueSight Operations Management: Explore Subscription</b> provides continuous access to introduction training material, free of charge, for a 12-month period.<br><a href="/education/courses/truesight-operations-management-explore-subscription.html" target="_blank"><b>See details &gt;</b></a></li>
+                              </ul>
+                           </div>
+                           <div class="htmlarea aem-GridColumn aem-GridColumn--default--12">
+                              <h3>Other Resources</h3>
+                              <ul class="plain">
+                                 <li><a href="http://documents.bmc.com/products/documents/09/96/500996/500996.pdf" target="_blank"><b>Subscription Service Overview ›</b></a></li>
+                              </ul>
+                              <ul class="plain">
+                                 <li><a href="https://www.bmc.com/content/dam/bmc/education/Subscriptions_Program_Guide.pdf" target="_blank"><b>Subscription Program Guide  ›</b></a></li>
+                              </ul>
+                           </div>
+                           <div class="experiencefragment aem-GridColumn aem-GridColumn--default--12">
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </section>
+            </div>
+         </section>
+      </div>
+   </div>
+</div>
+
+<?php include 'php-inc/foot.php'; ?>
