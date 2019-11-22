95d7faa322f3d942fbde95e50e8d986a50b40c65
Mahesh <mahesh_kotapali_tp@bmc.com>
2019-11-22 15:17:51 +0530

    WEB-6632 - Adjust Footer Height

--- front-end/Assets/src/templates/trials.php
+++ front-end/Assets/src/templates/trials.php
@@ -3,6 +3,56 @@
 	$bodyClass = 'contact full-width resource-hub';
 	include_once 'php-inc/head.php'; 
 ?>
+<!--<style>
+.product-list {
+    background-color: #fafafa;
+    padding: 5rem 0;
+	.flex-item{
+		transition: all .2s ease-in-out;
+		.cart-width {
+			margin: 15px;
+			width: 100%;
+			box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.12);
+			.card {
+				padding-bottom: 80px;
+				padding: 24px;
+				-webkit-transition: .3s linear;
+				transition: .3s linear;
+				.card-header{
+					h2{
+						color: #fe5000;
+						line-height: 1.3em;
+						font-size: 1rem;
+						font-weight: 400;
+					}
+				}
+				.card-content {
+					clear: both;
+					padding: 1rem 0;
+					p{
+						font-size: .847em;
+						color: #555557;
+						line-height: 1.6em;
+					}
+				}
+				.card-footer{
+					position: absolute;
+					bottom: 0.5rem;
+					right: 1rem;
+					color: #fe5000;
+				}
+			}
+			&:hover {
+				box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+			}
+		}
+		&:hover{
+			transform: scale(1.01);
+		}
+	}
+}
+</style>-->
+
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.js'></script>
 <section class="wallpapered bar full-bleed-page-banner product-category-header padding-bottom-0 wallpaper" data-wallpaper-options="{&quot;source&quot;:&quot;/content/dam/bmc/solutions/banners/tbn-corporate-free-trial.jpg?ab=2&quot;}">
@@ -29,6 +79,79 @@
 		</div>
 	</div>
 <div class="wallpaper-container"><div class="wallpaper-media wallpaper-image native" style="background-image: url(&quot;/content/dam/bmc/solutions/banners/tbn-corporate-free-trial.jpg?ab=2&quot;); height: 701.105px; width: 1903px; left: 0px; top: -132.553px; opacity: 1;"><img alt="" src="/content/dam/bmc/solutions/banners/tbn-corporate-free-trial.jpg?ab=2"></div></div></section>
+
+<!--<section class="product-list ">
+   <div class="layout-inner-wrap" id="product-list">
+     <div class="flex-wrap flex-justify-center mb2 js-eh">
+         <div class="flex flex-item col-12 sm-col sm-col-6 md-col-4 lg-col-3">
+            <a href="/it-solutions/bmc-helix-itsm.html" class="cart-width">
+               <div class="card bg-white title ">
+                  <div class="card-header heading">
+                     <h2>BMC Helix ITSM</h2>
+                  </div>
+                  <div class="card-content">
+                     <p>Predictive IT service management for your modern enterprise Predictive Predictive IT service management for your modern enterprise Predictive</p>
+                  </div>
+                  <div class="card-footer">
+                     <span class="">Start a Free Trial</span>
+                  </div>
+               </div>
+            </a>
+         </div>
+         <div class="flex flex-item col-12 sm-col sm-col-6 md-col-4 lg-col-3">
+            <a href="/it-solutions/bmc-helix-discovery.html" class="cart-width">
+               <div class="card bg-white title ">
+                  <div class="card-header heading">
+                     <h2>BMC Helix Discovery</h2>
+                  </div>
+                  <div class="card-content">
+                     <p>Automatic discovery of data center and multi-cloud inventory, configuration, and relationship data</p>
+                  </div>
+                  <div class="card-footer">
+                     <span class="">Start a Free Trial</span>
+                  </div>
+               </div>
+            </a>
+         </div>
+         <div class="flex flex-item col-12 sm-col sm-col-6 md-col-4 lg-col-3">
+            <a href="/it-solutions/digital-workplace.html" class="cart-width">
+               <div class="card bg-white title ">
+                  <div class="card-header heading">
+                     <h2>BMC Helix Digital Workplace</h2>
+                  </div>
+                  <div class="card-content">
+                     <p>Engaging omni-channel experiences to support employees however they want to work</p>
+                  </div>
+                  <div class="card-footer">
+                     <span class="">Start a Free Trial</span>
+                  </div>
+               </div>
+            </a>
+         </div>
+         <div class="flex flex-item col-12 sm-col sm-col-6 md-col-4 lg-col-3">
+            <a href="/it-solutions/bmc-helix-chatbot.html" class="cart-width">
+               <div class="card bg-white title ">
+                  <div class="card-header heading">
+                     <h2>BMC Helix Chatbot</h2>
+                  </div>
+                  <div class="card-content">
+                     <p>Intelligent, conversational, omni-channel experiences for users to find and request services</p>
+                  </div>
+                  <div class="card-footer">
+                     <span class="">Start a Free Trial</span>
+                  </div>
+               </div>
+            </a>
+         </div>					
+      </div>
+   </div>
+</section>-->
+<section class="bg-white filterListContainer" style="">
+	<div class="layout-inner-wrap py2">
+		<div class="cards-wrapper resource-hub-trials-cards" style="opacity: 1;"><div class="list-count text-center"><h5>1 - 16 of 16</h5><h5></h5></div><div class="cards-4-col js-eh"><div class="flex-item"><a style="height:100%" href="/forms/bmc-helix-itsm-trial.html" data-content_weighting="1" data-content_publish_date="" data-content_core_topic="Service Management" data-content_market_topic="Service Management" data-product_code="Remedy" data-product_line_code="DSM" data-product_listprop="Remedy" data-product_line="DSM" data-product_line_list_3="DSM" data-resource_type="trials" data-resource_inclusion_criteria="gate" data-target_buyer_stage="buy" data-target_persona="Director|Manager|IT Professional" data-target_industry="" data-target_company_size="" data-target_language="en-us"><div class="resource-container"><ul class="list-group"><li class="title row"><div class="heading" style="height: 46px;">Testversion von BMC Helix ITSM</div></li><li class="resource-details row js-ehItem" style="height: 152px;">Vorausschauendes ITSM mit intelligenten, anwenderorientierten Benutzererlebnissen, die Ihnen helfen, effizienter zu arbeiten</li><li class="resource-audience row"><span>Kostenlose Testversion starten</span></li></ul></div> </a></div><div class="flex-item"><a style="height:100%" href="/forms/control-m-enterprise-free-trial-runaware.html" data-content_weighting="1" data-content_publish_date="" data-content_core_topic="IT Automation" data-content_market_topic="Business &amp; IT Automation" data-product_code="Control M" data-product_line_code="DBA" data-product_listprop="Control M" data-product_line="DBA" data-product_line_list_3="DBA" data-resource_type="trials" data-resource_inclusion_criteria="gate" data-target_buyer_stage="buy" data-target_persona="Director|Manager|IT Professional" data-target_industry="" data-target_company_size="" data-target_language="en-us"><div class="resource-container"><ul class="list-group"><li class="title row"><div class="heading" style="height: 46px;">Testversion von Control-M</div></li><li class="resource-details row js-ehItem" style="height: 152px;">Beschleunigt die Bereitstellung digitaler Services mit einfachem, einheitlichem Batchmanagement</li><li class="resource-audience row"><span>Kostenlose Testversion starten</span></li></ul></div> </a></div><div class="flex-item"><a style="height:100%" href="/forms/truesight-operations-management-free-trial.html" data-content_weighting="1" data-content_publish_date="" data-content_core_topic="IT Operations" data-content_market_topic="AI &amp; Machine Learning|IT Operations Management" data-product_code="TrueSight Infrastructure" data-product_line_code="P&amp;A" data-product_listprop="TrueSight Infrastructure" data-product_line="P&amp;A" data-product_line_list_3="P&amp;A" data-resource_type="trials" data-resource_inclusion_criteria="gate" data-target_buyer_stage="buy" data-target_persona="Director|Manager|IT Professional" data-target_industry="" data-target_company_size="" data-target_language="en-us"><div class="resource-container"><ul class="list-group"><li class="title row"><div class="heading" style="height: 46px;">Testversion von TrueSight Operations Management</div></li><li class="resource-details row js-ehItem" style="height: 152px;">Analysebasiertes Performance Management zum Aufspüren und Lösen von Leistungs- und Verfügbarkeitsproblemen</li><li class="resource-audience row"><span>Kostenlose Testversion starten</span></li></ul></div> </a></div><div class="flex-item"><a style="height:100%" href="/forms/bmc-discovery-free-trial-download.html" data-content_weighting="1" data-content_publish_date="" data-content_core_topic="Service Management" data-content_market_topic="Service Management" data-product_code="Discovery" data-product_line_code="DSM" data-product_listprop="Discovery" data-product_line="DSM" data-product_line_list_3="DSM" data-resource_type="trials" data-resource_inclusion_criteria="gate" data-target_buyer_stage="buy" data-target_persona="Director|Manager|IT Professional" data-target_industry="" data-target_company_size="" data-target_language="en-us"><div class="resource-container"><ul class="list-group"><li class="title row"><div class="heading" style="height: 46px;">Testversion von BMC Helix Discovery</div></li><li class="resource-details row js-ehItem" style="height: 152px;">Automatische Inventarisierung der Daten aus Rechenzentren, Multi-Cloud-Umgebungen, Konfigurationen und Beziehungen.</li><li class="resource-audience row"><span>Kostenlose Testversion starten</span></li></ul></div> </a></div></div></div>
+
+	</div>
+</section>
 
 
 
