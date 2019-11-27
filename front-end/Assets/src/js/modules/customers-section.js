736ece780ef5b543ad4d0044c73700dd64c8885c
ADPROD\pthigale <pradip_thigale_tp@bmc.com>
2019-11-27 15:06:37 +0530

    condition added to exclude the contact page

--- front-end/Assets/src/js/modules/customers-section.js
+++ front-end/Assets/src/js/modules/customers-section.js
@@ -1,6 +1,6 @@
 ;( function($) {
     if(($(".customer_section").length) > 0 ){
-        if (window.location.href.indexOf("contact-us-forms-new") > -1) { 
+        if (window.location.href.indexOf("contact-bmc") > -1) { 
             if(($("#talk_to_bmc").length) > 0 ){
                 $("#talk_to_bmc").attr("href", "#leadgen");
             }            
