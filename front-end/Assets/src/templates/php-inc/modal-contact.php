3fbe2cd35748f0067c3d9752963e51558b89bf6e
ADPROD\pthigale <pradip_thigale_tp@bmc.com>
2020-01-02 16:57:50 +0530

    updated the cards with the links

--- front-end/Assets/src/templates/php-inc/modal-contact.php
+++ front-end/Assets/src/templates/php-inc/modal-contact.php
@@ -7,23 +7,23 @@
 		</div>
 		<div class="contact-options">
 			<div class="flex-wrap">
-                <div class="flex flex-item col-12 sm-col sm-col-6">
-                    <div class="content-box p2 bg-white">
-                        <img src="/front-end/Assets/src/img/Grey_Sales Edge.svg" alt="">
+                <div class="flex flex-item col-12 sm-col sm-col-6">                   
+                    <a href="//www.bmc.com/forms/contact-bmc.html" class="content-box p2 bg-white">                        
+                        <img src="/front-end/Assets/src/img/Grey_Sales Edge.svg" alt="Sales & Pricing">
                         <h3>Sales & Pricing</h3>
                         <hr>
                         <p>Speak to a rep about your business needs</p>
-                        <a href="//www.bmc.com/forms/contact-bmc.html" class="btn learn-more">Contact Sales</a>
-                    </div>                    
+                        <span class="btn learn-more">Contact Sales</span>                                        
+                    </a>                             
                 </div>    
                 <div class="flex flex-item col-12 sm-col sm-col-6">
-                    <div class="content-box p2 bg-white">
-                        <img src="/front-end/Assets/src/img/Customer_service_grey.svg" alt="">
+                    <a href="//www.bmc.com/contacts-locations/contact-bmc.html" class="content-box p2 bg-white">                        
+                        <img src="/front-end/Assets/src/img/Customer_service_grey.svg" alt="Help & Support">
                         <h3>Help & Support</h3>
                         <hr>
                         <p>See our product support options</p>
-                        <a href="//www.bmc.com/contacts-locations/contact-bmc.html" class="btn learn-more">Contact Support</a>
-                    </div>                    
+                        <span  class="btn learn-more">Contact Support</span>  
+                    </a>                  
                 </div>              
 			</div>
 			<div class="modal-footer text-center">
