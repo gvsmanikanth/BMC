4b3d5e77525518ea1a1dcf4b72d1cdd93d2c56ee
Mahesh <mahesh_kotapali_tp@bmc.com>
2020-02-13 18:01:05 +0530

    WEB-4036 - WEB-7254 fixed

--- front-end/Assets/src/templates/brandAlignmentComponent.php
+++ front-end/Assets/src/templates/brandAlignmentComponent.php
@@ -16,134 +16,188 @@
 }
 </style>
 <?php include 'php-inc/svg.php'; ?>
-<section class="wallpapered bar full-bleed-page-banner product-category-header product-category-header2   padding-bottom-0 wallpaper" data-wallpaper-options="{&quot;source&quot;:{&quot;fallback&quot;: &quot;/content/dam/bmc/header-mobile-images/Green-700x700px.jpg&quot;,&quot;(min-width: 640px)&quot;: &quot;/content/dam/bmc/solutions/banners/tb-truesight-orchestration.jpg&quot;}}" style="background-color:#4C9B00;">
- 	<div class="offset-wrapper flex-center ">
-		<div class="inner">
-			<div class="bannerContent flex-center">
-				<div class="layout-inner-wrap">
-					<div class="flex-item col col-12 md-col-6 sm-col-8 add-top-bottom-margin-140px">
-       						<h1 class="white">TrueSight Orchestration</h1>
-                            <h2>Reduce costs and errors through IT process automation</h2>
-                            <div class="button-wrapper">
-                                <a class="modal-youtube-video-player btn btn-secondary  mr1" href="/content/bmc/videos.html?vID=d8pMh5Cf5ac" data-location="Top-CTA-Button-Area">
-                                    Watch overview (2:14)
-                                    
-										<svg class="align-middle" height="1.2em" viewBox="0 0 100 100" style="padding-left: .5rem;fill: #333;"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#s-VideoPlay"></use></svg>
-									
-								</a>
-                                   
-                            </div>
-					</div>
-				</div>
-             </div>
+<div class="responsivegrid aem-GridColumn aem-GridColumn--default--12">
+<div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+    
+    <div class="maincontentcontainer responsivegrid aem-GridColumn aem-GridColumn--default--12">
+
+    
+    <section class="aem-Grid aem-Grid--12 aem-Grid--default--12  layout-full-bleed">
+        
+        
+            <div class="75-25contentcontainer aem-GridColumn aem-GridColumn--default--12"><section>
+    <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 layout-inner-wrap">
+     
+        <div class="responsivegrid layout-primary aem-GridColumn--phone--12 aem-GridColumn--default--9 aem-GridColumn">
+<div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+    
+    <div class="cmp cmp-title aem-GridColumn aem-GridColumn--default--12">
+<h2>
+    Business Challenge</h2>
+
+    
+
+</div>
+<div class="cmp cmp-text aem-GridColumn aem-GridColumn--default--12">
+
+<p>Medical professionals worldwide use Boston Scientific Corporation’s stents, catheters, pacemakers, and other products to treat over 22 million patients a year. For products such as pacemakers and stents, the company has committed that orders received will be delivered by the next morning. Failure to meet that commitment could delay a medical procedure and negatively affect patient health. IT has created a global command center that monitors the infrastructure and handles incidents for all Boston Scientific facilities worldwide to ensure the availability and performance of its IT infrastructure, as well as guarantee rapid recovery from problems.</p>
 
 
+    
 
-<!-- /* If not redesign */-->
-	
+</div>
+<div class="cmp cmp-title aem-GridColumn aem-GridColumn--default--12">
+<h2>
+    BMC Solution</h2>
 
-<!--/* Asset bars and Orange bar common for both the designs */ -->
+    
+
+</div>
+<div class="cmp cmp-text aem-GridColumn aem-GridColumn--default--12">
+
+<p>TrueSight proactively monitors the Boston Scientific IT environment that powers manufacturing execution, inventory control, distribution, communication, finance, and other essential business functions.</p>
+
+
+    
+
+</div>
+<div class="cmp cmp-title aem-GridColumn aem-GridColumn--default--12">
+<h2>
+    Business Impact</h2>
+
+    
+
+</div>
+<div class="cmp cmp-text aem-GridColumn aem-GridColumn--default--12">
+
+<p><a href="/it-solutions/truesight.html" data-ic-weighting="5" data-product-interest="Unknown" data-ic-app-inclusion="no" data-product-line="DSO" data-topics="Operations">TrueSight</a> enables IT to respond quickly to issues that could delay the manufacturing or delivery of products that medical professionals need to ensure positive outcomes for their patients.</p>
+<ul>
+<li>TrueSight’s rich, visual interface is intuitive, easy to use, and provides drill-down capabilities for additional details on impending issues.</li>
+<li>One-third of critical tickets are now intercepted by the command center and addressed proactively, accelerating response and resolution times.</li>
+<li>Dashboards, reports, and alerts enable monitoring of vital applications with just two full-time employees keeping tabs on its infrastructure and application health.</li>
+</ul>
+
+
+    
+
+</div>
+<div class="pullquote aem-GridColumn aem-GridColumn--default--12">
+
+
+<div>
+<blockquote class="testimonial divider">
+    <div class="layout-inner-wrap">
+
+        
+        
+            <p>“We’re heading toward the integration of TrueSight with Remedy Service Management and the CMDB, so we can implement autoticketing. Automation will help us streamline our processes and improve service and communication across the board. So it’s a bright future.”</p>
+        
+
+        <cite>
+            — Patrick McDonagh, Senior IT Infrastructure Manager
+        </cite>
+
+        
+
+        
+    </div>
+</blockquote>
+</div>
+</div>
+
+    
+</div></div>
+
+        <div class="responsivegrid aem-GridColumn--phone--12 aem-GridColumn aem-GridColumn--default--3 layout-secondary">
+<div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+    
+    <div class="responsivegrid aem-GridColumn aem-GridColumn--default--12">
+<div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+    
+    <div class="cmp cmp-title aem-GridColumn aem-GridColumn--default--12">
+<h2>
+    Let's discuss your transformation</h2>
+
+    
+
+</div>
+<div class="experiencefragment aem-GridColumn aem-GridColumn--default--12">
+
+    
+    
+
+<div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+    
+    <div class="CTAbutton aem-GridColumn aem-GridColumn--default--12">
+
+<a class="aem--inline-block aem--no-grid btn btn-corporate learn-more " href="/forms/contact-bmc.html" data-ic-weighting="5" data-product-interest="Unknown" data-ic-app-inclusion="no" data-product-line="Unknown" data-inc="1">
+    Contact Sales
+    
+</a>
+</div>
+
+    
+</div>
+</div>
+<div class="CTAbutton aem-GridColumn aem-GridColumn--default--12">
+
+<a class="aem--inline-block aem--no-grid btn learn-more " href="http://documents.bmc.com/products/documents/71/76/497176/497176.pdf" data-ic-weighting="5" data-ic-app-inclusion="no" target="_blank">
+    Download PDF
+    
+</a>
+</div>
+
+    
+</div></div>
+<div class="related-CTAs-list aem-GridColumn aem-GridColumn--default--12"><section>
+    
+    <h2>Featured offerings:</h2>
+    
+        <h3><a href="http://staging.bmc.com/it-solutions/truesight-operations-management.html" data-ic-weighting="5" data-product-interest="TrueSight Infrastructure" data-ic-app-inclusion="no" data-product-line="DSO" data-topics="Operations">TrueSight Operations Management</a></h3>
+        <p>Machine learning and advanced analytics for holistic monitoring and event management</p>
+        <a href="http://staging.bmc.com/it-solutions/truesight-operations-management.html" data-ic-weighting="5" data-product-interest="TrueSight Infrastructure" data-ic-app-inclusion="no" data-product-line="DSO" data-topics="Operations">Learn More</a>
+        
+            <span> | </span>
+            
 			
-			
-			
-			
-			
-			    
-			
+                    
+               
+             
+            <a href="http://staging.bmc.com/forms/truesight-operations-management-free-trial.html" data-ic-weighting="3" data-product-interest="TrueSight Infrastructure" data-ic-app-inclusion="gate" data-ic-target-persona="Director|Manager|IT Professional" data-ic-content-type="Trials" data-ic-topics="AI &amp; Machine Learning|IT Operations Management" data-ic-buyer-stage="Buy" data-topics="Operations" data-inc="1"> Try TrueSight Operations Management for Free</a>
+        
+    
+        <h3><a href="http://staging.bmc.com/it-solutions/bmc-helix-itsm.html" data-ic-weighting="5" data-product-interest="Helix ITSM" data-ic-app-inclusion="no" data-product-line="DSM" data-topics="Service Management">BMC Helix ITSM</a></h3>
+        <p>Predictive IT service management for your modern enterprise</p>
+        <a href="http://staging.bmc.com/it-solutions/bmc-helix-itsm.html" data-ic-weighting="5" data-product-interest="Helix ITSM" data-ic-app-inclusion="no" data-product-line="DSM" data-topics="Service Management">Learn More</a>
+        
+            <span> | </span>
+            
+			  
+             
+                 
+             
+            <a href="/forms/bmc-helix-itsm-trial.html" data-ic-weighting="5" data-ic-app-inclusion="no" data-inc="1">Try BMC Helix ITSM </a>
+        
+    
+      
+    
+</section>
 
-			<div class="assetStrip">
-				<div class="inner">
-					<div class="sm-flex flex-justify-center contentContainer">
-						 
-                        
+</div>
 
-                        
+    
+</div></div>
 
-                            
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                            
-                        
+	 
+    </div>
+</section></div>
 
+        
 
-                        <a href="http://staging.bmc.com/forms/it-process-automation-buyers-guide-ebook.html?productInterest=truesight%20orchestration" class="flex-item sm-col-4   shadow-top assetStripItem" target="_blank" data-inc="1">
-							<div class="inner assetTxt">
-								<div class="flex-center">
-									<img src="/content/dam/bmc/solutions/icons/icon-pdf.png" alt="">
-									<div class="inner">
-										<h5>E-book:</h5>
-										<p>Accelerate your Digital Transformation Journey</p>
-										<p class="link">Read now&nbsp;›</p>
-									</div>
-								</div>
-							</div>
-						</a> 
-						 
-                        
+        
+    </section>
+</div>
 
-                        
-
-                            
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                            
-                        
-
-
-                        <a href="http://staging.bmc.com/forms/truesight-server-automation-forrester-wave-analyst-report.html?productInterest=truesight%20orchestration" class="flex-item sm-col-4   shadow-top assetStripItem" data-ic-weighting="5" data-ic-app-inclusion="no" target="_blank" data-inc="1">
-							<div class="inner assetTxt">
-								<div class="flex-center">
-									<img src="/content/dam/bmc/solutions/icons/icon-pdf.png" alt="">
-									<div class="inner">
-										<h5>Analyst report:</h5>
-										<p>The Forrester Wave™: Infrastructure Automation Platforms, Q3 2019</p>
-										<p class="link">Download now&nbsp;›</p>
-									</div>
-								</div>
-							</div>
-						</a> 
-						 
-                        
-
-                        
-
-                            
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                            
-                        
-
-
-                        <a href="http://staging.bmc.com/forms/automated-operations-global-esg-report.html?productInterest=truesight%20orchestration" class="flex-item sm-col-4   shadow-top assetStripItem" data-ic-weighting="5" data-ic-app-inclusion="no" target="_blank" data-inc="1">
-							<div class="inner assetTxt">
-								<div class="flex-center">
-									<img src="/content/dam/bmc/solutions/icons/icon-pdf.png" alt="">
-									<div class="inner">
-										<h5>Analyst research:</h5>
-										<p>ESG: Achieving Automated Operations with TrueSight</p>
-										<p class="link">Download now&nbsp;›</p>
-									</div>
-								</div>
-							</div>
-						</a> 
-						
-					</div>
-				</div>
-			</div>
-</div></div><div class="wallpaper-container"><div class="wallpaper-media wallpaper-image wallpaper-responsive native" style="background-image: url(&quot;/content/dam/bmc/solutions/banners/tb-truesight-orchestration.jpg&quot;); height: 701.105px; width: 1903px; left: 0px; top: -52.0526px; opacity: 1;"><img alt="" src="/content/dam/bmc/solutions/banners/tb-truesight-orchestration.jpg"></div></div></section>
+    
+</div></div>
 <?php include 'php-inc/foot.php'; ?>
