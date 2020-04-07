83da8783cbb934fabb725e6d96ae7f02af54b408
ADPROD\pthigale <pradip_thigale_tp@bmc.com>
2020-04-07 22:23:11 +0530

    carousel bg and icon images seprated

--- front-end/Assets/src/templates/homepage-2020.php
+++ front-end/Assets/src/templates/homepage-2020.php
@@ -2,11 +2,11 @@
  <div class="homepage-2020 bg-white">   
     <div class="md-flex">
         <div class="flex-item md-col-7 right-col">
-            <div class="jumbotron">
-                <div class="flex">
-                    <div class="flex-item md-col-8">
+            <div class="jumbotron flex flex-center">
+                <div class="sm-flex">
+                    <div class="flex-item md-col-7 lg-col-8">
                         <h2 class="jTitle">Become an Autonomous Digital Enterprise</h2>
-                        <div class="md-flex">
+                        <div class="flex">
                             <div class="flex-item md-col-4 jHr"><hr></div>
                             <div class="flex-item md-col-8 jCont">
                                 <p>Drive an innovative mindset through agility, customer centricity, and actionable insights to deliver growth and thrive in a disruptive market.</p>
@@ -14,7 +14,7 @@
                             </div>
                         </div>
                     </div> 
-                    <div class="flex-item md-col-4"> 
+                    <div class="flex-item md-col-5 lg-col-4 mobile-hidden"> 
                         <img src="/front-end/Assets/src/img/homepage-2020/symbol.svg" alt="">
                     </div> 
                 </div>             
@@ -26,7 +26,7 @@
                         <div class="">
                             <img src="/front-end/Assets/src/img/homepage-2020/automation-icon.png" alt="" class="hcIcon">
                             <h3>Automation everywhere</h3>
-                            <p>Intelligent automation across cloud, distributed systems, and mainframe. </p>
+                            <p>Intelligent automation across cloud, distributed systems, and mainframe.  </p>
                             <a href="#." class="hpLearnMore">Learn More</a>
                         </div>
                     </div>
@@ -43,28 +43,44 @@
         </div>
         <div class="flex-item md-col-5 left-col">            
             <div class="owl-carousel-homepage">                              
-		        <div class="item " style="background-image: url('/front-end/Assets/src/img/homepage-2020/vector.png');">
-                    <div class="carouselCaption">
-                        <img src="/front-end/Assets/src/img/homepage-2020/Frame.svg" alt="" class="hpIcon">
-                        <h4>Run Your Mainframe as You Reinvent It with BMC AMI</h4>
-                        <p>Deliver on the promise of the self-managing mainframe</p>
-                        <a href="#." class="hpLearnMore">Learn More</a>
+                <div class="item " style="background-image: url('/front-end/Assets/src/img/homepage-2020/vector.png');">
+                    <div class="flex car-flex">
+                        <div class="carousel-image  mobile-hidden">
+                            <img src="/front-end/Assets/src/img/homepage-2020/Chatbot_2s_300x261px.gif" alt="">
+                        </div>    
+                        <div class="carouselCaption">
+                            <img src="/front-end/Assets/src/img/homepage-2020/Frame.svg" alt="" class="hpIcon">
+                            <h4>Run Your Mainframe as You Reinvent It with BMC AMI</h4>
+                            <p>Deliver on the promise of the self-managing mainframe</p>
+                            <a href="#." class="hpLearnMore">Learn More</a>
+                        </div>
                     </div>
+                    
                 </div>
                 <div class="item " style="background-image: url('/front-end/Assets/src/img/homepage-2020/slide2.png');">
-                    <div class="carouselCaption">
-                        <img src="/front-end/Assets/src/img/homepage-2020/control-m-icon.svg" alt="" class="hpIcon">
-                        <h4>Control-M</h4>
-                        <p>Transform your business with workflow orchestration</p>
-                        <a href="#." class="hpLearnMore">Learn More</a>
-                    </div>
+                    <div class="flex car-flex">
+                        <div class="carousel-image  mobile-hidden">    
+                            <img src="/front-end/Assets/src/img/homepage-2020/Chatbot_2s_300x261px.gif" alt="">
+                        </div>    
+                        <div class="carouselCaption">
+                            <img src="/front-end/Assets/src/img/homepage-2020/control-m-icon.svg" alt="" class="hpIcon">
+                            <h4>Control-M</h4>
+                            <p>Transform your business with workflow orchestration</p>
+                            <a href="#." class="hpLearnMore">Learn More</a>
+                        </div>
+                    </div>    
                 </div>
                 <div class="item " style="background-image: url('/front-end/Assets/src/img/homepage-2020/slide3.png');">
-                    <div class="carouselCaption">
-                        <img src="/front-end/Assets/src/img/homepage-2020/bmc-helix-icon.svg" alt="" class="hpIcon">
-                        <h4>BMC Helix</h4>
-                        <p>BMC Helix – The first and only end-to-end service and operations SaaS platform that’s integrated with 360-degree intelligence <small>Forrester: Total Economic Impact™ Of BMC Helix</small></p>
-                        <a href="#." class="hpLearnMore">Learn More</a>
+                    <div class="flex car-flex">
+                        <div class="carousel-image  mobile-hidden">
+                            <img src="/front-end/Assets/src/img/homepage-2020/Chatbot_2s_300x261px.gif" alt="">
+                        </div>
+                        <div class="carouselCaption">
+                            <img src="/front-end/Assets/src/img/homepage-2020/bmc-helix-icon.svg" alt="" class="hpIcon">
+                            <h4>BMC Helix</h4>
+                            <p>BMC Helix – The first and only end-to-end service and operations SaaS platform that’s integrated with 360-degree intelligence <small>Forrester: Total Economic Impact™ Of BMC Helix</small></p>
+                            <a href="#." class="hpLearnMore">Learn More</a>
+                        </div>
                     </div>
                 </div>                
             </div>
