3a2fc4ce8d0fe698a4b9c7778ea37c9be8ff34b8
ADPROD\pthigale <pradip_thigale_tp@bmc.com>
2020-04-11 11:18:00 +0530

    Updated home page with max-width 1440px

--- front-end/Assets/src/templates/homepage-2020.php
+++ front-end/Assets/src/templates/homepage-2020.php
@@ -7,8 +7,8 @@
                     <div class="flex-item sm-col-7 lg-col-8 wide-ful-width">
                         <h2 class="jTitle">Become an Autonomous Digital Enterprise</h2>
                         <div class="flex">
-                            <div class="flex-item md-col-4 jHr"><hr></div>
-                            <div class="flex-item md-col-8 jCont">
+                            <div class="flex-item md-col-4 jHr"><hr><a href="#." class="hpLearnMore">Learn More</a></div>
+                            <div class="flex-item md-col-8 jCont widescreen-hidden">
                                 <p>Drive an innovative mindset through agility, customer centricity, and actionable insights to deliver growth and thrive in a disruptive market.</p>
                                 <a href="#." class="hpLearnMore">Learn More</a>
                             </div>
@@ -16,7 +16,9 @@
                     </div> 
                     <div class="flex-item sm-col-5 lg-col-4 mobile-hidden widescreen-hidden"> 
                         <div class="penta" style=" background-image: url('/front-end/Assets/src/img/homepage-2020/penta.svg');">
-                            <div class="round" style="background-image: url('/front-end/Assets/src/img/homepage-2020/round.svg');"></div>
+                            <div class="round">
+                                <img src="/front-end/Assets/src/img/homepage-2020/round.svg" alt="">
+                            </div>
                         </div>
                     </div> 
                 </div>             
@@ -26,25 +28,27 @@
                 <div class="sm-flex">
                     
                     <div class="flex-item sm-col-6  "  >
+                        
+                        <div class="hcContent hcContent-green-bg flex flex-center " style="background-image: url('/front-end/Assets/src/img/homepage-2020/greenbg.png');">
                         <div class="box1-1"></div>
 		                <div class="box2-2"></div>
-                        <div class="hcContent hcContent-green-bg" style="background-image: url('/front-end/Assets/src/img/homepage-2020/greenbg.png');">
                             <div class="cWrap">
                                 <img src="/front-end/Assets/src/img/homepage-2020/automation-icon.png" alt="" class="hcIcon">
                                 <h3>Automation everywhere</h3>
-                                <p>Intelligent automation across cloud, distributed systems, and mainframe.  </p>
+                                <p class="widescreen-hidden">Intelligent automation across cloud, distributed systems, and mainframe.  </p>
                                 <a href="#." class="hpLearnMore">Learn More</a>
                             </div>                            
                         </div>
                     </div>
                     <div class="flex-item sm-col-6 ">
-                        <div class="box1"></div>
-		                <div class="box2"></div>
-                        <div class="hcContent hcContent-blue-bg" style="background-image: url('/front-end/Assets/src/img/homepage-2020/bluebg.png');">
+                        
+                        <div class="hcContent hcContent-blue-bg flex flex-center " style="background-image: url('/front-end/Assets/src/img/homepage-2020/bluebg.png');">
+                            <div class="box1"></div>
+                            <div class="box2"></div>
                             <div class="cWrap">
                                 <img src="/front-end/Assets/src/img/homepage-2020/innovation-icon.png" alt="" class="hcIcon">
                                 <h3>Innovation in the cloud</h3>
-                                <p>Manage, operate, and services your critical IT and business operations with industry-leading SaaS solutions. </p>
+                                <p class="widescreen-hidden">Manage, operate, and services your critical IT and business operations with industry-leading SaaS solutions. </p>
                                 <a href="#." class="hpLearnMore">Learn More</a>
                             </div>    
                         </div>
@@ -55,14 +59,14 @@
         <div class="flex-item md-col-5 left-col">            
             <div class="owl-carousel-homepage">                              
                 <div class="item " style="background-image: url('/front-end/Assets/src/img/homepage-2020/Compuware_image_768x913_BG.png');">
-                    <div class="flex car-flex flex-center">
+                    <div class="flex flex-center car-flex">
                         <div class="carousel-image  mobile-hidden">
                             <img src="/front-end/Assets/src/img/homepage-2020/News_image.png" alt="">
                         </div>    
                         <div class="carouselCaption">
                             <img src="/front-end/Assets/src/img/homepage-2020/Compuware_logo_700x137-002_2.svg" alt="" class="hpIcon">
                             <h4>BMC signs definitive agreement to purchase Compuware</h4>
-                            <p>BMC to acquire Compuware, Combination brings mainframe to the mainstream as part of a modern IT infrastructure with a full-service solution integrating DevOps.</p>
+                            <p class="widescreen-hidden">BMC to acquire Compuware, Combination brings mainframe to the mainstream as part of a modern IT infrastructure with a full-service solution integrating DevOps.</p>
                             <a href="#." class="hpLearnMore">Learn More</a>
                         </div>
                     </div>
@@ -76,7 +80,7 @@
                         <div class="carouselCaption">
                             <img src="/front-end/Assets/src/img/homepage-2020/control-m-icon.svg" alt="" class="hpIcon">
                             <h4>Control-M</h4>
-                            <p>Transform your business with workflow orchestration</p>
+                            <p class="widescreen-hidden">Transform your business with workflow orchestration</p>
                             <a href="#." class="hpLearnMore">Learn More</a>
                         </div>
                     </div>    
