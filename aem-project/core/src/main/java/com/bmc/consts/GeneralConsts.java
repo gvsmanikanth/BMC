ecf086b3e6e4d07eaa28027c09e6c88c5db59300
ADPROD\sanvekar <samiksha_anvekar@bmc.com>
2020-06-16 03:27:17 +0530

    Merge branch 'feature/WEB-6185' into release/qa/qa-release-9
    # Conflicts:
    #	aem-project/ui.apps/src/main/content/jcr_root/etc/clientlibs/bmc/head/main.js
    #	front-end/Assets/src/js/main.js

---
+++ aem-project/core/src/main/java/com/bmc/consts/GeneralConsts.java
@@ -0,0 +1,7 @@
+package com.bmc.consts;
+
+public class GeneralConsts {
+    public static final String TRUE = "true";
+    public static final String FALSE = "false";
+
+}
---
+++ aem-project/core/src/main/java/com/bmc/models/bmccontentapi/ResourceCenterConstants.java
@@ -0,0 +1,36 @@
+package com.bmc.models.bmccontentapi;
+
+public class ResourceCenterConstants {
+    public static final String loggerName = "recourceCenter";
+
+    public static final String PREDICATE_PREFIX = "group.";
+
+    /* url parameters */
+    public static final String RC_URL_PARAM_PATH = "rootPath";
+    public static final String RC_URL_PARAM_KEYWORD = "keyword";
+    public static final String RC_URL_PARAM_FILTER = "filter";
+    public static final String RC_URL_PARAM_PAGINATION = "pagination";
+    public static final String RC_URL_PARAM_PAGE_INDEX = "pageIndex";
+    public static final String RC_URL_PARAM_RESULTS_PER_PAGE = "resultsPerPage";
+    /* query parameters */
+    public static final String RC_QUERY_PARAM_PATH = "path";
+    public static final String RC_QUERY_PARAM_KEYWORD = "keyword";
+    public static final String RC_QUERY_PARAM_FILTER = "filter";
+    public static final String RC_QUERY_PARAM_PAGINATION = "pagination";
+
+
+
+    public static final String RC_QUERY_PARAM_PROP = "property";
+    public static final String RC_QUERY_PARAM_PROP_VAL = "property.value";
+
+    public static final String RC_QUERY_PARAM_PROP_OPERATION = "property.operation";
+    public static final String RC_QUERY_PARAM_PROP_OPERATION_LIKE = "like";
+    
+    public static final String QUERY_PARAM_PAGE_IDX = "p.offset";
+    public static final String QUERY_PARAM_RESULT_PER_PAGE = "p.limit";
+    public static final String QUERY_PARAM_PROP = "property";
+    public static final String QUERY_PARAM_PROP_VAL = "property.value";
+    public static final String QUERY_PARAM_PROP_OPERATION ="property.operation";
+    public static final String QUERY_PARAM_PROP_OPERATION_LIKE = "like";
+
+}
