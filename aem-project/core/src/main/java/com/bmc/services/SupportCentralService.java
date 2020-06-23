7ec6a596312089fd31df0173c3b66fe8882e4f89
poonam <poonam_maniar@bmc.com>
2020-06-23 13:07:43 +0530

    Added configuration required for personalisastion api call

--- aem-project/core/src/main/java/com/bmc/services/SupportCentralService.java
+++ aem-project/core/src/main/java/com/bmc/services/SupportCentralService.java
@@ -49,6 +49,62 @@ public class SupportCentralService {
     public String getSearchPageUrl() {
         return searchPageUrl;
     }
+    public String getPersonalisationApiOauthUser() {
+		return personalisationApiOauthUser;
+	}
+
+	public void setPersonalisationApiOauthUser(String personalisationApiOauthUser) {
+		this.personalisationApiOauthUser = personalisationApiOauthUser;
+	}
+
+	public String getPersonalisationApiOauthPass() {
+		return personalisationApiOauthPass;
+	}
+
+	public void setPersonalisationApiOauthPass(String personalisationApiOauthPass) {
+		this.personalisationApiOauthPass = personalisationApiOauthPass;
+	}
+
+	public String getPersonalisationApiUser() {
+		return personalisationApiUser;
+	}
+
+	public void setPersonalisationApiUser(String personalisationApiUser) {
+		this.personalisationApiUser = personalisationApiUser;
+	}
+
+	public String getPersonalisationApiPass() {
+		return personalisationApiPass;
+	}
+
+	public void setPersonalisationApiPass(String personalisationApiPass) {
+		this.personalisationApiPass = personalisationApiPass;
+	}
+
+	public String getSupportCentralPersonalisationUrl() {
+		return supportCentralPersonalisationUrl;
+	}
+
+	public void setSupportCentralPersonalisationUrl(
+			String supportCentralPersonalisationUrl) {
+		this.supportCentralPersonalisationUrl = supportCentralPersonalisationUrl;
+	}
+
+	public String getOauthUrl() {
+		return oauthUrl;
+	}
+
+	public void setOauthUrl(String oauthUrl) {
+		this.oauthUrl = oauthUrl;
+	}
+
+	public String getPopularProductUrl() {
+		return popularProductUrl;
+	}
+
+	public void setPopularProductUrl(String popularProductUrl) {
+		this.popularProductUrl = popularProductUrl;
+	}
     private String newCaseUrl;
     private String allCasesUrl;
     private String apiBaseUrl;
@@ -59,6 +115,13 @@ public class SupportCentralService {
     private String supportCoveoAccessToken;
     private String searchPageUrl;
 
+    private String personalisationApiOauthUser ;
+    private String personalisationApiOauthPass ;
+    private String personalisationApiUser ;
+    private String personalisationApiPass ;
+    private String supportCentralPersonalisationUrl;
+    private String oauthUrl;
+    private String popularProductUrl;
     @Activate
     public void activate(Map<String, String> config) {
         newCaseUrl = PropertiesUtil.toString(config.get("newCaseUrl"), "");
@@ -71,6 +134,14 @@ public class SupportCentralService {
         supportCentralUrl = PropertiesUtil.toString(config.get("supportCentralUrl"), "");
         supportCoveoAccessToken = PropertiesUtil.toString(config.get("supportCoveoAccessToken"), "");
         searchPageUrl = PropertiesUtil.toString(config.get("searchPageUrl"), "");
+        
+        personalisationApiOauthUser = PropertiesUtil.toString(config.get("personalisationApiOauthUser"), "");
+        personalisationApiOauthPass = PropertiesUtil.toString(config.get("personalisationApiOauthPass"), "");
+        personalisationApiUser = PropertiesUtil.toString(config.get("personalisationApiUser"), "");
+        personalisationApiPass = PropertiesUtil.toString(config.get("personalisationApiPass"), "");
+        supportCentralPersonalisationUrl = PropertiesUtil.toString(config.get("supportCentralPersonalisationUrl"), "");
+        oauthUrl = PropertiesUtil.toString(config.get("oauthUrl"), "");
+        popularProductUrl = PropertiesUtil.toString(config.get("popularProductUrl"), "");
         		
     }
 
