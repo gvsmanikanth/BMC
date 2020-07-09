96d40bee2c7b6159bd100b15b50045c59c92a155
dchornyi <dmytro_chornyi_tp@bmc.com>
2020-07-09 16:04:38 +0300

    Links added, changed structure to use object

--- front-end/Assets/src/support-central-angular/src/app/app.component.ts
+++ front-end/Assets/src/support-central-angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Widgets } from './models/widgets';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'support-central';
+  widgets: Widgets = window['psc'].widgets;
 }
---
+++ front-end/Assets/src/support-central-angular/src/app/models/widgets.ts
@@ -0,0 +1,13 @@
+export interface Widgets {
+    openCase: string,
+    listCase: string,
+    manageCase:  string,
+    docs: string,
+    prdDownloads: string,
+    askCommunity: string,
+    supportVideos: string,
+    supportedProducts: string,
+    prdCompatibility: string,
+    training: string,
+    prdSupport: string
+}
