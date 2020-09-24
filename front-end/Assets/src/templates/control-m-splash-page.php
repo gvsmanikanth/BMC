94c813c4717e81d528bafb561a7c27e96e64a2fc
ADPROD\pthigale <pradip_thigale_tp@bmc.com>
2020-09-24 13:53:03 +0530

    Orion splash page created

---
+++ front-end/Assets/src/templates/control-m-splash-page.php
@@ -0,0 +1,143 @@
+<?php
+	$pageTitle = 'BMC Control-M Splash Page';
+	$bodyClass = 'splashPage';
+	include 'php-inc/head.php';
+?>
+
+<section class="splashpage-banner orion-global">
+    <div class="sp-large-container">
+        <div class="layout-inner-wrap">
+            <div class="splashpage-banner-content text-center text-white " >
+                <div class="bannerLogo" >
+                    <img src="Assets/src/img/splashPage/bmc-helix-controlm-logo-semireversed.svg" alt="">
+                </div>
+                <h1>Enterprise automation and orchestration built <span>for the cloud.</span>  </h1>
+                <h3>Engineered from market leading technology.</h3>
+                <h3>Engineered from market leading technology.</h3>
+                <a href="#." class="btn btn-gradient">Keep Me Informed</a>
+            </div>
+        </div>
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 548 85.002" style="enable-background:new 0 0 548 85.002;" xml:space="preserve">
+        <style type="text/css">
+            .st0{fill:none;stroke:#2BD4EE;stroke-width:3.9985;stroke-linecap:round;stroke-miterlimit:3.9985;}
+            .st1{fill:none;stroke:#FFFFFF;stroke-width:5.9977;stroke-miterlimit:3.9985;stroke-dasharray:5.9977,11.9954;}
+            .st2{fill:none;stroke:#F98700;stroke-width:3.9985;stroke-linecap:round;stroke-miterlimit:3.9985;}
+            .st3{fill:#2BD4EE;stroke:#2BD4EE;stroke-width:3.9985;stroke-linecap:round;stroke-miterlimit:3.9985;}
+            .st4{fill:#FFFFFF;}
+            .st5{fill:#BDBDBD;}
+            .st6{fill:#FE5000;}
+        </style>
+        <path id="Path_545" class="st0" d="M20.223,29.113C70.462,0.854,136.47-0.032,190.364,16.67c5.381,1.668,10.704,3.514,15.967,5.539"
+            />
+        <path id="Path_545-2" class="st0" d="M54.225,32.242c14.955-4.531,30.373-7.359,45.963-8.433"/>
+        <path id="Path_545-3" class="st1" d="M227.555,31.665c19.821,9.351,30.383,16.087,46.954,25.838
+            c15.703,9.48,33.276,15.441,51.506,17.474"/>
+        <path id="Path_545-4" class="st2" d="M352.056,76.57c55.791-5.912,71.473-18.413,93.547-33.571
+            c20.378-13.417,34.971-31.464,82.835-29.587"/>
+        <path id="Path_545-4_2_" class="st3" d="M367.499,59.82c18.391-3.56,30.887-8.217,41.258-13.652"/>
+        <circle id="Ellipse_383" class="st4" cx="328.052" cy="76.438" r="5.852"/>
+        <path id="Rectangle_908" class="st5" d="M547.766,11.778l0.06,1.706c0.097,2.759-2.061,5.073-4.82,5.17l-6.701,0.235l0,0
+            l-0.41-11.696l0,0l6.701-0.235C545.355,6.861,547.669,9.019,547.766,11.778z"/>
+        <rect id="Rectangle_909" y="29.393" class="st6" width="12" height="13"/>
+        </svg>
+
+
+    </div>
+
+
+</section>
+
+<section class="video-feature text-center orion-global">
+    <div class="layout-inner-wrap video-feature-bg">        
+        <div class="video-hero-header">
+            <img src="Assets/src/img/splashPage/sp-divider.png" alt="" class="sp-divider" data-aos="fade-in">
+            <h2 data-aos="fade-up">The experts are talking about what’s to come…</h2>
+            <section class="hero flex-col">
+                <div class="hero-image xs-only" style="background-image: url(Assets/src/img/splashPage/video-bg.jpg);"></div>
+                <div class="hero-image video xs-max-hide">
+                    <div class="vidbg-box" style="width: 100%; height: 100%; position: relative; z-index: 1;" data-vidbg-bg="mp4:http://service.twistage.com/videos/de1ac177ef090/formats/360p-BitRateOptimized/file.mp4,webm:http://service.twistage.com/videos/de1ac177ef090/formats/360p-BitRateOptimized-Webm/file.webm,poster: Assets/src/img/splashPage/video-bg.jpg" data-vidbg-options="loop: true, muted: true, overlay: true, overlayColor: #000, overlayAlpha: 0.3"></div>
+                </div>
+                <div class="hero-content flex flex-center">
+                    <a href="/content/bmc/videos.html?vID=JOrfbJA5PrU" target="_self" class="modal-youtube-video-player" title="We're a great place to work">
+                        <img class="mb2 sp-video-icon" src="Assets/src/img/splashPage/icons/svg-colored/play-circle-orangegradient.svg">
+                    </a>              
+                </div>
+            </section>
+        </div>
+    </div>    
+</section>
+
+<section class="split-form orion-global">
+    <div class="sp-large-container">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1890.327" height="350.191" viewBox="0 0 1890.327 350.191" style="position: absolute; left: 0;  right: 0;   width: 100%;    margin: 0 auto;">
+            <defs>
+                <style>
+                .cls-1, .cls-2, .cls-3 {
+                    fill: none;
+                    stroke-width: 6px;
+                    
+                    stroke-dasharray: 1000;
+                    stroke-dashoffset: 1000;
+                    animation: dash 5s linear  infinite;
+                }
+
+                .cls-1, .cls-3 {
+                    stroke: #d2d7d9;
+                }
+
+                .cls-1 {
+                    stroke-dasharray: 6 16;
+                }
+
+                .cls-2 {
+                    stroke: #2bd4ee;
+                }
+
+                .cls-2, .cls-3 {
+                    stroke-linecap: round;
+                }
+
+                .cls-4 {
+                    fill: #2bd4ee;
+                }
+
+                @keyframes dash {
+                    from {
+                        stroke-dashoffset: 1000;
+                    }
+                    to {
+                        stroke-dashoffset: 400;
+                    }
+                    }
+            
+
+
+                </style>
+            </defs>
+            <g id="Group_660" data-name="Group 660" transform="translate(-17.966 -1447.735)">
+                <path id="Path_545" data-name="Path 545" class="cls-1" d="M1095.667,161.222c47.8,7.993,95.095,18.339,141.023,30.826q48.779,13.263,96.613,29.768,217.411,74.928,429.788,162.716" transform="translate(-1077.206 1289.472)"/>
+                <path id="Path_545-2" data-name="Path 545" class="cls-2" d="M2849.595,325.029c55.839-23.417,100.78-44.853,131.8-59.6" transform="translate(-1077.206 1329.472)"/>
+                <path id="Path_545-3" data-name="Path 545" class="cls-3" d="M1391.924,242.294q187.4,66.342,371.167,142.238c153.566,63.26,297.75,81.3,462.56,80.917,182.282-.414,354.413-42.706,491.977-90" transform="translate(-1077.206 1329.472)"/>
+                <circle id="Ellipse_390" data-name="Ellipse 390" class="cls-4" cx="10" cy="10" r="10" transform="translate(287 1556)"/>
+                <path id="Path_545-4" data-name="Path 545" class="cls-3" d="M2225.65,465.449c338.923-.77,642.753-146.316,755.851-200.069" transform="translate(-1077.206 1289.472)"/>
+            </g>
+        </svg>
+        <div class="sm-flex layout-inner-wrap">
+            <div class="flex-item sm-col-4">
+                <img src="Assets/src/img/splashPage/split-form-image.png" alt="" data-aos="fade-right"
+        data-aos-easing="linear"
+        data-aos-duration="1000">
+            </div>
+            <div class="flex-item sm-col-8">
+                <div class="sp-form-wrap">
+
+                </div>
+            </div>
+        </div>
+    </div>    
+</section>
+
+
+
+<?php include 'php-inc/foot.php'; ?>
