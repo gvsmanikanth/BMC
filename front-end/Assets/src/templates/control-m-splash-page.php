a353968381c7c206e9d4504941aade71f417bf9a
ADPROD\pthigale <pradip_thigale_tp@bmc.com>
2020-10-05 15:57:54 +0530

    Orion page updates

--- front-end/Assets/src/templates/control-m-splash-page.php
+++ front-end/Assets/src/templates/control-m-splash-page.php
@@ -71,30 +71,11 @@
             </div>
             <div class="flex-item md-col-7 sp-form-wrap">
                 <div class="iframe-wrap"> 
-                    <iframe id="spIframe" src="/front-end/splash-form.php" onload="setIframeHeight(this.id)"></iframe>
+                    <iframe id="spIframe" src="/front-end/splash-form.php" ></iframe>
                 </div>
             </div>
         </div>
         <div id="splitFormAnimation" ></div>       
     </div>    
 </section>
-<script>
-    function getDocHeight(doc) {
-    doc = doc || document;
-    var body = doc.body, html = doc.documentElement;
-    var height = Math.max( body.scrollHeight, body.offsetHeight, 
-        html.clientHeight, html.scrollHeight, html.offsetHeight );
-    return height;
-    }
-    function setIframeHeight(id) {
-        var ifrm = document.getElementById(id);
-        var doc = ifrm.contentDocument? ifrm.contentDocument: 
-            ifrm.contentWindow.document;
-        ifrm.style.visibility = 'hidden';
-        ifrm.style.height = "10px"; // reset to minimal height ...
-        // IE opt. for bing/msn needs a bit added or scrollbar appears
-        ifrm.style.height = getDocHeight( doc ) + 4 + "px";
-        ifrm.style.visibility = 'visible';
-    }
-</script>
 <?php include 'php-inc/foot.php'; ?>
