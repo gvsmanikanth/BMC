ef8bdb2676325e4bf50594b8351bede797ecbbc7
ADPROD\sanvekar <samiksha_anvekar@bmc.com>
2020-10-07 16:41:56 +0530

    WEB-8912 Video Component - Overlay URL/Text Enhancements

--- aem-project/ui.apps/src/main/content/jcr_root/apps/bmc/components/content/video/resolve-data.js
+++ aem-project/ui.apps/src/main/content/jcr_root/apps/bmc/components/content/video/resolve-data.js
@@ -3,17 +3,19 @@ use(function() {
     var overlayUrl = this.video.overlayUrl;
     if (overlayUrl.isEmpty())
         overlayUrl = '#';
-
+    
     var overlayText = this.video.overlayText;
     if (overlayText.isEmpty()) {
-        overlayText = 'Find out how BMC can help you. LINK-Contact a sales rep ›-LINK';
+        overlayText = 'Find out how BMC can help you. LINK-Contact a sales rep ›-LINK'; 
         overlayUrl = '/forms/contact-bmc.html';
+    }else 
+    {
+    	//WEB-8912 Video Component - Overlay URL/Text Enhancements
+         overlayText = 'LINK-'+overlayText+'-LINK'; 
     }
-
     overlayText = overlayText.replace('LINK-', "<a id='overlayLink' href='" + overlayUrl + "'>");
     overlayText = overlayText.replace('-LINK', '</a>');
-
-    return {
-        overlayText: overlayText
+    return {        
+        overlayText:  overlayText
     };
 });
--- aem-project/ui.apps/src/main/content/jcr_root/apps/bmc/components/structure/video-player/resolve-data.js
+++ aem-project/ui.apps/src/main/content/jcr_root/apps/bmc/components/structure/video-player/resolve-data.js
@@ -8,7 +8,11 @@ use(function() {
     if (overlayText.isEmpty()) {
         overlayText = 'Find out how BMC can help you. LINK-Contact a sales rep ›-LINK';
         overlayUrl = '/forms/contact-bmc.html';
-    }
+    }else 
+    {
+    	//WEB-8912 Video Component - Overlay URL/Text Enhancements
+        overlayText = 'LINK-'+overlayText+'-LINK'; 
+   }
 
     overlayText = overlayText.replace('LINK-', "<a id='overlayLink' target='_top' href='" + overlayUrl + "'>");
     overlayText = overlayText.replace('-LINK', '</a>');
