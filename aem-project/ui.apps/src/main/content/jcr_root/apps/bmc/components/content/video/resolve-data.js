b6fabc3060f13a6aaa6b6ed696a8d60e90dc43c8
ADPROD\sanvekar <samiksha_anvekar@bmc.com>
2020-10-07 18:29:25 +0530

    WEB-8912 Video OverlayText and URL enhancements

--- aem-project/ui.apps/src/main/content/jcr_root/apps/bmc/components/content/video/resolve-data.js
+++ aem-project/ui.apps/src/main/content/jcr_root/apps/bmc/components/content/video/resolve-data.js
@@ -1,14 +1,14 @@
 "use strict";
 use(function() {
-    var overlayUrl = this.video.overlayUrl;
-    if (overlayUrl.isEmpty())
-        overlayUrl = '#';
+    var overlayUrl = this.video.overlayUrl;   
+    /* if (overlayUrl.isEmpty())
+    overlayUrl = '#';*/
     
     var overlayText = this.video.overlayText;
     if (overlayText.isEmpty()) {
         overlayText = 'Find out how BMC can help you. LINK-Contact a sales rep ›-LINK'; 
         overlayUrl = '/forms/contact-bmc.html';
-    }else 
+    }else if(!overlayUrl.isEmpty())
     {
     	//WEB-8912 Video Component - Overlay URL/Text Enhancements
          overlayText = 'LINK-'+overlayText+'-LINK'; 
