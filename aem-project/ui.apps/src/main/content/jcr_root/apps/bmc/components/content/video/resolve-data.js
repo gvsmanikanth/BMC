ba4fb21eb8e80da52d2b7b20d7990616879fee63
ADPROD\sanvekar <samiksha_anvekar@bmc.com>
2020-10-08 20:24:31 +0530

    WEB-8912 Video Component - Overlay URL/Text Enhancements

--- aem-project/ui.apps/src/main/content/jcr_root/apps/bmc/components/content/video/resolve-data.js
+++ aem-project/ui.apps/src/main/content/jcr_root/apps/bmc/components/content/video/resolve-data.js
@@ -1,18 +1,17 @@
 "use strict";
 use(function() {
-    var overlayUrl = this.video.overlayUrl;   
-    /* if (overlayUrl.isEmpty())
-    overlayUrl = '#';*/
-    
+    var overlayUrl = this.video.overlayUrl;       
     var overlayText = this.video.overlayText;
-    if (overlayText.isEmpty()) {
+
+	  //WEB-8912 Video Component - Overlay URL/Text Enhancements
+  	if (overlayUrl.isEmpty() || overlayText.isEmpty()) {
         overlayText = 'Find out how BMC can help you. LINK-Contact a sales rep ›-LINK'; 
         overlayUrl = '/forms/contact-bmc.html';
-    }else if(!overlayUrl.isEmpty())
-    {
-    	//WEB-8912 Video Component - Overlay URL/Text Enhancements
+    } else {
+
          overlayText = 'LINK-'+overlayText+'-LINK'; 
     }
+
     overlayText = overlayText.replace('LINK-', "<a id='overlayLink' href='" + overlayUrl + "'>");
     overlayText = overlayText.replace('-LINK', '</a>');
     return {        
--- aem-project/ui.apps/src/main/content/jcr_root/apps/bmc/components/structure/video-player/resolve-data.js
+++ aem-project/ui.apps/src/main/content/jcr_root/apps/bmc/components/structure/video-player/resolve-data.js
@@ -1,23 +1,19 @@
 "use strict";
 use(function() {
-    var overlayUrl = this.video.overlayUrl;
-   /* if (overlayUrl.isEmpty())
-        overlayUrl = '#';*/
-
+    var overlayUrl = this.video.overlayUrl;	
     var overlayText = this.video.overlayText;
-    if (overlayText.isEmpty()) {
-        overlayText = 'Find out how BMC can help you. LINK-Contact a sales rep ›-LINK';
+    //WEB-8912 Video Component - Overlay URL/Text Enhancements
+  if (overlayUrl.isEmpty() || overlayText.isEmpty()) {
+        overlayText = 'Find out how BMC can help you. LINK-Contact a sales rep ›-LINK'; 
         overlayUrl = '/forms/contact-bmc.html';
-    }else  if(!overlayUrl.isEmpty())
-    {
-    	//WEB-8912 Video Component - Overlay URL/Text Enhancements
-        overlayText = 'LINK-'+overlayText+'-LINK'; 
-   }
+    } else {
 
-    overlayText = overlayText.replace('LINK-', "<a id='overlayLink' target='_top' href='" + overlayUrl + "'>");
+         overlayText = 'LINK-'+overlayText+'-LINK'; 
+    }
+
+    overlayText = overlayText.replace('LINK-', "<a id='overlayLink' href='" + overlayUrl + "'>");
     overlayText = overlayText.replace('-LINK', '</a>');
-
-    return {
-        overlayText: overlayText
+    return {        
+        overlayText:  overlayText
     };
 });
