cca2521a5b63a25ff72fd9e6fcaa6e83c3320807
Shailesh Kadam <shailesh_kadam@bmc.com>
2020-10-09 17:22:23 +0530

    Added new file structure.

---
+++ front-end/Assets/src/templates/orion-homepage.php
@@ -0,0 +1,11 @@
+<?php
+	$pageTitle = 'About Us';
+	$bodyClass = 'company page-about-us';
+	include 'php-inc/head.php';
+?>
+
+<!--Header Component Start -->
+	<H1>Header</H1>
+<!--Header Component End -->
+
+<?php include 'php-inc/foot.php'; ?>
