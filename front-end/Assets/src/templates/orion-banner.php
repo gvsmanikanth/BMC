a6d3ace0339542635c9805c20db67225b5ac2e70
Perla Renee <perlarenee@gmail.com>
2020-10-26 20:13:09 -0500

    WEB-8998 anchor banner with links

---
+++ front-end/Assets/src/templates/orion-banner.php
@@ -0,0 +1,52 @@
+<?php
+	$pageTitle = 'Anchor Banner w Links - v1';
+	$bodyClass = 'splashPage';
+	include 'php-inc/head.php';
+?>
+
+<!--Header Component Start -->
+<section class="bg-white anchor-banner-wrap orion-global orion-anchor-banner">
+    <div class="sp-large-container anchor-banner-orange-blob rellax" data-rellax-speed="3" style='background-image: url("Assets/src/img/orion/banner-orange-blob.png");'  >
+		
+		<div class="anchor-banner rellax" data-rellax-speed="-3" style='background-image: url("Assets/src/img/orion/banner-blue-blob.png");'  >			
+            <div class="layout-inner-wrap">
+                <div class="anchor-banner-content text-black ">
+                    <h2>Integrations</h2>
+                    <p><strong>Jump to an integration</strong></p>
+					<ul class="anchor-banner-menu">
+						<li><a>Platform</a></li>
+						<li><a>SAP</a></li>
+						<li><a>Informatica</a></li>
+						<li><a>Databases</a></li>
+						<li><a>AWS</a></li>
+						<li><a>Managed File Transfer</a></li>
+						<li><a>Microsoft Azure</a></li>
+						<li><a>Hadoop</a></li>
+						<li><a>Robotic Process Automation (RPA)</a></li>
+						<li><a>Lorum Ipsum</a></li>
+						<li><a>Lorum Ipsum</a></li>
+						<li><a>Lorum Ipsum</a></li>
+					</ul>
+				</div>     
+			</div> 			  				
+        </div>         
+    </div>
+</section>
+<!--Header Component End -->
+
+
+<div class="bg-white " style="min-height: 64px"></div>
+
+<section class="orion-CTA-banner orion-global" style="background-color:#FE5000;">
+    <div class="layout-inner-wrap banner-area">
+        <div class="image-bop"></div>  
+        <div class="text-center content-banner">
+            <div>
+                <h2>Ready to peek under the hood?</h2>
+                <p>Schedule a demo to get a look at all that Control-M SaaS has to offer, from its easy visual workflow orchestration designer to its lightning fast deployments.</p>
+                <a href="#" class="btn-octa">Watch a Demo</a>
+            </div>
+        </div>
+    </div>    
+</section>
+<?php include 'php-inc/foot.php'; ?>
