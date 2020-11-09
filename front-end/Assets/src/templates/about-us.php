209a14595d0fd9d742cf79e7e946c9128c2fece3
ADPROD\pthigale <pradip_thigale_tp@bmc.com>
2020-11-09 13:15:49 +0530

    updated the resurce page

--- front-end/Assets/src/templates/about-us.php
+++ front-end/Assets/src/templates/about-us.php
@@ -3,9 +3,77 @@
 	$bodyClass = 'company page-about-us';
 	include 'php-inc/head.php';
 ?>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 
 <?php include 'php-inc/svg.php'; ?>
+<!-- Exit Intent Code Start -->
 
+
+<style>
+   /* Exit Intent Modal Popup CSS */
+   
+   .layout-exit-modal .imagesection{
+      margin-top:50px;
+      margin-bottom:30px;
+    }
+   .layout-exit-modal .nothanks{
+      margin-top:40px;
+      margin-bottom: 20px;
+    }
+   .layout-exit-modal h2{
+      font-weight: 400;
+      font-size: 50px; 
+      margin-bottom: 10px;
+      line-height: 68px;        
+    }
+   .layout-exit-modal h3{
+      font-weight:600;
+    }
+    .layout-exit-modal p{
+       max-width:720px;
+       margin:auto;
+    }
+   .layout-exit-modal .btn{       
+      border-radius: 0;
+      border-color: #F86E01; 
+      margin: 0;    
+      background-color: #F86E01;
+      transition: 0.3s linear;	
+      font-size: 14px;
+      font-weight: 600;
+      line-height: 24px;
+      margin-top:30px;
+    }
+   .layout-exit-modal .btn:hover{
+        background-color: #f83200;
+        border-color: #f83200; 
+        transition: 0.4s linear;
+      } 
+   .layout-exit-modal .btn:focus{
+        outline: #F86E01 thin dotted;
+      }
+</style>
+<!-- Exit Intent HTML for Popup start-->
+<div id="exitIntentPopup" style="display:none">
+   <div class="layout-exit-modal">
+      <div class="contact-modal-inner-wrap text-center">
+         <div class="contact-modal-header">
+            <h2>Saas Means Success</h2>
+            <!--<a href="#" class="modal-close"></a>-->
+         </div>
+         <div class="imagesection">
+            <img src="/front-end/Assets/src/img/exitintentimg.png" alt="exit Intent"/>
+         </div>
+         <div>
+            <h3>Learn about Saas, PaaS, Iaas, & more</h3>
+            <p> Learn how to effectively and efficiently deliver the features and functionality required of this modern digital economy</p>
+            <div><a class="btn learn-more" href="https://www.bmc.com/forms/saas-accelerating-success-ebook.html">Get the Free e-Book</a></div>
+            <div class="nothanks"><a href="#" id="closeExitIntentPopup">No thanks</a></div>
+         </div>
+      </div>
+   </div>
+</div>
+<!-- Exit Intent Code End -->
 <div class="htmlarea aem-GridColumn aem-GridColumn--default--12">
    <section class="wallpapered bar full-bleed-page-banner product-category-header product-category-header2 padding-bottom-0 wallpaper" data-wallpaper-options="{&quot;source&quot;:{&quot;fallback&quot;: &quot;/content/dam/bmc/corporate/tb-about-bmc-700x700px.png&quot;,&quot;(min-width: 640px)&quot;: &quot;/content/dam/bmc/corporate/tbn-about-bmc.jpg&quot;}}" style="background-color:#0091DD;">
       <div class="offset-wrapper flex-center">
@@ -175,4 +243,81 @@
    </section>
 </div>
 
+
+<script>
+   (function ($) {
+    'use strict';
+
+    var timer;
+
+    function trackLeave(ev) {
+        if (ev.clientY > 0) {
+            return;
+        }
+
+        if (timer) {
+            clearTimeout(timer);
+        }
+
+        if ($.exitIntent.settings.sensitivity <= 0) {
+            $.event.trigger('exitintent');
+            return;
+        }
+
+        timer = setTimeout(
+            function() {
+                timer = null;
+                $.event.trigger('exitintent');
+            }, $.exitIntent.settings.sensitivity);
+    }
+
+    function trackEnter() {
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        }
+    }
+
+    $.exitIntent = function(enable, options) {
+        $.exitIntent.settings = $.extend($.exitIntent.settings, options);
+
+        if (enable == 'enable') {
+            $(window).mouseleave(trackLeave);
+            $(window).mouseenter(trackEnter);
+        } else if (enable == 'disable') {
+            trackEnter(); // Turn off any outstanding timer
+            $(window).unbind('mouseleave', trackLeave);
+            $(window).unbind('mouseenter', trackEnter);
+        } else {
+            throw "Invalid parameter to jQuery.exitIntent -- should be 'enable'/'disable'";
+        }
+    }
+
+    $.exitIntent.settings = {
+        'sensitivity': 300
+    };
+
+   })(jQuery);
+
+   function exitIntentfunction() {
+		$.exitIntent('enable');
+		$(document).bind('exitintent', function() {
+			$("#exitIntentPopup").slideDown();
+		});
+		$("#closeExitIntentPopup").bind('click', function() {
+         $("#exitIntentPopup").slideUp();
+         $.exitIntent('disable');
+			return false;
+		});
+	}
+   function deferLoadjquery(method) {
+      if (window.$ && $.exitIntent){
+         method();
+       }
+      else{
+        setTimeout(function() {deferLoadjquery(method)}, 1000);
+      }
+    }
+    deferLoadjquery(exitIntentfunction);
+</script>
 <?php include 'php-inc/foot.php'; ?>
