8055f92a475f93bc5ab2def1373c0664015fd488
Pradip Thigale <40828590+Pradipthigale@users.noreply.github.com>
2020-11-25 14:10:04 +0530

    video component issue solved

--- front-end/Assets/src/templates/orion-CTA.php
+++ front-end/Assets/src/templates/orion-CTA.php
@@ -4,6 +4,41 @@
 	include 'php-inc/head.php';
 ?>
 
+
+<!--orion-split-content with cloud background Component start -->
+<section class="orion-split-content bg-white orion-global">
+	<div class="layout-inner-wrap">
+		<div class="flex-wrap  mb2">
+			<div class="flex flex-item col-12 lg-col-5 mb2 flex-justify-center" style="max-width: 500px;">
+				<div class="imageContainer">
+					<a href="https://www.bmc.com/content/dam/bmc/solutions/banners/banner-placeholder.png" class="modal-image">
+						<img class="" src="https://www.bmc.com/content/dam/bmc/solutions/screenshots/split-content.png" alt="split-content" title="split-content" />
+						<div class="image-modal-overlay ">
+							<img src="https://www.bmc.com/content/dam/bmc/solutions/icons/zoom-cyan.svg" />
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="flex flex-item col-12 lg-col-7 flex-center">
+				<div class="split-text-content">
+					<h2 class="orion-block-title">With BMC Helix Control-M You Can:</h2>
+					<ul class="orange-tick-bullets">
+						<li><strong></strong>Orchestrate applications across multiple clouds/on-prem without relying on disparate tools</li>
+						<li>Eliminate unreliable delivery of critical business services</li>
+						<li>Deliver business agility by integrating into any DevOps automation tool chain with ‘as-Code’ interfaces </li>
+						<li>Simplify the adoption of new technologies into your technology ecosystem </li>
+						<li>And much more!</li>
+					</ul>
+					<div class="button-set">
+						<a class="btn btn-gradient" href="/it-solutions/bmc-helix-control-m-features.html">See all features&nbsp;›</a>
+					</div>
+				</div>
+			</div>
+		</div>
+  	</div>
+</section>
+<!--orion-split-content with cloud background Component ends -->
+
 <section class="orion-CTA-banner  orion-global" style="background-color:#FE5000;">
     <div class="banner-area layout-inner-wrap">
     <div class="image-blob animate-CTA-banner"></div>  
