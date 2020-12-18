dc9ef5a4fb1e853a44c0ee1c84ddabf51d3e8ee4
dnyanesh21 <61614503+dnyanesh21@users.noreply.github.com>
2020-12-18 19:41:22 +0530

    Update orion-secondary-navigation.js
    Code modified as previous code not working in IE11

--- front-end/Assets/src/js/modules/orion/orion-secondary-navigation.js
+++ front-end/Assets/src/js/modules/orion/orion-secondary-navigation.js
@@ -73,8 +73,28 @@
 
 	}
 
-    
-
+  if($('.orion-seconday-nav').length > 0){
+    var current = location.pathname;
+    for(var i = 0; i < $('.orion-seconday-nav .nav-list li ').length; i++) {  
+        var lilist = $('.orion-seconday-nav .nav-list li ')[i];
+        var elementList = $(lilist).children('a');//querySelectorAll("a");
+        //var elementList = $('.orion-seconday-nav .nav-list li')[i];
+        var innerElementList=$(lilist).children('ul li a');
+        //var innerElementList=$('.orion-seconday-nav .nav-list li ul li')[i];
+        for(var j = 0; j < elementList.length; j++) {
+            if($(elementList[j]).attr('href') !== -1){
+                $(elementList[j]).parent().addClass('activePage');
+            }   
+        }
+        for(var k = 0; k < innerElementList.length; k++){
+            if($(innerElementList[k]).attr('href') !== -1){
+                $(innerElementList[k]).parent().addClass('activePage');
+                $(innerElementList[k]).parent().parent().parent().addClass('activePage');
+            }
+        }       
+    }
+} 
+/* commenting this code as this is not working in IE11 Added above without "each"
          // Adding Active Navigation Class Based on URL
       if($('.orion-seconday-nav').length > 0){
         var current = location.pathname;
@@ -97,7 +117,7 @@
             
         })
     } 
-
+*/
     }
   }); // end DOM ready
 })(jQuery); // end jQuery
