4d58e1aa38459691d7a90322c0806a9b18cddd55
PreetyK <preety_kumari@bmc.com>
2021-02-04 17:25:28 +0530

    WEB-9670 Handled NPE for RC Section Component

--- aem-project/core/src/main/java/com/bmc/components/ResourceSection.java
+++ aem-project/core/src/main/java/com/bmc/components/ResourceSection.java
@@ -1,13 +1,11 @@
 package com.bmc.components;
 
 import com.adobe.cq.sightly.WCMUsePojo;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.slf4j.LoggerFactory;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+
 import java.util.*;
-import com.bmc.mixins.MetadataInfoProvider;
+
 import com.day.cq.wcm.api.Page;
 import com.bmc.mixins.MultifieldDataProvider;
 import org.apache.sling.api.resource.Resource;
@@ -31,16 +29,10 @@ public class ResourceSection extends WCMUsePojo implements MultifieldDataProvide
     @Inject
     private Resource resource;
 
-    @OSGiService
-    private Session session;
-
     protected HashMap<String,String> resourceSection;
 
-    protected List<HashMap> resourceSectionCards;
+    protected List<HashMap<String,String>> resourceSectionCards;
 
-    private  String contentType;
-
-    private Boolean isVideo = false;
     @Override
     public void activate() throws Exception {
         try {
@@ -54,6 +46,7 @@ public class ResourceSection extends WCMUsePojo implements MultifieldDataProvide
                 String cardTitle = "";
                 String cardDescription = "";
                 String cardIconPath = "";
+                String contentType = "";
                 Page page = this.getResourceProvider().getPage(pagePath);
                 if (page != null){
                     ValueMap pageMap = page.getProperties();
@@ -84,7 +77,7 @@ public class ResourceSection extends WCMUsePojo implements MultifieldDataProvide
                 resourceSection.put("pagePath", pagePath);
                 resourceSection.put("description", cardDescription);
                 resourceSection.put("rcCardsImagePath", cardIconPath);
-
+                resourceSection.put("contentType", contentType);
                 // Handle the case of the image not existing.
                 resourceSectionCards.add(resourceSection);
             }
@@ -96,29 +89,34 @@ public class ResourceSection extends WCMUsePojo implements MultifieldDataProvide
 
     }
 
-    public List<HashMap> getResourceSectionCards() {
+    public List<HashMap<String,String>> getResourceSectionCards() {
         return resourceSectionCards;
-    }
-
-    public String getContentType(){
-        return contentType;
     }
 
     public String getVideoPath(String pagePath){
         String videoPath = "";
-        String videoId = "";
+        String videoID = "";
+        ResourceResolver resourceResolver = getResourceResolver();
+        Session session = resourceResolver.adaptTo(Session.class);
         try{
-            if(pagePath != null){
+            if(pagePath != null && session != null){
                 Node videoNode = session.getNode(pagePath);
-                if(videoNode != null){
-                    videoId = videoNode.getNode("jcr:content").getNode("video-data").getProperty("vID").getString();
+                if(videoNode != null && videoNode.hasNode("jcr:content")){
+                    Node videoContent = videoNode.getNode("jcr:content");
+                    if(videoContent.hasNode("video-data")){
+                        Node videoData = videoContent.getNode("video-data");
+                        if(videoData.hasProperty("vID")){
+                            videoID = videoData.getProperty("vID").getString();
+                        }
+                    }
+
                 }
             }
         }catch (Exception e){
             e.getMessage();
         }
-        if(videoId != null){
-            videoPath = "/content/bmc/videos.html?vID="+videoId;
+        if(videoID != null){
+            videoPath = "/content/bmc/videos.html?vID="+videoID;
         }
         return videoPath;
     }
