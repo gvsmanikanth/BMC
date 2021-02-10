832f9e8e462d4eaaee5949723c72b27f296c82ab
Pavan <pavan.aem12@gmail.com>
2021-02-10 14:29:41 -0500

    Automated Sitemap XML

---
+++ aem-project/core/src/main/java/com/bmc/models/sitemap/SitemapModel.java
@@ -0,0 +1,43 @@
+package com.bmc.models.sitemap;
+
+import java.util.Map;
+
+public class SitemapModel {
+	String sitemapsectiontitle;
+	String subsitemapsectiontitle;
+	String title;
+	Map<String, String> sitemapsectionmap;
+
+	public String getSitemapsectiontitle() {
+		return sitemapsectiontitle;
+	}
+
+	public void setSitemapsectiontitle(String sitemapsectiontitle) {
+		this.sitemapsectiontitle = sitemapsectiontitle;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Map<String, String> getSitemapsectionmap() {
+		return sitemapsectionmap;
+	}
+
+	public void setSitemapsectionmap(Map<String, String> sitemapsectionmap) {
+		this.sitemapsectionmap = sitemapsectionmap;
+	}
+
+	public String getSubsitemapsectiontitle() {
+		return subsitemapsectiontitle;
+	}
+
+	public void setSubsitemapsectiontitle(String subsitemapsectiontitle) {
+		this.subsitemapsectiontitle = subsitemapsectiontitle;
+	}
+
+}
---
+++ aem-project/core/src/main/java/com/bmc/services/BMCSitemapService.java
@@ -0,0 +1,78 @@
+package com.bmc.services;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.apache.commons.lang3.StringUtils;
+import com.bmc.models.sitemap.SitemapModel;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageFilter;
+import com.day.cq.wcm.api.PageManager;
+
+@Component(service = BMCSitemapService.class, immediate = true)
+public class BMCSitemapService {
+	@Reference
+	private transient SiteMapXMLService siteMapXMLService;
+	private String[] excludeFromSiteMapProperty;
+	private String[] excludedPageTemplates;
+	public List<SitemapModel> productListService(PageManager pageManager, Resource htmlpath) {
+		this.excludeFromSiteMapProperty = siteMapXMLService.getExcludeProperty();
+		this.excludedPageTemplates = siteMapXMLService.getExcludeTemplates();
+		List<SitemapModel> sitemapmodellist = new ArrayList<SitemapModel>();
+		List<Map<String, String>> finalProductList = new ArrayList<Map<String, String>>();
+		if (pageManager != null) {
+			for (Iterator<Resource> children = htmlpath.listChildren(); children.hasNext();) {
+				SitemapModel sitemapModel = new SitemapModel();
+				Resource child = children.next();
+				ValueMap provalue = child.getValueMap();
+				String value = provalue.get("htmlpath", String.class);
+				Map<String, String> details = new HashMap<String, String>();
+				Page page = pageManager.getPage(value);
+				for (Iterator<Page> pchildren = page.listChildren(new PageFilter(false, true), true); pchildren
+						.hasNext();) {
+					getPagePropertyValue(pchildren.next(), details);
+				}
+				sitemapModel.setSitemapsectiontitle(provalue.get("sitemapsectiontitle", String.class));
+				sitemapModel.setSubsitemapsectiontitle(provalue.get("subsitemapsectiontitle", String.class));
+				sitemapModel.setSitemapsectionmap(details);
+				sitemapmodellist.add(sitemapModel);
+				finalProductList.add(details);
+			}
+		}
+		return sitemapmodellist;
+	}
+
+	private Map<String, String> getPagePropertyValue(Page page, Map<String, String> details) {
+		if (isHiddenByPageProperty(page)|| isHiddenByPageTemplate(page)) {
+			return details;
+		}
+		details.put(page.getTitle(), page.getPath() + ".html");
+		return details;
+	}
+
+	private boolean isHiddenByPageProperty(Page page) {
+		boolean flag = false;
+		if (this.excludeFromSiteMapProperty != null) {
+			for (String pageProperty : this.excludeFromSiteMapProperty) {
+				flag = flag || page.getProperties().get(pageProperty, Boolean.FALSE);
+			}
+		}
+		return flag;
+	}
+	private boolean isHiddenByPageTemplate(Page page) {
+		boolean flag = false;
+		if(this.excludedPageTemplates != null){
+			for(String pageTemplate : this.excludedPageTemplates){
+				flag = flag || page.getProperties().get("cq:template", StringUtils.EMPTY).equalsIgnoreCase(pageTemplate);
+			}
+		}
+		return flag;
+	} 
+}
