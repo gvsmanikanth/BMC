d94e8ee354a14906979e4b068bd46eaafa5e8010
dchornyi <dmytro_chornyi_tp@bmc.com>
2021-03-05 16:22:52 +0200

    Guided tour mobile view fix

--- front-end/Assets/src/support-central-angular/src/app/guided-tour/guided-tour.component.ts
+++ front-end/Assets/src/support-central-angular/src/app/guided-tour/guided-tour.component.ts
@@ -17,23 +17,40 @@ export class GuidedTourComponent implements OnInit {
   communitiesOpened = false;
   downloadsOpened = false;
   unsubscribeObs$ = new Subject();
+  tourRunning = false;
+  intro = introJs();
 
   ngOnInit() {
     this.state.caseManagementOpened$.pipe(takeUntil(this.unsubscribeObs$)).subscribe((value) => {
       this.caseOpened = value;
+      // if (value && this.tourRunning) {
+      //   setTimeout(() => {
+      //     console.log(this.intro);
+      //     this.intro.nextStep();
+      //   }, 800)
+      // }
     });
     this.state.communitiesOpened$.pipe(takeUntil(this.unsubscribeObs$)).subscribe((value) => {
       this.communitiesOpened = value;
+      // if (value && this.tourRunning) {
+      //   setTimeout(() => {
+      //     this.intro.nextStep();
+      //   }, 800)
+      // }
     });
     this.state.productDownloadsOpened$.pipe(takeUntil(this.unsubscribeObs$)).subscribe((value) => {
       this.downloadsOpened = value;
+      // if (value && this.tourRunning) {
+      //   setTimeout(() => {
+      //     this.intro.nextStep();
+      //   }, 800)
+      // }
     });
-
   }
 
   loggedInUserTour() {
-    let intro = introJs();
-    intro.setOptions(
+    this.tourRunning = true;
+    this.intro.setOptions(
       {
         steps: [
           { 
@@ -47,34 +64,41 @@ export class GuidedTourComponent implements OnInit {
       }
     );
     if (!!document.getElementById('newUser')) {
-      intro.addStep({
+      this.intro.addStep({
         element: '#newUser a',
         intro: 'If you are a new user, the orientation checklist is a great place to start.'
       })//3
     } else {
-      intro.addStep({
-        element: '.psc-o-checklist .learn-more',
+      this.intro.addStep({
+      element: '.psc-o-checklist .learn-more',
         intro: 'If you are a new user, the orientation checklist is a great place to start.'
       })//3
     }
-    intro.addStep({
+    this.intro.addStep({
       element: '#searchbox',
       intro: 'Use the search box to find answers to your questions.'
     });//4
-    intro.addStep({
+    this.intro.addStep({
       element: '#tile0 .tile-body',
       intro: 'Clicking here will lead to the fully detailed case management application.'
     });//5
-    intro.addStep({
+    this.intro.addStep({
       element: '#tile0 .tile-arrow',
       intro: 'Click on the personalized view icon to go into a summary of your support cases.',
+      preChange: (step, ctx) =>  {
+        let element = document.querySelector('#tile0 .tile-arrow');
+        if (!!element) {
+          step.element = element;
+          return true;
+        } 
+      }
     });//6
-    intro.addStep({
+    this.intro.addStep({
       element: '.extended-widget-body .btn-primary',
       intro: 'You can submit a new support case here.',
       preChange: (step, ctx) => {
         if (!this.caseOpened) {
-          intro.goToStepNumber(10);
+          this.intro.goToStepNumber(10);
           ++ctx._currentStep
           return false;
         } else {
@@ -87,12 +111,12 @@ export class GuidedTourComponent implements OnInit {
         }
       }
     });//7
-    intro.addStep({
+    this.intro.addStep({
       element: '.extended-widget-body .btn-secondary',
       intro: 'View all your cases in the fully detailed case management application.',
       preChange: (step, ctx) => {
         if (!this.caseOpened) {
-          intro.goToStepNumber(6);
+          this.intro.goToStepNumber(6);
           --ctx._currentStep;
           return false;
         } else {
@@ -105,27 +129,40 @@ export class GuidedTourComponent implements OnInit {
         }
       }
     });//8
-    intro.addStep({
+    this.intro.addStep({
       element: '#tile0 .tile-arrow',
       intro: 'Click here to exit the case management widget and go back to the homepage.',
       preChange: (step, ctx) => {
         if (!this.caseOpened) {
-          intro.goToStepNumber(6);
+          this.intro.goToStepNumber(6);
           --ctx._currentStep;
           return false;
+        } else {
+          let element = document.querySelector('#tile0 .tile-arrow');
+          if (!!element) {
+            step.element = element;
+            return true;
+          } 
         }
       }
     });//9
-    intro.addStep({
+    this.intro.addStep({
       element: '#tile1 .tile-arrow',
-      intro: 'Click here to open the community widget.'
+      intro: 'Click here to open the community widget.',
+      preChange: (step, ctx) =>  {
+        let element = document.querySelector('#tile1 .tile-arrow');
+        if (!!element) {
+          step.element = element;
+          return true;
+        } 
+      }
     });//10
-    intro.addStep({
+    this.intro.addStep({
       element:'.extended-widget-body',
       intro: 'You are now seeing your top 5 personalized products. This personalization is based on your favorite products and activity on BMC websites.',
       preChange: (step, ctx) => {
         if (!this.communitiesOpened) {
-          intro.goToStepNumber(16);
+          this.intro.goToStepNumber(16);
           ++ctx._currentStep;
           return false;
         } else {
@@ -138,12 +175,12 @@ export class GuidedTourComponent implements OnInit {
         }
       }
     });//11
-    intro.addStep({
+    this.intro.addStep({
       element:'.extended-widget-header .description a',
       intro: 'You can edit your favorite products and influence your personalization here.',
       preChange: (step, ctx) => {
         if (!this.communitiesOpened) {
-          intro.goToStepNumber(10);
+          this.intro.goToStepNumber(10);
           --ctx._currentStep;
           return false;
         } else {
@@ -156,12 +193,12 @@ export class GuidedTourComponent implements OnInit {
         }
       }
     });//12
-    intro.addStep({
+    this.intro.addStep({
       element:'.tab-container .nav-pills',
       intro: 'Click a tab to go to a particular content type.',
       preChange: (step, ctx) => {
         if (!this.communitiesOpened) {
-          intro.goToStepNumber(10);
+          this.intro.goToStepNumber(10);
           --ctx._currentStep;
           return false;
         } else {
@@ -174,12 +211,12 @@ export class GuidedTourComponent implements OnInit {
         }
       }
     });//13
-    intro.addStep({
+    this.intro.addStep({
       element:'adapt-accordion-tab:nth-child(2) .card',
       intro: 'Click on another product in the list to see that product\'s community content.',
       preChange: (step, ctx) => {
         if (!this.communitiesOpened) {
-          intro.goToStepNumber(10);
+          this.intro.goToStepNumber(10);
           --ctx._currentStep;
           return false;
         } else {
@@ -192,13 +229,13 @@ export class GuidedTourComponent implements OnInit {
         }
       }
     });//14
-    intro.addStep({
+    this.intro.addStep({
       element: '.container .close-action',
       intro: 'Click the X here to close this widget and go back to the homepage.',
       position: 'left',
       preChange: (step, ctx) => {
         if (!this.communitiesOpened) {
-          intro.goToStepNumber(10);
+          this.intro.goToStepNumber(10);
           --ctx._currentStep;
           return false;
         } else {
@@ -211,17 +248,24 @@ export class GuidedTourComponent implements OnInit {
         }
       }
     });//15
-    intro.addStep({
+    this.intro.addStep({
       element:'#tile2 .tile-arrow',
       intro: 'Click here to open a personalized view of Product downloads.',
-      position: 'left'
+      position: 'left',
+      preChange: (step, ctx) =>  {
+        let element = document.querySelector('#tile2 .tile-arrow');
+        if (!!element) {
+          step.element = element;
+          return true;
+        } 
+      }
     });//16
-    intro.addStep({
+    this.intro.addStep({
       element:'.extended-widget-body',
       intro: 'You are now seeing your top 5 personalized products. This personalization is based on your favorite products and activity on BMC websites.',
       preChange: (step, ctx) => {
         if (!this.downloadsOpened) {
-          intro.goToStepNumber(21);
+          this.intro.goToStepNumber(21);
           ++ctx._currentStep;
           return false;
         } else {
@@ -234,12 +278,12 @@ export class GuidedTourComponent implements OnInit {
         }
       }
     });//17
-    intro.addStep({
+    this.intro.addStep({
       element:'app-epd-product:first-child .epd-version:first-child',
       intro: 'You can click here to go straight to this product version\'s download page.',
       preChange: (step, ctx) => {
         if (!this.downloadsOpened) {
-          intro.goToStepNumber(16);
+          this.intro.goToStepNumber(16);
           --ctx._currentStep;
           return false;
         } else {
@@ -252,12 +296,12 @@ export class GuidedTourComponent implements OnInit {
         }
       }
     });//18
-    intro.addStep({
+    this.intro.addStep({
       element:'.extended-widget-header .external-app-link',
       intro: 'Clicking here will lead to the full download center.',
       preChange: (step, ctx) => {
         if (!this.downloadsOpened) {
-          intro.goToStepNumber(16);
+          this.intro.goToStepNumber(16);
           --ctx._currentStep;
           return false;
         } else {
@@ -270,48 +314,63 @@ export class GuidedTourComponent implements OnInit {
         }
       }
     });//19
-    intro.addStep({
+    this.intro.addStep({
       element:'#tile2 .tile-arrow',
       intro: 'Click here to exit the Product Downloads widget and go back to the homepage.',
       preChange: (step, ctx) => {
         if (!this.downloadsOpened) {
-          intro.goToStepNumber(16);
+          this.intro.goToStepNumber(16);
           --ctx._currentStep;
           return false;
+        } else {
+          let element = document.querySelector('#tile2 .tile-arrow');
+          if (!!element) {
+            step.element = element;
+            return true;
+          }
         }
       }
     });//20
-    intro.addStep({
+    this.intro.addStep({
       element:'.psc-news .news-container-wrapper',
       intro: 'Here you can see the latest support news and features.Click on any one news card to read the news in detail.'
     });//21
-    intro.addStep({
+    this.intro.addStep({
       element:'.psc-orientation-checklist .psc-fs-12:first-child .psc-checklist-right',
       intro: 'If you are upgrading a BMC product, please click here & read about the Amigo program.'
     });//22
-    intro.addStep({
+    this.intro.addStep({
       element:'.psc-orientation-checklist .psc-fs-12:nth-child(2) .psc-checklist-right',
       intro: 'You can click here to view the Support Central user guide in PDF format or online.'
     });//23
-    intro.addStep({
+    this.intro.addStep({
       element:'.support-chat-now',
-      intro: 'If you don\'t find an answer to your questions, please use the chat function.'
+      intro: 'If you don\'t find an answer to your questions, please use the chat function.',
+      preChange: (step, ctx) => {
+        if (step.element.style.display === 'none') {
+          this.intro.nextStep();
+          ++ctx._currentStep;
+          return false;
+        } 
+        return true;
+      }
     });//24
-    intro.addStep({
+    this.intro.addStep({
       element:'#feedbackTab',
       intro: 'If you have feedback for us about this new support homepage, please click here.'
     });//25
-    intro.start();
-    intro.onbeforechange(function (step){
-      console.log(this._currentStep);
-      console.log(this._currentStepNumber);
+    this.intro.onbeforechange(function (step){
       if (this._introItems[this._currentStep].preChange) {
-        console.log(this._introItems[this._currentStep])
         return this._introItems[this._currentStep].preChange(this._introItems[this._currentStep], this);
       } else {
         return true;
       }
+    });
+    this.intro.onexit(() => {
+      this.tourRunning = false;
     })
+    this.intro.start();
+    
   }
 
 
